e2ff525038cd5ee154a53da7f7ddae7742df0880
-drik S <drikkes@gmail.com>
2016-08-29 12:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14530,
-	"created_at": "Mon Aug 29 07:11:17 +0000 2016",
+	"tweets": 14531,
+	"created_at": "Mon Aug 29 10:11:18 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_08.js",
 		"year": 2016,
 		"var_name": "tweets_2016_08",
-		"tweet_count": 113,
+		"tweet_count": 114,
 		"month": 8
 	},
 	{
