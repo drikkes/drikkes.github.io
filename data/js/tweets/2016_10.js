b4a07ebb22ea5270e09a93e6e1f505cf403cb8a5
-drik S <drikkes@gmail.com>
2016-10-17 11:11:19 +0200

    Added 1 tweets

--- data/js/tweets/2016_10.js
+++ data/js/tweets/2016_10.js
@@ -1,6 +1,185 @@
 Grailbird.data.tweets_2016_10 = 
 [
 	{
+		"created_at": "Mon Oct 17 08:54:09 +0000 2016",
+		"id": 787939746467184600,
+		"id_str": "787939746467184640",
+		"text": "Sie betreten jetzt den 1. Kreis der Internethölle: Abstieg in den CAPSLOCKWAHNSINN. https://t.co/D9vYdwmGlS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 787939743166304300,
+					"id_str": "787939743166304257",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Cu9TBY_XEAER1fm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Cu9TBY_XEAER1fm.jpg",
+					"url": "https://t.co/D9vYdwmGlS",
+					"display_url": "pic.twitter.com/D9vYdwmGlS",
+					"expanded_url": "https://twitter.com/drikkes/status/787939746467184640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 216,
+							"h": 44,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 44,
+							"h": 44,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 216,
+							"h": 44,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 216,
+							"h": 44,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 787939743166304300,
+					"id_str": "787939743166304257",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/Cu9TBY_XEAER1fm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Cu9TBY_XEAER1fm.jpg",
+					"url": "https://t.co/D9vYdwmGlS",
+					"display_url": "pic.twitter.com/D9vYdwmGlS",
+					"expanded_url": "https://twitter.com/drikkes/status/787939746467184640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 216,
+							"h": 44,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 44,
+							"h": 44,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 216,
+							"h": 44,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 216,
+							"h": 44,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "-drik S.",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 900,
+			"friends_count": 158,
+			"listed_count": 87,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 3632,
+			"utc_offset": 7200,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 14773,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de"
+	},
+	{
 		"created_at": "Mon Oct 17 07:02:03 +0000 2016",
 		"id": 787911537897959400,
 		"id_str": "787911537897959424",
