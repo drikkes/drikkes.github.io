4aa777af591d8913ddbb24a0a13d94e981260c01
-drik S <drikkes@gmail.com>
2016-12-12 13:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15060,
-	"created_at": "Mon Dec 12 08:11:17 +0000 2016",
+	"tweets": 15061,
+	"created_at": "Mon Dec 12 12:11:17 +0000 2016",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2016_12.js",
 		"year": 2016,
 		"var_name": "tweets_2016_12",
-		"tweet_count": 47,
+		"tweet_count": 48,
 		"month": 12
 	},
 	{
