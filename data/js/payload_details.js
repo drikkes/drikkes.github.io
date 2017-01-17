fefa1baf44bf8f13bc14315ff058cb720a7bad1c
-drik S <drikkes@gmail.com>
2017-01-17 15:11:22 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15219,
-	"created_at": "Tue Jan 17 13:11:21 +0000 2017",
+	"tweets": 15220,
+	"created_at": "Tue Jan 17 14:11:21 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_01.js",
 		"year": 2017,
 		"var_name": "tweets_2017_01",
-		"tweet_count": 82,
+		"tweet_count": 83,
 		"month": 1
 	},
 	{
