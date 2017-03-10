db4e966b72eea7638265462f8c7cbc78a22f9ebc
-drik S <drikkes@gmail.com>
2017-03-10 13:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15489,
-	"created_at": "Thu Mar 09 22:11:18 +0000 2017",
+	"tweets": 15490,
+	"created_at": "Fri Mar 10 12:11:18 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_03.js
+++ data/js/tweets/2017_03.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2017_03 = 
 [
 	{
+		"created_at": "Fri Mar 10 11:11:24 +0000 2017",
+		"id": 840158140184313900,
+		"id_str": "840158140184313856",
+		"text": "@ralfheimann Den Feuerwehrvergleich hat leider schon jemand vorher gebracht – noch dazu anständig gegendert. https://t.co/PdS6pQsRWt",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ralfheimann",
+					"name": "Ralf Heimann",
+					"id": 23431577,
+					"id_str": "23431577",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/PdS6pQsRWt",
+					"expanded_url": "https://twitter.com/L_Petersdotter/status/839970094264553472",
+					"display_url": "twitter.com/L_Petersdotter…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 840145008745312300,
+		"in_reply_to_status_id_str": "840145008745312256",
+		"in_reply_to_user_id": 23431577,
+		"in_reply_to_user_id_str": "23431577",
+		"in_reply_to_screen_name": "ralfheimann",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "-drik S.",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 897,
+			"friends_count": 159,
+			"listed_count": 86,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 3867,
+			"utc_offset": 3600,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15482,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 839970094264553500,
+		"quoted_status_id_str": "839970094264553472",
+		"quoted_status": {
+			"created_at": "Thu Mar 09 22:44:11 +0000 2017",
+			"id": 839970094264553500,
+			"id_str": "839970094264553472",
+			"text": "Wie erkläre ich einer Mutter, deren Sohn oder Tochter bei der Feuerwehr ist, dass wir die Menschen nicht ins brenne… https://t.co/J5p4Ad6bIt",
+			"truncated": true,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/J5p4Ad6bIt",
+						"expanded_url": "https://twitter.com/i/web/status/839970094264553472",
+						"display_url": "twitter.com/i/web/status/8…",
+						"indices": [
+							117,
+							140
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1281030504,
+				"id_str": "1281030504",
+				"name": "Lasse Petersdotter",
+				"screen_name": "L_Petersdotter",
+				"location": "Kiel",
+				"description": "Listenplatz 8 der Grünen zur #ltwsh. Für mehr Solidarität. Für mehr Tacheles. Für Tanzen statt Gleichschritt!",
+				"url": "https://t.co/sg0FVrwIem",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/sg0FVrwIem",
+								"expanded_url": "http://www.lasse-petersdotter.de",
+								"display_url": "lasse-petersdotter.de",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 960,
+				"friends_count": 962,
+				"listed_count": 36,
+				"created_at": "Tue Mar 19 17:38:56 +0000 2013",
+				"favourites_count": 2119,
+				"utc_offset": -28800,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 5014,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/763842823229730816/I_XpF4B5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/763842823229730816/I_XpF4B5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1281030504/1470949351",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1129,
+			"favorite_count": 1799,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de"
+	},
+	{
 		"created_at": "Thu Mar 09 21:53:01 +0000 2017",
 		"id": 839957220720840700,
 		"id_str": "839957220720840705",
