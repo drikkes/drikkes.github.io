9413e262706c51cbdcb43e48486bdcec69830198
-drik S <drikkes@gmail.com>
2017-04-04 23:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15601,
-	"created_at": "Tue Apr 04 17:11:17 +0000 2017",
+	"tweets": 15602,
+	"created_at": "Tue Apr 04 21:11:18 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_04.js
+++ data/js/tweets/2017_04.js
@@ -1,6 +1,376 @@
 Grailbird.data.tweets_2017_04 = 
 [
 	{
+		"created_at": "Tue Apr 04 20:39:09 +0000 2017",
+		"id": 849360713776861200,
+		"id_str": "849360713776861185",
+		"text": "RT @verkehrsdaten: #BétonGate https://t.co/ZtogpyTyXw",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BétonGate",
+					"indices": [
+						19,
+						29
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "verkehrsdaten",
+					"name": "Artur",
+					"id": 4836498256,
+					"id_str": "4836498256",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 849335082393509900,
+					"id_str": "849335082393509889",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+					"url": "https://t.co/ZtogpyTyXw",
+					"display_url": "pic.twitter.com/ZtogpyTyXw",
+					"expanded_url": "https://twitter.com/verkehrsdaten/status/849335163922391040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2000,
+							"h": 1315,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 447,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 789,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 849335163922391000,
+					"source_status_id_str": "849335163922391040",
+					"source_user_id": 4836498256,
+					"source_user_id_str": "4836498256"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 849335082393509900,
+					"id_str": "849335082393509889",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+					"url": "https://t.co/ZtogpyTyXw",
+					"display_url": "pic.twitter.com/ZtogpyTyXw",
+					"expanded_url": "https://twitter.com/verkehrsdaten/status/849335163922391040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2000,
+							"h": 1315,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 447,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 789,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 849335163922391000,
+					"source_status_id_str": "849335163922391040",
+					"source_user_id": 4836498256,
+					"source_user_id_str": "4836498256"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "-drik S.",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 896,
+			"friends_count": 158,
+			"listed_count": 85,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 3916,
+			"utc_offset": 7200,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15594,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Apr 04 18:57:37 +0000 2017",
+			"id": 849335163922391000,
+			"id_str": "849335163922391040",
+			"text": "#BétonGate https://t.co/ZtogpyTyXw",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "BétonGate",
+						"indices": [
+							0,
+							10
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 849335082393509900,
+						"id_str": "849335082393509889",
+						"indices": [
+							11,
+							34
+						],
+						"media_url": "http://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+						"url": "https://t.co/ZtogpyTyXw",
+						"display_url": "pic.twitter.com/ZtogpyTyXw",
+						"expanded_url": "https://twitter.com/verkehrsdaten/status/849335163922391040/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2000,
+								"h": 1315,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 447,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 789,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 849335082393509900,
+						"id_str": "849335082393509889",
+						"indices": [
+							11,
+							34
+						],
+						"media_url": "http://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C8lxwelXoAEVauH.jpg",
+						"url": "https://t.co/ZtogpyTyXw",
+						"display_url": "pic.twitter.com/ZtogpyTyXw",
+						"expanded_url": "https://twitter.com/verkehrsdaten/status/849335163922391040/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2000,
+								"h": 1315,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 447,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 789,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 4836498256,
+				"id_str": "4836498256",
+				"name": "Artur",
+				"screen_name": "verkehrsdaten",
+				"location": "",
+				"description": "Immer Locher vom Hocker.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1695,
+				"friends_count": 173,
+				"listed_count": 21,
+				"created_at": "Fri Jan 22 21:30:59 +0000 2016",
+				"favourites_count": 83125,
+				"utc_offset": 7200,
+				"time_zone": "Berlin",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1905,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/848187469812117504/f8-YsPeP_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/848187469812117504/f8-YsPeP_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/4836498256/1490005730",
+				"profile_link_color": "7323DC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 67,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und"
+	},
+	{
 		"created_at": "Tue Apr 04 17:02:31 +0000 2017",
 		"id": 849306197224304600,
 		"id_str": "849306197224304643",
