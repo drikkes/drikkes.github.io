12ff84101bc5de957cbc26761df263fb1ec1ab10
-drik S <drikkes@gmail.com>
2017-05-10 20:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15737,
-	"created_at": "Wed May 10 16:11:18 +0000 2017",
+	"tweets": 15738,
+	"created_at": "Wed May 10 18:11:18 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_05.js",
 		"year": 2017,
 		"var_name": "tweets_2017_05",
-		"tweet_count": 44,
+		"tweet_count": 45,
 		"month": 5
 	},
 	{
