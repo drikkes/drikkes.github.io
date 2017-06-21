3098cf8cc84c19f625f0d9220af722a8cbf2415e
-drik S <drikkes@gmail.com>
2017-06-22 00:11:21 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15932,
-	"created_at": "Wed Jun 21 19:11:21 +0000 2017",
+	"tweets": 15933,
+	"created_at": "Wed Jun 21 22:11:20 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_06.js",
 		"year": 2017,
 		"var_name": "tweets_2017_06",
-		"tweet_count": 98,
+		"tweet_count": 99,
 		"month": 6
 	},
 	{
