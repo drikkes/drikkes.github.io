e3a1b237a451d6b68c6d37180e07119848411f74
-drik S <drikkes@gmail.com>
2017-08-16 19:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16153,
-	"created_at": "Wed Aug 16 16:11:19 +0000 2017",
+	"tweets": 16154,
+	"created_at": "Wed Aug 16 17:11:19 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,263 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Wed Aug 16 17:09:54 +0000 2017",
+		"id": 897868028955824100,
+		"id_str": "897868028955824128",
+		"text": "Um 22:10 Uhr für die Linearen. https://t.co/C5C3AA8Ipb",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/C5C3AA8Ipb",
+					"expanded_url": "https://twitter.com/Shelfd/status/897829104455700481",
+					"display_url": "twitter.com/Shelfd/status/…",
+					"indices": [
+						31,
+						54
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "-drik S.",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 904,
+			"friends_count": 160,
+			"listed_count": 85,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4077,
+			"utc_offset": 7200,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16142,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 897829104455700500,
+		"quoted_status_id_str": "897829104455700481",
+		"quoted_status": {
+			"created_at": "Wed Aug 16 14:35:14 +0000 2017",
+			"id": 897829104455700500,
+			"id_str": "897829104455700481",
+			"text": "Heute Abend im TV, aber schon jetzt in der #Mediathek: Wir empfehlen #UnderTheSkin.\nhttps://t.co/chJY9YZCld #ScarlettJohansson via @ARTEde",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Mediathek",
+						"indices": [
+							43,
+							53
+						]
+					},
+					{
+						"text": "UnderTheSkin",
+						"indices": [
+							69,
+							82
+						]
+					},
+					{
+						"text": "ScarlettJohansson",
+						"indices": [
+							108,
+							126
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "ARTEde",
+						"name": "ARTE",
+						"id": 26472681,
+						"id_str": "26472681",
+						"indices": [
+							131,
+							138
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/chJY9YZCld",
+						"expanded_url": "https://shelfd.com/video/under-the-skin",
+						"display_url": "shelfd.com/video/under-th…",
+						"indices": [
+							84,
+							107
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://www.hootsuite.com\" rel=\"nofollow\">Hootsuite</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1590616704,
+				"id_str": "1590616704",
+				"name": "Shelfd",
+				"screen_name": "Shelfd",
+				"location": "Berlin, Deutschland",
+				"description": "Videos, die du nicht verpassen darfst! Wir empfehlen die besten Filme, Dokus & Serien aus der #Mediathek. 📺👀🍿 Auch im Newsletter: https://t.co/fLfuM7N7AD",
+				"url": "https://t.co/n9W5ge0jep",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/n9W5ge0jep",
+								"expanded_url": "https://www.shelfd.com",
+								"display_url": "shelfd.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/fLfuM7N7AD",
+								"expanded_url": "http://eepurl.com/cNPaYb",
+								"display_url": "eepurl.com/cNPaYb",
+								"indices": [
+									130,
+									153
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 3039,
+				"friends_count": 750,
+				"listed_count": 80,
+				"created_at": "Sat Jul 13 09:58:01 +0000 2013",
+				"favourites_count": 2365,
+				"utc_offset": 7200,
+				"time_zone": "Bern",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5118,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/648500069033975809/WWm---_4.jpg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/648500069033975809/WWm---_4.jpg",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/861351893645352964/NA-NWlwH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/861351893645352964/NA-NWlwH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1590616704/1494221137",
+				"profile_link_color": "8E60FC",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 15,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de"
+	},
+	{
 		"created_at": "Wed Aug 16 15:31:17 +0000 2017",
 		"id": 897843210197897200,
 		"id_str": "897843210197897217",
