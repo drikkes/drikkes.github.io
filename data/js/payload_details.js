8f5c7b45cc43b0e818506ed32e81cbb56e39bd7c
-drik S <drikkes@gmail.com>
2017-08-21 12:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16170,
-	"created_at": "Sun Aug 20 20:11:19 +0000 2017",
+	"tweets": 16171,
+	"created_at": "Mon Aug 21 10:11:19 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_08.js
+++ data/js/tweets/2017_08.js
@@ -1,6 +1,157 @@
 Grailbird.data.tweets_2017_08 = 
 [
 	{
+		"created_at": "Mon Aug 21 09:12:21 +0000 2017",
+		"id": 899559790556450800,
+		"id_str": "899559790556450816",
+		"text": "Sorry, sowas finde ich ulkig. @titanic über @erlebnischris im @t3n. https://t.co/g4z61I2TPD (siehe Interview https://t.co/Eb0RU8BrDk)",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "titanic",
+					"name": "TITANIC",
+					"id": 12509262,
+					"id_str": "12509262",
+					"indices": [
+						30,
+						38
+					]
+				},
+				{
+					"screen_name": "erlebnischris",
+					"name": "Christian Cordes",
+					"id": 15324212,
+					"id_str": "15324212",
+					"indices": [
+						44,
+						58
+					]
+				},
+				{
+					"screen_name": "t3n",
+					"name": "t3n Magazin",
+					"id": 11060982,
+					"id_str": "11060982",
+					"indices": [
+						62,
+						66
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/g4z61I2TPD",
+					"expanded_url": "http://www.titanic-magazin.de/briefe/2017/august/#c27982",
+					"display_url": "titanic-magazin.de/briefe/2017/au…",
+					"indices": [
+						68,
+						91
+					]
+				},
+				{
+					"url": "https://t.co/Eb0RU8BrDk",
+					"expanded_url": "http://t3n.de/news/coworking-buzzword-christian-cordes-831515/",
+					"display_url": "t3n.de/news/coworking…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "-drik S.",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 905,
+			"friends_count": 160,
+			"listed_count": 85,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4088,
+			"utc_offset": 7200,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16158,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de"
+	},
+	{
 		"created_at": "Sun Aug 20 19:49:23 +0000 2017",
 		"id": 899357714744303600,
 		"id_str": "899357714744303616",
