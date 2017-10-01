045127fd5c3e30cf8b55386cf93d367d030eaf36
-drik S <drikkes@gmail.com>
2017-10-01 11:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16318,
-	"created_at": "Sat Sep 30 16:11:19 +0000 2017",
+	"tweets": 16319,
+	"created_at": "Sun Oct 01 09:11:19 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2017_10.js",
+		"year": 2017,
+		"var_name": "tweets_2017_10",
+		"tweet_count": 1,
+		"month": 10
+	},
+	{
 		"file_name": "data/js/tweets/2017_09.js",
 		"year": 2017,
 		"var_name": "tweets_2017_09",
