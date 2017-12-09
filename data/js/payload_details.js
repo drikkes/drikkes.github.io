b9cc8fba5f136005ab1ad3098a20c64d89a22502
-drik S <drikkes@gmail.com>
2017-12-09 15:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16608,
-	"created_at": "Fri Dec 08 17:11:18 +0000 2017",
+	"tweets": 16609,
+	"created_at": "Sat Dec 09 14:11:18 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_12.js",
 		"year": 2017,
 		"var_name": "tweets_2017_12",
-		"tweet_count": 38,
+		"tweet_count": 39,
 		"month": 12
 	},
 	{
