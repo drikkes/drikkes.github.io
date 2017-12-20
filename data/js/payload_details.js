8d256dd753ff3868a36e162b7a71afde8b0fd8e6
-drik S <drikkes@gmail.com>
2017-12-20 20:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16668,
-	"created_at": "Wed Dec 20 18:11:18 +0000 2017",
+	"tweets": 16669,
+	"created_at": "Wed Dec 20 19:11:18 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_12.js",
 		"year": 2017,
 		"var_name": "tweets_2017_12",
-		"tweet_count": 98,
+		"tweet_count": 99,
 		"month": 12
 	},
 	{
