cb61c3ec40c62c4b90a86a50cfea2bc485ae4bab
-drik S <drikkes@gmail.com>
2018-01-02 23:11:18 +0100

    Added 1 tweets

--- data/js/tweets/2018_01.js
+++ data/js/tweets/2018_01.js
@@ -1,6 +1,373 @@
 Grailbird.data.tweets_2018_01 = 
 [
 	{
+		"created_at": "Tue Jan 02 21:17:00 +0000 2018",
+		"id": 948302129269215200,
+		"id_str": "948302129269215232",
+		"text": "RT @DanaSchwartzzz: How will the Bachelor ever choose which Blonde Ombre Woman to make his wIFE????? https://t.co/JgmIFMbmIS",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DanaSchwartzzz",
+					"name": "Dana Schwartz",
+					"id": 331120729,
+					"id_str": "331120729",
+					"indices": [
+						3,
+						18
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 947964016713924600,
+					"id_str": "947964016713924608",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+					"url": "https://t.co/JgmIFMbmIS",
+					"display_url": "pic.twitter.com/JgmIFMbmIS",
+					"expanded_url": "https://twitter.com/DanaSchwartzzz/status/947964069558013952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 674,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 674,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 554,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 947964069558014000,
+					"source_status_id_str": "947964069558013952",
+					"source_user_id": 331120729,
+					"source_user_id_str": "331120729"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 947964016713924600,
+					"id_str": "947964016713924608",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+					"url": "https://t.co/JgmIFMbmIS",
+					"display_url": "pic.twitter.com/JgmIFMbmIS",
+					"expanded_url": "https://twitter.com/DanaSchwartzzz/status/947964069558013952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 674,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 674,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 554,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 947964069558014000,
+					"source_status_id_str": "947964069558013952",
+					"source_user_id": 331120729,
+					"source_user_id_str": "331120729"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 903,
+			"friends_count": 161,
+			"listed_count": 80,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4272,
+			"utc_offset": 3600,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 16705,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Jan 01 22:53:41 +0000 2018",
+			"id": 947964069558014000,
+			"id_str": "947964069558013952",
+			"text": "How will the Bachelor ever choose which Blonde Ombre Woman to make his wIFE????? https://t.co/JgmIFMbmIS",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 947964016713924600,
+						"id_str": "947964016713924608",
+						"indices": [
+							81,
+							104
+						],
+						"media_url": "http://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+						"url": "https://t.co/JgmIFMbmIS",
+						"display_url": "pic.twitter.com/JgmIFMbmIS",
+						"expanded_url": "https://twitter.com/DanaSchwartzzz/status/947964069558013952/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 828,
+								"h": 674,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 828,
+								"h": 674,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 554,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 947964016713924600,
+						"id_str": "947964016713924608",
+						"indices": [
+							81,
+							104
+						],
+						"media_url": "http://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DSfYQCwWAAA-f_y.jpg",
+						"url": "https://t.co/JgmIFMbmIS",
+						"display_url": "pic.twitter.com/JgmIFMbmIS",
+						"expanded_url": "https://twitter.com/DanaSchwartzzz/status/947964069558013952/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 828,
+								"h": 674,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 828,
+								"h": 674,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 554,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 331120729,
+				"id_str": "331120729",
+				"name": "Dana Schwartz",
+				"screen_name": "DanaSchwartzzz",
+				"location": "New York, NY",
+				"description": "Correspondent @EW. Author of AND WE'RE OFF and CHOOSE YOUR OWN DISASTER (6/18). See also: @GuyInYourMFA, @DystopianYA.",
+				"url": "https://t.co/4J9hgZ0XBB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/4J9hgZ0XBB",
+								"expanded_url": "http://DanaSchwartzDotCom.com",
+								"display_url": "DanaSchwartzDotCom.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 71357,
+				"friends_count": 975,
+				"listed_count": 1065,
+				"created_at": "Thu Jul 07 17:42:14 +0000 2011",
+				"favourites_count": 233920,
+				"utc_offset": -18000,
+				"time_zone": "Eastern Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 35412,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/925182126475137025/xOEMYrgE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/925182126475137025/xOEMYrgE_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/331120729/1431404969",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9167,
+			"favorite_count": 38353,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 9167,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Tue Jan 02 10:50:00 +0000 2018",
 		"id": 948144336742568000,
 		"id_str": "948144336742567936",
