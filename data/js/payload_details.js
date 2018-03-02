15b5b247036c82659e9db630449f40d947b3949b
-drik S <drikkes@gmail.com>
2018-03-02 16:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17036,
-	"created_at": "Fri Mar 02 13:11:17 +0000 2018",
+	"tweets": 17037,
+	"created_at": "Fri Mar 02 15:11:18 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_03.js",
 		"year": 2018,
 		"var_name": "tweets_2018_03",
-		"tweet_count": 6,
+		"tweet_count": 7,
 		"month": 3
 	},
 	{
