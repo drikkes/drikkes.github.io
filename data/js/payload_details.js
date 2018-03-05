463d63bc46b8b3beea10155d01b77ea08e40d67c
-drik S <drikkes@gmail.com>
2018-03-05 12:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17042,
-	"created_at": "Mon Mar 05 10:11:18 +0000 2018",
+	"tweets": 17043,
+	"created_at": "Mon Mar 05 11:11:18 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Mon Mar 05 10:50:08 +0000 2018",
+		"id": 970612419113508900,
+		"id_str": "970612419113508864",
+		"text": "RT @NextLevel_Fest: Top 7: Wenn Games zum Musikvideo werden https://t.co/gpVRr12zrK. @intromagazin",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NextLevel_Fest",
+					"name": "Next Level Festival",
+					"id": 184755258,
+					"id_str": "184755258",
+					"indices": [
+						3,
+						18
+					]
+				},
+				{
+					"screen_name": "intromagazin",
+					"name": "Intro",
+					"id": 14675103,
+					"id_str": "14675103",
+					"indices": [
+						85,
+						98
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gpVRr12zrK",
+					"expanded_url": "http://www.intro.de/popmusik/wenn-games-zum-musikvideo-werden-top-7#",
+					"display_url": "intro.de/popmusik/wenn-…",
+					"indices": [
+						60,
+						83
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 898,
+			"friends_count": 157,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4361,
+			"utc_offset": 3600,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17027,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Mar 05 08:58:44 +0000 2018",
+			"id": 970584383550820400,
+			"id_str": "970584383550820352",
+			"text": "Top 7: Wenn Games zum Musikvideo werden https://t.co/gpVRr12zrK. @intromagazin",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "intromagazin",
+						"name": "Intro",
+						"id": 14675103,
+						"id_str": "14675103",
+						"indices": [
+							65,
+							78
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/gpVRr12zrK",
+						"expanded_url": "http://www.intro.de/popmusik/wenn-games-zum-musikvideo-werden-top-7#",
+						"display_url": "intro.de/popmusik/wenn-…",
+						"indices": [
+							40,
+							63
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 184755258,
+				"id_str": "184755258",
+				"name": "Next Level Festival",
+				"screen_name": "NextLevel_Fest",
+				"location": "9.–12. November / Düsseldorf",
+				"description": "Next Level - Festival for Games vereint Fans und Fachleute der digitalen Spielekultur und bietet Input zu Games+Bildung+Kunst+Wirtschaft @kaaro_linka @LinScholz",
+				"url": "https://t.co/dLC6ZQOQxM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/dLC6ZQOQxM",
+								"expanded_url": "http://next-level.org",
+								"display_url": "next-level.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 880,
+				"friends_count": 743,
+				"listed_count": 36,
+				"created_at": "Mon Aug 30 12:05:06 +0000 2010",
+				"favourites_count": 1039,
+				"utc_offset": 3600,
+				"time_zone": "Paris",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1719,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "00A1C1",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/884377776190615553/dw8JQMgr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/884377776190615553/dw8JQMgr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/184755258/1499687568",
+				"profile_link_color": "36F43C",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de"
+	},
+	{
 		"created_at": "Mon Mar 05 09:34:17 +0000 2018",
 		"id": 970593330831724500,
 		"id_str": "970593330831724544",
