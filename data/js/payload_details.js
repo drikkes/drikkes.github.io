19658a15715e9d519f630d19887cd0e96935648f
-drik S <drikkes@gmail.com>
2018-04-08 16:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17187,
-	"created_at": "Sun Apr 08 12:11:18 +0000 2018",
+	"tweets": 17188,
+	"created_at": "Sun Apr 08 14:11:18 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,383 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Sun Apr 08 13:33:38 +0000 2018",
+		"id": 982974751600758800,
+		"id_str": "982974751600758785",
+		"text": "RT @pixelatedboat: Donkey Kong https://t.co/HnTrBqBCOx",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pixelatedboat",
+					"name": "pixelated boat [ASMR] binaural ~4 hours~",
+					"id": 2999158160,
+					"id_str": "2999158160",
+					"indices": [
+						3,
+						17
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 982791350876647400,
+					"id_str": "982791350876647424",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+					"url": "https://t.co/HnTrBqBCOx",
+					"display_url": "pic.twitter.com/HnTrBqBCOx",
+					"expanded_url": "https://twitter.com/pixelatedboat/status/982791379481870336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1710,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 568,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1002,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 982791379481870300,
+					"source_status_id_str": "982791379481870336",
+					"source_user_id": 2999158160,
+					"source_user_id_str": "2999158160"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 982791350876647400,
+					"id_str": "982791350876647424",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+					"url": "https://t.co/HnTrBqBCOx",
+					"display_url": "pic.twitter.com/HnTrBqBCOx",
+					"expanded_url": "https://twitter.com/pixelatedboat/status/982791379481870336/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1710,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 568,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1002,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 982791379481870300,
+					"source_status_id_str": "982791379481870336",
+					"source_user_id": 2999158160,
+					"source_user_id_str": "2999158160"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 892,
+			"friends_count": 157,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4395,
+			"utc_offset": 7200,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17172,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun Apr 08 01:24:58 +0000 2018",
+			"id": 982791379481870300,
+			"id_str": "982791379481870336",
+			"text": "Donkey Kong https://t.co/HnTrBqBCOx",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 982791350876647400,
+						"id_str": "982791350876647424",
+						"indices": [
+							12,
+							35
+						],
+						"media_url": "http://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+						"url": "https://t.co/HnTrBqBCOx",
+						"display_url": "pic.twitter.com/HnTrBqBCOx",
+						"expanded_url": "https://twitter.com/pixelatedboat/status/982791379481870336/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1710,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 568,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1002,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 982791350876647400,
+						"id_str": "982791350876647424",
+						"indices": [
+							12,
+							35
+						],
+						"media_url": "http://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaOThs1UMAA39X7.jpg",
+						"url": "https://t.co/HnTrBqBCOx",
+						"display_url": "pic.twitter.com/HnTrBqBCOx",
+						"expanded_url": "https://twitter.com/pixelatedboat/status/982791379481870336/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1710,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 568,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1002,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2999158160,
+				"id_str": "2999158160",
+				"name": "pixelated boat [ASMR] binaural ~4 hours~",
+				"screen_name": "pixelatedboat",
+				"location": "Tiny Train World",
+				"description": "Prince of lies. Read my comics on IG https://t.co/FB75MYaFo4.",
+				"url": "https://t.co/Zkk0nai3v2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Zkk0nai3v2",
+								"expanded_url": "http://onegianthand.com",
+								"display_url": "onegianthand.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/FB75MYaFo4",
+								"expanded_url": "http://instagram.com/onegianthand",
+								"display_url": "instagram.com/onegianthand",
+								"indices": [
+									37,
+									60
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 213622,
+				"friends_count": 784,
+				"listed_count": 1697,
+				"created_at": "Wed Jan 28 08:27:10 +0000 2015",
+				"favourites_count": 183227,
+				"utc_offset": 34200,
+				"time_zone": "Adelaide",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 29739,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/875489341291675649/hc8K1aT0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/875489341291675649/hc8K1aT0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2999158160/1440480267",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2280,
+			"favorite_count": 9924,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "in"
+		},
+		"is_quote_status": false,
+		"retweet_count": 2280,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "in"
+	},
+	{
 		"created_at": "Sun Apr 08 11:34:15 +0000 2018",
 		"id": 982944707998289900,
 		"id_str": "982944707998289920",
