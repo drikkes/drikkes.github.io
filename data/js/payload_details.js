a106aa4cd2571e80b6e57b3f4dae72204c17542e
-drik S <drikkes@gmail.com>
2018-04-21 11:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17245,
-	"created_at": "Fri Apr 20 18:11:19 +0000 2018",
+	"tweets": 17246,
+	"created_at": "Sat Apr 21 09:11:19 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_04.js",
 		"year": 2018,
 		"var_name": "tweets_2018_04",
-		"tweet_count": 99,
+		"tweet_count": 100,
 		"month": 4
 	},
 	{
