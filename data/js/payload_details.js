dcf6395cf5f07f5cdee186393b7fa0219f35c571
-drik S <drikkes@gmail.com>
2018-04-22 16:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17248,
-	"created_at": "Sun Apr 22 08:11:18 +0000 2018",
+	"tweets": 17249,
+	"created_at": "Sun Apr 22 14:11:19 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,360 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Sun Apr 22 13:42:53 +0000 2018",
+		"id": 988050512640692200,
+		"id_str": "988050512640692224",
+		"text": "RT @ctucx: RT [protected]: lol, leipzig. deine mama mag blockchain https://t.co/AbnqV8axeX",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ctucx",
+					"name": "GNU/cTucx",
+					"id": 1900260367,
+					"id_str": "1900260367",
+					"indices": [
+						3,
+						9
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 987726342929166300,
+					"id_str": "987726342929166336",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+					"url": "https://t.co/AbnqV8axeX",
+					"display_url": "pic.twitter.com/AbnqV8axeX",
+					"expanded_url": "https://twitter.com/ctucx/status/987726357802225664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 987726357802225700,
+					"source_status_id_str": "987726357802225664",
+					"source_user_id": 1900260367,
+					"source_user_id_str": "1900260367"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 987726342929166300,
+					"id_str": "987726342929166336",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+					"url": "https://t.co/AbnqV8axeX",
+					"display_url": "pic.twitter.com/AbnqV8axeX",
+					"expanded_url": "https://twitter.com/ctucx/status/987726357802225664/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1024,
+							"h": 768,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 987726357802225700,
+					"source_status_id_str": "987726357802225664",
+					"source_user_id": 1900260367,
+					"source_user_id_str": "1900260367"
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 892,
+			"friends_count": 158,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4411,
+			"utc_offset": 7200,
+			"time_zone": "Paris",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17233,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/400182686/aufdemlaufenden_gr.jpg",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sat Apr 21 16:14:49 +0000 2018",
+			"id": 987726357802225700,
+			"id_str": "987726357802225664",
+			"text": "RT [protected]: lol, leipzig. deine mama mag blockchain https://t.co/AbnqV8axeX",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 987726342929166300,
+						"id_str": "987726342929166336",
+						"indices": [
+							56,
+							79
+						],
+						"media_url": "http://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+						"url": "https://t.co/AbnqV8axeX",
+						"display_url": "pic.twitter.com/AbnqV8axeX",
+						"expanded_url": "https://twitter.com/ctucx/status/987726357802225664/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 987726342929166300,
+						"id_str": "987726342929166336",
+						"indices": [
+							56,
+							79
+						],
+						"media_url": "http://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DbUb4C2WsAAA7kb.jpg",
+						"url": "https://t.co/AbnqV8axeX",
+						"display_url": "pic.twitter.com/AbnqV8axeX",
+						"expanded_url": "https://twitter.com/ctucx/status/987726357802225664/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 768,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1900260367,
+				"id_str": "1900260367",
+				"name": "GNU/cTucx",
+				"screen_name": "ctucx",
+				"location": "IRE Hamburg-Berlin",
+				"description": "19y old | actually autistic | homeless | nb | er/he | interested in: trains, electronics, technology, linux, OOS |  @f2k1de | DE/EN | Darki: @darkctucx",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 135,
+				"friends_count": 98,
+				"listed_count": 16,
+				"created_at": "Tue Sep 24 11:54:11 +0000 2013",
+				"favourites_count": 6556,
+				"utc_offset": 7200,
+				"time_zone": "Bern",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 6801,
+				"lang": "de",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/576927027481030656/DyN2GOb0.png",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/576927027481030656/DyN2GOb0.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/972960702821486593/74eJLaNn_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/972960702821486593/74eJLaNn_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1900260367/1520806947",
+				"profile_link_color": "333333",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 53,
+			"favorite_count": 96,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de"
+	},
+	{
 		"created_at": "Sun Apr 22 08:08:34 +0000 2018",
 		"id": 987966376244645900,
 		"id_str": "987966376244645888",
