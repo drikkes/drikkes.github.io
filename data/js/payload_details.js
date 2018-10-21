fc79a0e7f81ce3169d7b2990d7ad61dc55448994
-drik S <drikkes@gmail.com>
2018-10-21 17:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18065,
-	"created_at": "Sat Oct 20 22:11:19 +0000 2018",
+	"tweets": 18066,
+	"created_at": "Sun Oct 21 15:11:19 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Sun Oct 21 13:34:06 +0000 2018",
+		"id": 1054002890732376000,
+		"id_str": "1054002890732376064",
+		"full_text": "Der Sonntagscartoon von Huse: https://t.co/y5dkHE7fsn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1054002884935893000,
+					"id_str": "1054002884935892993",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/DqCSChzXQAEx6iu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DqCSChzXQAEx6iu.jpg",
+					"url": "https://t.co/y5dkHE7fsn",
+					"display_url": "pic.twitter.com/y5dkHE7fsn",
+					"expanded_url": "https://twitter.com/titanic/status/1054002890732376064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1054002884935893000,
+					"id_str": "1054002884935892993",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/DqCSChzXQAEx6iu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DqCSChzXQAEx6iu.jpg",
+					"url": "https://t.co/y5dkHE7fsn",
+					"display_url": "pic.twitter.com/y5dkHE7fsn",
+					"expanded_url": "https://twitter.com/titanic/status/1054002890732376064/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 720,
+							"h": 540,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 12509262,
+			"id_str": "12509262",
+			"name": "TITANIC",
+			"screen_name": "titanic",
+			"location": "Germany",
+			"description": "Das endgültige Satiremagazin. https://t.co/BTRPVc9qZH – https://t.co/QtUvJW3BUJ",
+			"url": "https://t.co/LSRmhdazhJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LSRmhdazhJ",
+							"expanded_url": "http://www.titanic-magazin.de/",
+							"display_url": "titanic-magazin.de",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/BTRPVc9qZH",
+							"expanded_url": "http://www.titanic-magazin.de/abo",
+							"display_url": "titanic-magazin.de/abo",
+							"indices": [
+								30,
+								53
+							]
+						},
+						{
+							"url": "https://t.co/QtUvJW3BUJ",
+							"expanded_url": "http://www.titanic-magazin.de/gold/mitglied-werden/",
+							"display_url": "titanic-magazin.de/gold/mitglied-…",
+							"indices": [
+								56,
+								79
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 475881,
+			"friends_count": 10,
+			"listed_count": 2933,
+			"created_at": "Mon Jan 21 21:00:57 +0000 2008",
+			"favourites_count": 23,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 16777,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "D50000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1027822688356769792/WPJJso9P_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1027822688356769792/WPJJso9P_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/12509262/1538129416",
+			"profile_link_color": "D60000",
+			"profile_sidebar_border_color": "EBEBEB",
+			"profile_sidebar_fill_color": "EBEBEB",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 28,
+		"favorite_count": 105,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Der Sonntagscartoon von Huse: https://t.co/y5dkHE7fsn"
+	},
+	{
 		"created_at": "Sat Oct 20 22:07:23 +0000 2018",
 		"id": 1053769675052646400,
 		"id_str": "1053769675052646402",
