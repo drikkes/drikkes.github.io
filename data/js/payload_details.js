e6920dd9feacd79cca2dd39863acbf9449ebce9c
-drik S <drikkes@gmail.com>
2018-10-24 16:11:21 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18094,
-	"created_at": "Wed Oct 24 12:11:20 +0000 2018",
+	"tweets": 18095,
+	"created_at": "Wed Oct 24 14:11:21 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Wed Oct 24 13:45:22 +0000 2018",
+		"id": 1055092889632628700,
+		"id_str": "1055092889632628736",
+		"full_text": "Wellness-Friseure, Ihr enttäuscht mich. https://t.co/nzMxLuloyj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			39
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1055092886943993900,
+					"id_str": "1055092886943993856",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/DqRxY_tWkAAzS4-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DqRxY_tWkAAzS4-.jpg",
+					"url": "https://t.co/nzMxLuloyj",
+					"display_url": "pic.twitter.com/nzMxLuloyj",
+					"expanded_url": "https://twitter.com/drikkes/status/1055092889632628736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 290,
+							"h": 60,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 60,
+							"h": 60,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 290,
+							"h": 60,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 290,
+							"h": 60,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1055092886943993900,
+					"id_str": "1055092886943993856",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/DqRxY_tWkAAzS4-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DqRxY_tWkAAzS4-.jpg",
+					"url": "https://t.co/nzMxLuloyj",
+					"display_url": "pic.twitter.com/nzMxLuloyj",
+					"expanded_url": "https://twitter.com/drikkes/status/1055092889632628736/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 290,
+							"h": 60,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 60,
+							"h": 60,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 290,
+							"h": 60,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 290,
+							"h": 60,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 862,
+			"friends_count": 158,
+			"listed_count": 82,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4478,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17978,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Wellness-Friseure, Ihr enttäuscht mich. https://t.co/nzMxLuloyj"
+	},
+	{
 		"created_at": "Wed Oct 24 11:13:25 +0000 2018",
 		"id": 1055054649303142400,
 		"id_str": "1055054649303142400",
