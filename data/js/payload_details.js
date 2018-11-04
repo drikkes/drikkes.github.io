fa5812f4083377eec47c870dd7d32561ef864cf8
-drik S <drikkes@gmail.com>
2018-11-04 11:11:18 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64140,
-	"created_at": "Sun Nov 04 09:11:18 +0000 2018",
+	"tweets": 64142,
+	"created_at": "Sun Nov 04 10:11:17 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,296 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Sun Nov 04 09:27:29 +0000 2018",
+		"id": 1059014257940013000,
+		"id_str": "1059014257940013056",
+		"full_text": "“Incongruously, athleisure has grown in popularity among teens at the same time that American youth sport participation has declined significantly.”\n\nHow Athleisure Conquered Modern Fashion\nhttps://t.co/SXzpaOfXZN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			213
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SXzpaOfXZN",
+					"expanded_url": "https://www.theatlantic.com/ideas/archive/2018/10/bicycle-bloomers-yoga-pants-how-sports-shaped-modern-fashion/574081/",
+					"display_url": "theatlantic.com/ideas/archive/…",
+					"indices": [
+						190,
+						213
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 861,
+			"friends_count": 159,
+			"listed_count": 82,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4493,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18010,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“Incongruously, athleisure has grown in popularity among teens at the same time that American youth sport participation has declined significantly.”\n\nHow Athleisure Conquered Modern Fashion\nhttps://t.co/SXzpaOfXZN"
+	},
+	{
+		"created_at": "Sat Nov 03 11:33:37 +0000 2018",
+		"id": 1058683614722961400,
+		"id_str": "1058683614722961408",
+		"full_text": "“Gebaut, um den Atem zu rauben.” - Die merken nix mehr, oder? https://t.co/8kZtyr3aXR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1058683582359785500,
+					"id_str": "1058683582359785474",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/DrEzG-gX0AIu_Es.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrEzG-gX0AIu_Es.jpg",
+					"url": "https://t.co/8kZtyr3aXR",
+					"display_url": "pic.twitter.com/8kZtyr3aXR",
+					"expanded_url": "https://twitter.com/der_handwerk/status/1058683614722961408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1058683582359785500,
+					"id_str": "1058683582359785474",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/DrEzG-gX0AIu_Es.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DrEzG-gX0AIu_Es.jpg",
+					"url": "https://t.co/8kZtyr3aXR",
+					"display_url": "pic.twitter.com/8kZtyr3aXR",
+					"expanded_url": "https://twitter.com/der_handwerk/status/1058683614722961408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 59064184,
+			"id_str": "59064184",
+			"name": "der_handwerk",
+			"screen_name": "der_handwerk",
+			"location": "Muri Buri Land, Vanuatu, Pott",
+			"description": "Attaché des Sonderbeauftragten",
+			"url": "https://t.co/vaBnh3abCD",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vaBnh3abCD",
+							"expanded_url": "http://handwerkszeuch.wordpress.com",
+							"display_url": "handwerkszeuch.wordpress.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18457,
+			"friends_count": 178,
+			"listed_count": 815,
+			"created_at": "Wed Jul 22 07:17:07 +0000 2009",
+			"favourites_count": 46540,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16650,
+			"lang": "de",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FAFAFA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2360244387/7a66vj23llft0x4uekoi_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2360244387/7a66vj23llft0x4uekoi_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/59064184/1398429902",
+			"profile_link_color": "919196",
+			"profile_sidebar_border_color": "C9C9C9",
+			"profile_sidebar_fill_color": "FAFAFA",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 28,
+		"favorite_count": 141,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "“Gebaut, um den Atem zu rauben.” - Die merken nix mehr, oder? https://t.co/8kZtyr3aXR"
+	},
+	{
 		"created_at": "Sat Nov 03 11:33:37 +0000 2018",
 		"id": 1058683614722961400,
 		"id_str": "1058683614722961408",
