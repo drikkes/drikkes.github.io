a46bde897064645ce08ea7edaecb14862dda2034
-drik S <drikkes@gmail.com>
2018-12-05 23:11:18 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64324,
-	"created_at": "Wed Dec 05 21:11:18 +0000 2018",
+	"tweets": 64326,
+	"created_at": "Wed Dec 05 22:11:17 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -149,6 +149,307 @@ Grailbird.data.tweets_2018_12 =
 				}
 			},
 			"protected": false,
+			"followers_count": 6874,
+			"friends_count": 5299,
+			"listed_count": 268,
+			"created_at": "Sat Dec 15 19:43:23 +0000 2012",
+			"favourites_count": 3333,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 50514,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1064161984793518081/47BwVFuq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1064161984793518081/47BwVFuq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1013808600/1453653495",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 58,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#Capitalism is nothing if it's not adaptable. #YellowVests #GiletJaunes https://t.co/Lyt6iyyaqW"
+	},
+	{
+		"created_at": "Wed Dec 05 15:54:53 +0000 2018",
+		"id": 1070345775899729900,
+		"id_str": "1070345775899729925",
+		"full_text": "[Splatter am Abend…]",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "Hendrik Spree",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 851,
+			"friends_count": 159,
+			"listed_count": 82,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4533,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18118,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "[Splatter am Abend…]"
+	},
+	{
+		"created_at": "Wed Dec 05 14:29:03 +0000 2018",
+		"id": 1070324174286012400,
+		"id_str": "1070324174286012416",
+		"full_text": "#Capitalism is nothing if it's not adaptable. #YellowVests #GiletJaunes https://t.co/Lyt6iyyaqW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Capitalism",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"text": "YellowVests",
+					"indices": [
+						46,
+						58
+					]
+				},
+				{
+					"text": "GiletJaunes",
+					"indices": [
+						59,
+						71
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1070323841061204000,
+					"id_str": "1070323841061203968",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/DtqN3NUVsAALhaZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtqN3NUVsAALhaZ.jpg",
+					"url": "https://t.co/Lyt6iyyaqW",
+					"display_url": "pic.twitter.com/Lyt6iyyaqW",
+					"expanded_url": "https://twitter.com/JohnOCAP/status/1070324174286012416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 670,
+							"h": 557,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 670,
+							"h": 557,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 670,
+							"h": 557,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1070323841061204000,
+					"id_str": "1070323841061203968",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/DtqN3NUVsAALhaZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtqN3NUVsAALhaZ.jpg",
+					"url": "https://t.co/Lyt6iyyaqW",
+					"display_url": "pic.twitter.com/Lyt6iyyaqW",
+					"expanded_url": "https://twitter.com/JohnOCAP/status/1070324174286012416/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 670,
+							"h": 557,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 670,
+							"h": 557,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 670,
+							"h": 557,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1013808600,
+			"id_str": "1013808600",
+			"name": "John Clarke",
+			"screen_name": "JohnOCAP",
+			"location": "Toronto",
+			"description": "Ontario Coalition Against Poverty (OCAP) Organizer, John Clarke.  RTs can mean anything from solidarity to hostility. \r\nI don't debate racists and bigots.",
+			"url": "http://t.co/HBGiwcGOOq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/HBGiwcGOOq",
+							"expanded_url": "http://www.ocap.ca",
+							"display_url": "ocap.ca",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 6873,
 			"friends_count": 5298,
 			"listed_count": 268,
