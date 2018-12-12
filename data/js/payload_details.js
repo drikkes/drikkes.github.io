e674dd8a356e6bca61d50882a30dcc90e418d754
-drik S <drikkes@gmail.com>
2018-12-12 11:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64388,
-	"created_at": "Wed Dec 12 09:11:18 +0000 2018",
+	"tweets": 64389,
+	"created_at": "Wed Dec 12 10:11:19 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Mon Dec 03 15:52:35 +0000 2018",
+		"id": 1069620421417467900,
+		"id_str": "1069620421417467906",
+		"full_text": "Theodor Fontane, Popliterat https://t.co/4cVcvojJYv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			27
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1069620416564654100,
+					"id_str": "1069620416564654080",
+					"indices": [
+						28,
+						51
+					],
+					"media_url": "http://pbs.twimg.com/media/DtgOGg3XcAA8-JA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtgOGg3XcAA8-JA.jpg",
+					"url": "https://t.co/4cVcvojJYv",
+					"display_url": "pic.twitter.com/4cVcvojJYv",
+					"expanded_url": "https://twitter.com/fxstpn/status/1069620421417467906/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 333,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 588,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1069620416564654100,
+					"id_str": "1069620416564654080",
+					"indices": [
+						28,
+						51
+					],
+					"media_url": "http://pbs.twimg.com/media/DtgOGg3XcAA8-JA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtgOGg3XcAA8-JA.jpg",
+					"url": "https://t.co/4cVcvojJYv",
+					"display_url": "pic.twitter.com/4cVcvojJYv",
+					"expanded_url": "https://twitter.com/fxstpn/status/1069620421417467906/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 333,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1004,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 588,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 399252592,
+			"id_str": "399252592",
+			"name": "Felix Stephan",
+			"screen_name": "fxstpn",
+			"location": "Berlin",
+			"description": "Süddeutsche Zeitung - Literatur",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 980,
+			"friends_count": 1209,
+			"listed_count": 35,
+			"created_at": "Thu Oct 27 06:56:40 +0000 2011",
+			"favourites_count": 743,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 646,
+			"lang": "de",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/993947195450765312/SEO3Fthy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/993947195450765312/SEO3Fthy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/399252592/1445168398",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ro",
+		"text": "Theodor Fontane, Popliterat https://t.co/4cVcvojJYv"
+	},
+	{
 		"created_at": "Wed Dec 12 08:58:55 +0000 2018",
 		"id": 1072777810652090400,
 		"id_str": "1072777810652090373",
