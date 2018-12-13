d12ad7bb5657c4ca825ad5fe1e1c7d7f0ea3fa8e
-drik S <drikkes@gmail.com>
2018-12-13 21:11:20 +0100

    Added 1 tweets

--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,356 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Thu Dec 13 19:56:52 +0000 2018",
+		"id": 1073305776540803100,
+		"id_str": "1073305776540803072",
+		"full_text": "Auf meiner Unterhose sind HotDogs. Und jetzt? https://t.co/6cdhQAD6VC https://t.co/RnBRgAPsa3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6cdhQAD6VC",
+					"expanded_url": "https://twitter.com/RedTube/status/903300164483416069",
+					"display_url": "twitter.com/RedTube/status…",
+					"indices": [
+						46,
+						69
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1073305766306701300,
+					"id_str": "1073305766306701312",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/DuUl6GgW4AAMZ8N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DuUl6GgW4AAMZ8N.jpg",
+					"url": "https://t.co/RnBRgAPsa3",
+					"display_url": "pic.twitter.com/RnBRgAPsa3",
+					"expanded_url": "https://twitter.com/drikkes/status/1073305776540803072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1151,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1073305766306701300,
+					"id_str": "1073305766306701312",
+					"indices": [
+						70,
+						93
+					],
+					"media_url": "http://pbs.twimg.com/media/DuUl6GgW4AAMZ8N.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DuUl6GgW4AAMZ8N.jpg",
+					"url": "https://t.co/RnBRgAPsa3",
+					"display_url": "pic.twitter.com/RnBRgAPsa3",
+					"expanded_url": "https://twitter.com/drikkes/status/1073305776540803072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1151,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 674,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 851,
+			"friends_count": 159,
+			"listed_count": 82,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4543,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18147,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91712869,
+				6.96591932
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96591932,
+				50.91712869
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 903300164483416000,
+		"quoted_status_id_str": "903300164483416069",
+		"quoted_status_permalink": {
+			"url": "https://t.co/6cdhQAD6VC",
+			"expanded": "https://twitter.com/RedTube/status/903300164483416069",
+			"display": "twitter.com/RedTube/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Aug 31 16:55:16 +0000 2017",
+			"id": 903300164483416000,
+			"id_str": "903300164483416069",
+			"full_text": "Your Pornstar name is the color of your underwear and the last thing you ate.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				77
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://www.hootsuite.com\" rel=\"nofollow\">Hootsuite</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 148824710,
+				"id_str": "148824710",
+				"name": "RedTube Emma",
+				"screen_name": "RedTube",
+				"location": "DM for Snapchat Takeover Inquiries",
+				"description": "Account run by Emma&emsp;&emsp;&emsp; &emsp;&emsp;&emsp;&emsp;&emsp;IG: RedTube.Official &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;RTVerified👻https://t.co/7qr4ShaG6y",
+				"url": "https://t.co/lYcwvXc0Ik",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lYcwvXc0Ik",
+								"expanded_url": "http://redtube.com",
+								"display_url": "redtube.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/7qr4ShaG6y",
+								"expanded_url": "http://snapchat.com/add/rtverified",
+								"display_url": "snapchat.com/add/rtverified",
+								"indices": [
+									136,
+									159
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 272471,
+				"friends_count": 4373,
+				"listed_count": 1117,
+				"created_at": "Thu May 27 18:04:51 +0000 2010",
+				"favourites_count": 8666,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 54213,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1D1D1D",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/877893072729845761/dOE9f-Hy_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/877893072729845761/dOE9f-Hy_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/148824710/1509130049",
+				"profile_link_color": "AD151D",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 312,
+			"favorite_count": 1056,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Auf meiner Unterhose sind HotDogs. Und jetzt? https://t.co/6cdhQAD6VC https://t.co/RnBRgAPsa3"
+	},
+	{
 		"created_at": "Thu Dec 13 16:39:31 +0000 2018",
 		"id": 1073256109450948600,
 		"id_str": "1073256109450948611",
