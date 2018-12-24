b6a0eca9a2574acdfc8d73a7203c6ee6a3e7159e
-drik S <drikkes@gmail.com>
2018-12-24 23:11:20 +0100

    Added 4 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 211,
+		"tweet_count": 215,
 		"month": 12
 	},
 	{
