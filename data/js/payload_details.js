082555964edd0b07bd3f939ed5b1bde90a42873d
-drik S <drikkes@gmail.com>
2019-02-01 08:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 64726,
-	"created_at": "Thu Jan 31 19:11:20 +0000 2019",
+	"tweets": 64727,
+	"created_at": "Fri Feb 01 07:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2019_02.js",
+		"year": 2019,
+		"var_name": "tweets_2019_02",
+		"tweet_count": 1,
+		"month": 2
+	},
+	{
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2019,
 		"var_name": "tweets_2019_01",
