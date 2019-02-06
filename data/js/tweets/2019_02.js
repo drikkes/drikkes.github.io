5faa1b00517f2a9bf6c7292234f96a35fbb76904
-drik S <drikkes@gmail.com>
2019-02-06 09:11:21 +0100

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 05 22:35:51 +0000 2019",
+		"id": 1092914729918623700,
+		"id_str": "1093046432632852480",
+		"full_text": "Über ein dunkles Kapitel. Als die Hölle aufging. https://t.co/oZ0vqj0dEA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oZ0vqj0dEA",
+					"expanded_url": "http://www.beimnollar.de/2019/02/05/als-die-holle-aufging/",
+					"display_url": "beimnollar.de/2019/02/05/als…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 71992998,
+			"id_str": "71992998",
+			"name": "Stephan Noller",
+			"screen_name": "holadiho",
+			"location": "Cologne/Germany",
+			"description": "The armies of those I love engirth me, and I engirth them; #ubirch #calliope #trackle #D64 #bvdw",
+			"url": "https://t.co/w5szGzHu2G",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/w5szGzHu2G",
+							"expanded_url": "http://stephannoller.eu",
+							"display_url": "stephannoller.eu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2725,
+			"friends_count": 2143,
+			"listed_count": 205,
+			"created_at": "Sun Sep 06 07:56:17 +0000 2009",
+			"favourites_count": 13412,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 34185,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1044460234171961344/lBm7auIM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1044460234171961344/lBm7auIM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/71992998/1408024913",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Über ein dunkles Kapitel. Als die Hölle aufging. https://t.co/oZ0vqj0dEA"
+	},
+	{
 		"created_at": "Tue Feb 05 16:10:44 +0000 2019",
 		"id": 1092817813297860600,
 		"id_str": "1092817813297860608",
