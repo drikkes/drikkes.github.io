8917b8079ae40645ad46ab7aeaf30158e25a96ad
-drik S <drikkes@gmail.com>
2019-02-26 12:11:19 +0100

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 26 11:00:33 +0000 2019",
+		"id": 1100349897256112100,
+		"id_str": "1100349897256112128",
+		"full_text": "Freue mich schon auf die nächsten Artikel von @SPIEGELONLINE: „Ex-Chefarzt warnt OP-Assistenten vor blutigen Aussichten“ und „Ex-Möbelpacker warnt neue Kollegen vor tragfähigem Job“. https://t.co/o5sOeti1Xa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SPIEGELONLINE",
+					"name": "SPIEGEL ONLINE",
+					"id": 2834511,
+					"id_str": "2834511",
+					"indices": [
+						46,
+						60
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1100349892805963800,
+					"id_str": "1100349892805963776",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/D0U6ZclWsAAQPBm.png",
+					"media_url_https": "https://pbs.twimg.com/media/D0U6ZclWsAAQPBm.png",
+					"url": "https://t.co/o5sOeti1Xa",
+					"display_url": "pic.twitter.com/o5sOeti1Xa",
+					"expanded_url": "https://twitter.com/drikkes/status/1100349897256112128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 510,
+							"h": 105,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 105,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 105,
+							"h": 105,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 510,
+							"h": 105,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1100349892805963800,
+					"id_str": "1100349892805963776",
+					"indices": [
+						183,
+						206
+					],
+					"media_url": "http://pbs.twimg.com/media/D0U6ZclWsAAQPBm.png",
+					"media_url_https": "https://pbs.twimg.com/media/D0U6ZclWsAAQPBm.png",
+					"url": "https://t.co/o5sOeti1Xa",
+					"display_url": "pic.twitter.com/o5sOeti1Xa",
+					"expanded_url": "https://twitter.com/drikkes/status/1100349897256112128/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 510,
+							"h": 105,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 105,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 105,
+							"h": 105,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 510,
+							"h": 105,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 850,
+			"friends_count": 158,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4644,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18422,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0ea0e665161844a4",
+			"url": "https://api.twitter.com/1.1/geo/id/0ea0e665161844a4.json",
+			"place_type": "neighborhood",
+			"name": "Pempelfort",
+			"full_name": "Pempelfort, Düsseldorf",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.765948,
+							51.228052
+						],
+						[
+							6.798002,
+							51.228052
+						],
+						[
+							6.798002,
+							51.243446
+						],
+						[
+							6.765948,
+							51.243446
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Freue mich schon auf die nächsten Artikel von @SPIEGELONLINE: „Ex-Chefarzt warnt OP-Assistenten vor blutigen Aussichten“ und „Ex-Möbelpacker warnt neue Kollegen vor tragfähigem Job“. https://t.co/o5sOeti1Xa"
+	},
+	{
 		"created_at": "Tue Feb 26 08:00:00 +0000 2019",
 		"id": 1100304460713246700,
 		"id_str": "1100304460713246720",
