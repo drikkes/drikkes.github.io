7906b5e0a77a4214fa2859027485444e19cd5bbd
-drik S <drikkes@gmail.com>
2019-05-01 11:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65028,
-	"created_at": "Tue Apr 30 15:11:18 +0000 2019",
+	"tweets": 65029,
+	"created_at": "Wed May 01 09:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2019_05.js",
+		"year": 2019,
+		"var_name": "tweets_2019_05",
+		"tweet_count": 1,
+		"month": 5
+	},
+	{
 		"file_name": "data/js/tweets/2019_04.js",
 		"year": 2019,
 		"var_name": "tweets_2019_04",
