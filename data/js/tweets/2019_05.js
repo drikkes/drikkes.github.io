ee5c36e045d5717de7a8b20df3c5d82e455f464e
-drik S <drikkes@gmail.com>
2019-05-05 20:11:19 +0200

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 05 17:11:30 +0000 2019",
+		"id": 1125061275074093000,
+		"id_str": "1125085623566635008",
+		"full_text": "https://t.co/98U1KqBDZ3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1125061252261326800,
+					"id_str": "1125061252261326853",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D50FP4OXsAUu9Qo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D50FP4OXsAUu9Qo.jpg",
+					"url": "https://t.co/98U1KqBDZ3",
+					"display_url": "pic.twitter.com/98U1KqBDZ3",
+					"expanded_url": "https://twitter.com/arambartholl/status/1125061275074093062/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1125061252261326800,
+					"id_str": "1125061252261326853",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D50FP4OXsAUu9Qo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D50FP4OXsAUu9Qo.jpg",
+					"url": "https://t.co/98U1KqBDZ3",
+					"display_url": "pic.twitter.com/98U1KqBDZ3",
+					"expanded_url": "https://twitter.com/arambartholl/status/1125061275074093062/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 5375902,
+			"id_str": "5375902",
+			"name": "Ar@m B@rtholl",
+			"screen_name": "arambartholl",
+			"location": "Berlin",
+			"description": "",
+			"url": "https://t.co/Y66cDmCiCA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Y66cDmCiCA",
+							"expanded_url": "http://arambartholl.com",
+							"display_url": "arambartholl.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7703,
+			"friends_count": 1947,
+			"listed_count": 344,
+			"created_at": "Sat Apr 21 09:05:45 +0000 2007",
+			"favourites_count": 5479,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18592,
+			"lang": "de",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/946607353/gmaps-avatar_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/946607353/gmaps-avatar_normal.jpeg",
+			"profile_link_color": "787878",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D4DBEE",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 16,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/98U1KqBDZ3"
+	},
+	{
 		"created_at": "Sun May 05 16:32:43 +0000 2019",
 		"id": 1125075865824649200,
 		"id_str": "1125075865824649216",
