e060d322cbbdd7abafbde8d1955b0d4e68e45038
-drik S <drikkes@gmail.com>
2019-05-13 14:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65066,
-	"created_at": "Mon May 13 11:11:19 +0000 2019",
+	"tweets": 65067,
+	"created_at": "Mon May 13 12:11:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 13 11:50:20 +0000 2019",
+		"id": 1127903901167030300,
+		"id_str": "1127903901167030274",
+		"full_text": "Since @GIPHY can't create animated GIFs out of most @LastWeekTonight episodes any more, maybe @iamjohnoliver and @HBO should rethink their @YouTube video length. https://t.co/jguFkFYpxr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			161
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GIPHY",
+					"name": "GIPHY",
+					"id": 1020383864,
+					"id_str": "1020383864",
+					"indices": [
+						6,
+						12
+					]
+				},
+				{
+					"screen_name": "LastWeekTonight",
+					"name": "Last Week Tonight",
+					"id": 2317351705,
+					"id_str": "2317351705",
+					"indices": [
+						52,
+						68
+					]
+				},
+				{
+					"screen_name": "iamjohnoliver",
+					"name": "John Oliver",
+					"id": 316389142,
+					"id_str": "316389142",
+					"indices": [
+						94,
+						108
+					]
+				},
+				{
+					"screen_name": "HBO",
+					"name": "HBO",
+					"id": 15635604,
+					"id_str": "15635604",
+					"indices": [
+						113,
+						117
+					]
+				},
+				{
+					"screen_name": "YouTube",
+					"name": "YouTube",
+					"id": 10228272,
+					"id_str": "10228272",
+					"indices": [
+						139,
+						147
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1127903896071024600,
+					"id_str": "1127903896071024640",
+					"indices": [
+						162,
+						185
+					],
+					"media_url": "http://pbs.twimg.com/media/D6cenhoXsAAs3YX.png",
+					"media_url_https": "https://pbs.twimg.com/media/D6cenhoXsAAs3YX.png",
+					"url": "https://t.co/jguFkFYpxr",
+					"display_url": "pic.twitter.com/jguFkFYpxr",
+					"expanded_url": "https://twitter.com/drikkes/status/1127903901167030274/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 327,
+							"h": 42,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 327,
+							"h": 42,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 327,
+							"h": 42,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 42,
+							"h": 42,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1127903896071024600,
+					"id_str": "1127903896071024640",
+					"indices": [
+						162,
+						185
+					],
+					"media_url": "http://pbs.twimg.com/media/D6cenhoXsAAs3YX.png",
+					"media_url_https": "https://pbs.twimg.com/media/D6cenhoXsAAs3YX.png",
+					"url": "https://t.co/jguFkFYpxr",
+					"display_url": "pic.twitter.com/jguFkFYpxr",
+					"expanded_url": "https://twitter.com/drikkes/status/1127903901167030274/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 327,
+							"h": 42,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 327,
+							"h": 42,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 327,
+							"h": 42,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 42,
+							"h": 42,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 849,
+			"friends_count": 158,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4698,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18692,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Since @GIPHY can't create animated GIFs out of most @LastWeekTonight episodes any more, maybe @iamjohnoliver and @HBO should rethink their @YouTube video length. https://t.co/jguFkFYpxr"
+	},
+	{
 		"created_at": "Mon May 13 10:32:45 +0000 2019",
 		"id": 1127884378007330800,
 		"id_str": "1127884378007330817",
