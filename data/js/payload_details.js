9ae4dc6df2e4b6bcf2ee2138b2d57c252a84cab0
-drik S <drikkes@gmail.com>
2019-07-02 12:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65220,
-	"created_at": "Tue Jul 02 08:11:18 +0000 2019",
+	"tweets": 65221,
+	"created_at": "Tue Jul 02 10:11:17 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Tue Jul 02 09:28:30 +0000 2019",
+		"id": 1145952100905566200,
+		"id_str": "1145987603197497344",
+		"full_text": "Boykottaufrufe bescheren #Hartaberfair die beste Quote 14-49 in diesem Jahr. https://t.co/Ig8x5SxJqZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Hartaberfair",
+					"indices": [
+						25,
+						38
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ig8x5SxJqZ",
+					"expanded_url": "http://www.quotenmeter.de/n/110429/boykottaufrufe-haben-nichts-gebracht-hart-aber-fair-holt-bei-den-jungen-einen-jahresrekord",
+					"display_url": "quotenmeter.de/n/110429/boyko…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 40934492,
+			"id_str": "40934492",
+			"name": "Quotenmeter",
+			"screen_name": "quotenmeter",
+			"location": "Germany",
+			"description": "Die Redaktion des Medienmagazins Quotenmeter versorgt euch mit brandneuen Nachrichten & Aktionen aus unserem Loft!",
+			"url": "http://t.co/SlsgPbko9e",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/SlsgPbko9e",
+							"expanded_url": "http://www.quotenmeter.de",
+							"display_url": "quotenmeter.de",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 25340,
+			"friends_count": 150,
+			"listed_count": 450,
+			"created_at": "Mon May 18 18:34:36 +0000 2009",
+			"favourites_count": 727,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53014,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FEFEFE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1061920791472816128/KJ8AvSxJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1061920791472816128/KJ8AvSxJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/40934492/1542016803",
+			"profile_link_color": "2A64A4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E9E9E9",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Boykottaufrufe bescheren #Hartaberfair die beste Quote 14-49 in diesem Jahr. https://t.co/Ig8x5SxJqZ"
+	},
+	{
 		"created_at": "Tue Jul 02 07:43:44 +0000 2019",
 		"id": 1145961236678533100,
 		"id_str": "1145961236678533121",
