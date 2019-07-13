3909d684f2f34d8d0130cc13045e408270921b5a
-drik S <drikkes@gmail.com>
2019-07-13 20:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65261,
-	"created_at": "Sat Jul 13 17:11:18 +0000 2019",
+	"tweets": 65262,
+	"created_at": "Sat Jul 13 18:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sat Jul 13 17:35:04 +0000 2019",
+		"id": 1150001403374571500,
+		"id_str": "1150096319438041091",
+		"full_text": "Wie der Streit zwischen Kaiser-Ururenkel und Bund eskalieren konnte https://t.co/SJZ19JoYxb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SJZ19JoYxb",
+					"expanded_url": "https://www.tagesspiegel.de/berlin/eigentumsansprueche-auf-tausende-bedeutsame-kunstwerke-wie-der-streit-zwischen-kaiser-ururenkel-und-bund-eskalieren-konnte/24588740.html",
+					"display_url": "tagesspiegel.de/berlin/eigentu…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 118341985,
+			"id_str": "118341985",
+			"name": "Svenja · Castleholic · Luxarazzi",
+			"screen_name": "SydneyLux",
+			"location": "www.luxarazzi.com",
+			"description": "Political travelholic. Castle hunter. History nerd. Royal watcher. Member of Team @Luxarazzi. My views and castle adventures at https://t.co/eZoUyU7q09.",
+			"url": "https://t.co/fuzsPzk1SC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/fuzsPzk1SC",
+							"expanded_url": "http://www.castleholic.com",
+							"display_url": "castleholic.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/eZoUyU7q09",
+							"expanded_url": "http://Castleholic.com",
+							"display_url": "Castleholic.com",
+							"indices": [
+								128,
+								151
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2155,
+			"friends_count": 280,
+			"listed_count": 38,
+			"created_at": "Sun Feb 28 10:49:19 +0000 2010",
+			"favourites_count": 4979,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16419,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1011657333406076929/q3kVxOsi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1011657333406076929/q3kVxOsi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/118341985/1530728340",
+			"profile_link_color": "990000",
+			"profile_sidebar_border_color": "DFDFDF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Wie der Streit zwischen Kaiser-Ururenkel und Bund eskalieren konnte https://t.co/SJZ19JoYxb"
+	},
+	{
 		"created_at": "Sat Jul 13 16:29:21 +0000 2019",
 		"id": 1150079780517220400,
 		"id_str": "1150079780517220352",
