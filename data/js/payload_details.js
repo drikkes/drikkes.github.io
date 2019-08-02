fc8d23a75258fef173a042d11edd3854e603cdf3
-drik S <drikkes@gmail.com>
2019-08-02 20:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65330,
-	"created_at": "Fri Aug 02 14:11:17 +0000 2019",
+	"tweets": 65331,
+	"created_at": "Fri Aug 02 18:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 02 17:37:01 +0000 2019",
+		"id": 1157301883805024300,
+		"id_str": "1157344563889135621",
+		"full_text": "Saxe appeal: king of Belgium reintroduces German lineage https://t.co/9m9QzyvYzM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9m9QzyvYzM",
+					"expanded_url": "https://www.theguardian.com/world/2019/aug/02/saxe-appeal-king-of-belgium-reintroduces-german-lineage?CMP=Share_iOSApp_Other",
+					"display_url": "theguardian.com/world/2019/aug…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 147172003,
+			"id_str": "147172003",
+			"name": "Mo",
+			"screen_name": "HistoryRoyalWmn",
+			"location": "Netherlands",
+			"description": "Owner of History of Royal Women and Assistant Editor @RoyalCentral |\nAuthor of \"Carolina of Orange-Nassau\" - https://t.co/NIkXriQly2",
+			"url": "http://t.co/0Xzxn00zjR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/0Xzxn00zjR",
+							"expanded_url": "http://www.historyofroyalwomen.com",
+							"display_url": "historyofroyalwomen.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/NIkXriQly2",
+							"expanded_url": "https://amzn.to/2HE7FPh",
+							"display_url": "amzn.to/2HE7FPh",
+							"indices": [
+								109,
+								132
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2002,
+			"friends_count": 593,
+			"listed_count": 75,
+			"created_at": "Sun May 23 11:52:09 +0000 2010",
+			"favourites_count": 6278,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 46677,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "8D4FD2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1147807405171982336/SYNELcdW_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1147807405171982336/SYNELcdW_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/147172003/1553433968",
+			"profile_link_color": "0A0950",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Saxe appeal: king of Belgium reintroduces German lineage https://t.co/9m9QzyvYzM"
+	},
+	{
 		"created_at": "Fri Aug 02 13:48:40 +0000 2019",
 		"id": 1157287098191360000,
 		"id_str": "1157287098191360001",
