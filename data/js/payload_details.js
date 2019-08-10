881a403cb47452c8a66c73d5d819aa061bcec1a2
-drik S <drikkes@gmail.com>
2019-08-10 16:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65346,
-	"created_at": "Sat Aug 10 12:11:18 +0000 2019",
+	"tweets": 65347,
+	"created_at": "Sat Aug 10 14:11:17 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 10 13:51:21 +0000 2019",
+		"id": 1160186875883217000,
+		"id_str": "1160186875883216896",
+		"full_text": "@Nouveaubeton „Varus, gib mir meine Subventionen wieder!“",
+		"truncated": false,
+		"display_text_range": [
+			14,
+			57
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Nouveaubeton",
+					"name": "Nouveaubéton",
+					"id": 995015327795875800,
+					"id_str": "995015327795875842",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1160167317197721600,
+		"in_reply_to_status_id_str": "1160167317197721600",
+		"in_reply_to_user_id": 995015327795875800,
+		"in_reply_to_user_id_str": "995015327795875842",
+		"in_reply_to_screen_name": "Nouveaubeton",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 850,
+			"friends_count": 162,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4770,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 18996,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				51.36271373,
+				6.42242109
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.42242109,
+				51.36271373
+			]
+		},
+		"place": {
+			"id": "2a59892b8ee145ad",
+			"url": "https://api.twitter.com/1.1/geo/id/2a59892b8ee145ad.json",
+			"place_type": "city",
+			"name": "Kempen",
+			"full_name": "Kempen, Deutschland",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.3587914,
+							51.3306204
+						],
+						[
+							6.535521,
+							51.3306204
+						],
+						[
+							6.535521,
+							51.424948
+						],
+						[
+							6.3587914,
+							51.424948
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@Nouveaubeton „Varus, gib mir meine Subventionen wieder!“"
+	},
+	{
 		"created_at": "Sat Aug 10 12:03:51 +0000 2019",
 		"id": 1160159823301947400,
 		"id_str": "1160159823301947392",
