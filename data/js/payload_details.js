fabce9be5ea2adc777e738e07949b0404ebef05e
-drik S <drikkes@gmail.com>
2019-08-17 18:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65368,
-	"created_at": "Sat Aug 17 13:11:18 +0000 2019",
+	"tweets": 65369,
+	"created_at": "Sat Aug 17 16:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,165 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Sat Aug 17 15:57:42 +0000 2019",
+		"id": 1162755389080973300,
+		"id_str": "1162755389080973317",
+		"full_text": "@surfguard @DB_Bahn Also ich meine explizit „geändert“, nicht „umgekehrt gereiht“.",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "surfguard",
+					"name": "surfguard",
+					"id": 14296237,
+					"id_str": "14296237",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "DB_Bahn",
+					"name": "Deutsche Bahn Personenverkehr",
+					"id": 39999078,
+					"id_str": "39999078",
+					"indices": [
+						11,
+						19
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1162754413527740400,
+		"in_reply_to_status_id_str": "1162754413527740416",
+		"in_reply_to_user_id": 14296237,
+		"in_reply_to_user_id_str": "14296237",
+		"in_reply_to_screen_name": "surfguard",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 162,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4782,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19021,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "5f095b2f61761428",
+			"url": "https://api.twitter.com/1.1/geo/id/5f095b2f61761428.json",
+			"place_type": "neighborhood",
+			"name": "Brackwede",
+			"full_name": "Brackwede, Bielefeld",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							8.3776387,
+							51.944669
+						],
+						[
+							8.535376,
+							51.944669
+						],
+						[
+							8.535376,
+							52.017733
+						],
+						[
+							8.3776387,
+							52.017733
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@surfguard @DB_Bahn Also ich meine explizit „geändert“, nicht „umgekehrt gereiht“."
+	},
+	{
 		"created_at": "Sat Aug 17 12:41:16 +0000 2019",
 		"id": 1162705953571180500,
 		"id_str": "1162705953571180545",
