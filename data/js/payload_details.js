ff217a549d1aa34be7385a2ead45fbbb0de72f8b
-drik S <drikkes@gmail.com>
2019-08-21 18:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65388,
-	"created_at": "Wed Aug 21 14:11:18 +0000 2019",
+	"tweets": 65389,
+	"created_at": "Wed Aug 21 16:11:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 21 15:59:08 +0000 2019",
+		"id": 1164205302226591700,
+		"id_str": "1164205302226591744",
+		"full_text": "Twitter kennt mich einfach am besten. Alle meine Interessen mit F... https://t.co/9jbaA3NQtH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1164205300276236300,
+					"id_str": "1164205300276236288",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/ECgWjWiXkAA-1UI.png",
+					"media_url_https": "https://pbs.twimg.com/media/ECgWjWiXkAA-1UI.png",
+					"url": "https://t.co/9jbaA3NQtH",
+					"display_url": "pic.twitter.com/9jbaA3NQtH",
+					"expanded_url": "https://twitter.com/drikkes/status/1164205302226591744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 180,
+							"h": 370,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 180,
+							"h": 370,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 180,
+							"h": 370,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1164205300276236300,
+					"id_str": "1164205300276236288",
+					"indices": [
+						69,
+						92
+					],
+					"media_url": "http://pbs.twimg.com/media/ECgWjWiXkAA-1UI.png",
+					"media_url_https": "https://pbs.twimg.com/media/ECgWjWiXkAA-1UI.png",
+					"url": "https://t.co/9jbaA3NQtH",
+					"display_url": "pic.twitter.com/9jbaA3NQtH",
+					"expanded_url": "https://twitter.com/drikkes/status/1164205302226591744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 180,
+							"h": 370,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 180,
+							"h": 370,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 180,
+							"h": 370,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 162,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4784,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19044,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Twitter kennt mich einfach am besten. Alle meine Interessen mit F... https://t.co/9jbaA3NQtH"
+	},
+	{
 		"created_at": "Wed Aug 21 13:11:56 +0000 2019",
 		"id": 1164163224780050400,
 		"id_str": "1164163224780050432",
