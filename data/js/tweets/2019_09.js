a122d3a3e3c2f3c435dc22dbb1e9459163d9bad5
-drik S <drikkes@gmail.com>
2019-09-03 15:11:18 +0200

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,345 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 03 12:59:41 +0000 2019",
+		"id": 1168871184504508400,
+		"id_str": "1168871184504508416",
+		"full_text": "Während Untergebene oft meinen, sie könnten selbst in firmenweiten Verteilern den unverfrorensten Scheiß von sich geben, wenn sie ans Ende der Mail nur einen Zwinkersmiley setzen. ;) https://t.co/muyFeZMZ2x",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			182
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/muyFeZMZ2x",
+					"expanded_url": "https://twitter.com/kosmar/status/1168776338901278721",
+					"display_url": "twitter.com/kosmar/status/…",
+					"indices": [
+						183,
+						206
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 163,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4799,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19084,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0ea0e665161844a4",
+			"url": "https://api.twitter.com/1.1/geo/id/0ea0e665161844a4.json",
+			"place_type": "neighborhood",
+			"name": "Pempelfort",
+			"full_name": "Pempelfort, Düsseldorf",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.765948,
+							51.228052
+						],
+						[
+							6.798002,
+							51.228052
+						],
+						[
+							6.798002,
+							51.243446
+						],
+						[
+							6.765948,
+							51.243446
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1168776338901278700,
+		"quoted_status_id_str": "1168776338901278721",
+		"quoted_status_permalink": {
+			"url": "https://t.co/muyFeZMZ2x",
+			"expanded": "https://twitter.com/kosmar/status/1168776338901278721",
+			"display": "twitter.com/kosmar/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 03 06:42:48 +0000 2019",
+			"id": 1168776338901278700,
+			"id_str": "1168776338901278721",
+			"full_text": "🤷🏼‍♂️ https://t.co/QNfGsYRHT5 https://t.co/eujdxeeZgL",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				29
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/QNfGsYRHT5",
+						"expanded_url": "https://de.wikipedia.org/wiki/Emoji?wprov=sfia1",
+						"display_url": "de.wikipedia.org/wiki/Emoji?wpr…",
+						"indices": [
+							6,
+							29
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1168776336355332000,
+						"id_str": "1168776336355332096",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/EDhT4q5XkAAfU2H.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDhT4q5XkAAfU2H.jpg",
+						"url": "https://t.co/eujdxeeZgL",
+						"display_url": "pic.twitter.com/eujdxeeZgL",
+						"expanded_url": "https://twitter.com/kosmar/status/1168776338901278721/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1168776336355332000,
+						"id_str": "1168776336355332096",
+						"indices": [
+							30,
+							53
+						],
+						"media_url": "http://pbs.twimg.com/media/EDhT4q5XkAAfU2H.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EDhT4q5XkAAfU2H.jpg",
+						"url": "https://t.co/eujdxeeZgL",
+						"display_url": "pic.twitter.com/eujdxeeZgL",
+						"expanded_url": "https://twitter.com/kosmar/status/1168776338901278721/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 360,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1487011,
+				"id_str": "1487011",
+				"name": "kosmar hilft 🇪🇺",
+				"screen_name": "kosmar",
+				"location": "Berlin, Germany",
+				"description": "Markus Angermeier·digital native since 1998/design·frontend·ui·ux·prototypes·logos·lettering·teaching/RT≠ack·opinions are yours·parody account·lass mich",
+				"url": "https://t.co/IBUVQZep0M",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IBUVQZep0M",
+								"expanded_url": "http://kosmar.de/",
+								"display_url": "kosmar.de",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 8025,
+				"friends_count": 5420,
+				"listed_count": 500,
+				"created_at": "Mon Mar 19 10:00:56 +0000 2007",
+				"favourites_count": 16136,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 63543,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EEEEEE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/2228570189/kosmar_150_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/2228570189/kosmar_150_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1487011/1348087304",
+				"profile_link_color": "F83E00",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "EEEEEE",
+				"profile_text_color": "555555",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Während Untergebene oft meinen, sie könnten selbst in firmenweiten Verteilern den unverfrorensten Scheiß von sich geben, wenn sie ans Ende der Mail nur einen Zwinkersmiley setzen. ;) https://t.co/muyFeZMZ2x"
+	},
+	{
 		"created_at": "Tue Sep 03 09:52:42 +0000 2019",
 		"id": 1168824129677660200,
 		"id_str": "1168824129677660160",
