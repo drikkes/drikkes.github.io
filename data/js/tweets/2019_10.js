3f294ba4fae295ddebcc736a32cb46b9a9485b63
-drik S <drikkes@gmail.com>
2019-10-07 11:11:19 +0200

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,166 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Mon Oct 07 08:26:01 +0000 2019",
+		"id": 1180556700891455500,
+		"id_str": "1181123500922281984",
+		"full_text": "The year is 1986. The sign on Maradona's shirt says \"No drugs\" , on Platini's \"No to corruption\" 🤣 https://t.co/8VTJosPxZL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1180556695489122300,
+					"id_str": "1180556695489122305",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/EGIuD0fWkAEqrT8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGIuD0fWkAEqrT8.jpg",
+					"url": "https://t.co/8VTJosPxZL",
+					"display_url": "pic.twitter.com/8VTJosPxZL",
+					"expanded_url": "https://twitter.com/minimal1023/status/1180556700891455490/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 631,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 631,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 631,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1180556695489122300,
+					"id_str": "1180556695489122305",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/EGIuD0fWkAEqrT8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGIuD0fWkAEqrT8.jpg",
+					"url": "https://t.co/8VTJosPxZL",
+					"display_url": "pic.twitter.com/8VTJosPxZL",
+					"expanded_url": "https://twitter.com/minimal1023/status/1180556700891455490/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 631,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 631,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 631,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 113381627,
+			"id_str": "113381627",
+			"name": "Rumen Tabakov",
+			"screen_name": "minimal1023",
+			"location": "Bulgaria",
+			"description": "Liverpool supporter🇧🇬🇧🇬🇧🇬! YNWA JFT96",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 308,
+			"friends_count": 713,
+			"listed_count": 0,
+			"created_at": "Thu Feb 11 15:39:37 +0000 2010",
+			"favourites_count": 160,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 112,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1281823359/63505_178805465472638_100000295586885_555406_5781127_n_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1281823359/63505_178805465472638_100000295586885_555406_5781127_n_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 30721,
+		"favorite_count": 92320,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The year is 1986. The sign on Maradona's shirt says \"No drugs\" , on Platini's \"No to corruption\" 🤣 https://t.co/8VTJosPxZL"
+	},
+	{
 		"created_at": "Mon Oct 07 06:27:23 +0000 2019",
 		"id": 1181093646331994000,
 		"id_str": "1181093646331994112",
