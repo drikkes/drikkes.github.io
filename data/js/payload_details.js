1c524932feaccc310de66c14bbb0dd42400b2dcb
-drik S <drikkes@gmail.com>
2019-10-28 00:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65590,
-	"created_at": "Sun Oct 27 19:11:19 +0000 2019",
+	"tweets": 65591,
+	"created_at": "Sun Oct 27 23:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,335 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sun Oct 27 23:07:12 +0000 2019",
+		"id": 1188593013498491000,
+		"id_str": "1188593013498490880",
+		"full_text": "Außer Spesen… https://t.co/ylQWdqtZMR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ylQWdqtZMR",
+					"expanded_url": "https://twitter.com/MeredithFrost/status/1188473393441595395",
+					"display_url": "twitter.com/MeredithFrost/…",
+					"indices": [
+						14,
+						37
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 849,
+			"friends_count": 159,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4862,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19262,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "255115bcb65f5da6",
+			"url": "https://api.twitter.com/1.1/geo/id/255115bcb65f5da6.json",
+			"place_type": "neighborhood",
+			"name": "Bayenthal",
+			"full_name": "Bayenthal, Köln",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.960374,
+							50.903919
+						],
+						[
+							6.979611,
+							50.903919
+						],
+						[
+							6.979611,
+							50.917496
+						],
+						[
+							6.960374,
+							50.917496
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1188473393441595400,
+		"quoted_status_id_str": "1188473393441595395",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ylQWdqtZMR",
+			"expanded": "https://twitter.com/MeredithFrost/status/1188473393441595395",
+			"display": "twitter.com/MeredithFrost/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Oct 27 15:11:52 +0000 2019",
+			"id": 1188473393441595400,
+			"id_str": "1188473393441595395",
+			"full_text": "Hunter S. Thompson attempting to justify his Fear &amp; Loathing expenses to his editor https://t.co/ZKJlXWh8F4",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				87
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1188473389817708500,
+						"id_str": "1188473389817708546",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/EH5OQFEWwAIsp6I.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EH5OQFEWwAIsp6I.jpg",
+						"url": "https://t.co/ZKJlXWh8F4",
+						"display_url": "pic.twitter.com/ZKJlXWh8F4",
+						"expanded_url": "https://twitter.com/MeredithFrost/status/1188473393441595395/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 445,
+								"h": 790,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 445,
+								"h": 790,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1188473389817708500,
+						"id_str": "1188473389817708546",
+						"indices": [
+							88,
+							111
+						],
+						"media_url": "http://pbs.twimg.com/media/EH5OQFEWwAIsp6I.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EH5OQFEWwAIsp6I.jpg",
+						"url": "https://t.co/ZKJlXWh8F4",
+						"display_url": "pic.twitter.com/ZKJlXWh8F4",
+						"expanded_url": "https://twitter.com/MeredithFrost/status/1188473393441595395/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 445,
+								"h": 790,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 445,
+								"h": 790,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 26659703,
+				"id_str": "26659703",
+				"name": "Meredith Frost",
+				"screen_name": "MeredithFrost",
+				"location": "New York, NY",
+				"description": "Producer. Photographer. @Nightline @ABC News alum. My favorite superhero is Lois Lane.",
+				"url": "https://t.co/uXQ21dubl3",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/uXQ21dubl3",
+								"expanded_url": "http://instagram.com/meredithfrost",
+								"display_url": "instagram.com/meredithfrost",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 155503,
+				"friends_count": 248,
+				"listed_count": 1727,
+				"created_at": "Thu Mar 26 01:55:43 +0000 2009",
+				"favourites_count": 85015,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 21773,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "5B5D63",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1170444233486098443/i4jCNpJM_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1170444233486098443/i4jCNpJM_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/26659703/1540225151",
+				"profile_link_color": "C90606",
+				"profile_sidebar_border_color": "09383B",
+				"profile_sidebar_fill_color": "777E85",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 198,
+			"favorite_count": 986,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Außer Spesen… https://t.co/ylQWdqtZMR"
+	},
+	{
 		"created_at": "Sun Oct 27 18:12:31 +0000 2019",
 		"id": 1188518855670452200,
 		"id_str": "1188518855670452226",
