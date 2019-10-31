708dde985115352b22b142e200df845ca21fc188
-drik S <drikkes@gmail.com>
2019-10-31 17:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65605,
-	"created_at": "Thu Oct 31 12:11:18 +0000 2019",
+	"tweets": 65606,
+	"created_at": "Thu Oct 31 16:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 31 15:33:16 +0000 2019",
+		"id": 1189928332386865200,
+		"id_str": "1189928332386865153",
+		"full_text": "Word. https://t.co/Gfw18jz9E5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			5
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1189928330591645700,
+					"id_str": "1189928330591645699",
+					"indices": [
+						6,
+						29
+					],
+					"media_url": "http://pbs.twimg.com/media/EIN5gx0WsAMo7vu.png",
+					"media_url_https": "https://pbs.twimg.com/media/EIN5gx0WsAMo7vu.png",
+					"url": "https://t.co/Gfw18jz9E5",
+					"display_url": "pic.twitter.com/Gfw18jz9E5",
+					"expanded_url": "https://twitter.com/drikkes/status/1189928332386865153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 165,
+							"h": 55,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 165,
+							"h": 55,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 165,
+							"h": 55,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 55,
+							"h": 55,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1189928330591645700,
+					"id_str": "1189928330591645699",
+					"indices": [
+						6,
+						29
+					],
+					"media_url": "http://pbs.twimg.com/media/EIN5gx0WsAMo7vu.png",
+					"media_url_https": "https://pbs.twimg.com/media/EIN5gx0WsAMo7vu.png",
+					"url": "https://t.co/Gfw18jz9E5",
+					"display_url": "pic.twitter.com/Gfw18jz9E5",
+					"expanded_url": "https://twitter.com/drikkes/status/1189928332386865153/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 165,
+							"h": 55,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 165,
+							"h": 55,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 165,
+							"h": 55,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 55,
+							"h": 55,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 850,
+			"friends_count": 159,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4869,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19280,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Word. https://t.co/Gfw18jz9E5"
+	},
+	{
 		"created_at": "Thu Oct 31 12:09:28 +0000 2019",
 		"id": 1189877041753206800,
 		"id_str": "1189877041753206784",
