11010321c0d409e86f1adc79e98b00bb0f8ff4cf
-drik S <drikkes@gmail.com>
2019-11-21 10:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65649,
-	"created_at": "Thu Nov 21 07:11:18 +0000 2019",
+	"tweets": 65650,
+	"created_at": "Thu Nov 21 09:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Thu Nov 21 08:50:50 +0000 2019",
+		"id": 1197437201073746000,
+		"id_str": "1197437201073745921",
+		"full_text": "Hello @pubpxp! https://t.co/CEqq8s6WVU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "pubpxp",
+					"name": "Publicis Pixelpark",
+					"id": 38268583,
+					"id_str": "38268583",
+					"indices": [
+						6,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1197437199299547100,
+					"id_str": "1197437199299547137",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ4mydkWkAEtfjt.png",
+					"media_url_https": "https://pbs.twimg.com/media/EJ4mydkWkAEtfjt.png",
+					"url": "https://t.co/CEqq8s6WVU",
+					"display_url": "pic.twitter.com/CEqq8s6WVU",
+					"expanded_url": "https://twitter.com/drikkes/status/1197437201073745921/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 160,
+							"h": 75,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 160,
+							"h": 75,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 160,
+							"h": 75,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 75,
+							"h": 75,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1197437199299547100,
+					"id_str": "1197437199299547137",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/EJ4mydkWkAEtfjt.png",
+					"media_url_https": "https://pbs.twimg.com/media/EJ4mydkWkAEtfjt.png",
+					"url": "https://t.co/CEqq8s6WVU",
+					"display_url": "pic.twitter.com/CEqq8s6WVU",
+					"expanded_url": "https://twitter.com/drikkes/status/1197437201073745921/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 160,
+							"h": 75,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 160,
+							"h": 75,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 160,
+							"h": 75,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 75,
+							"h": 75,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 851,
+			"friends_count": 161,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4893,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19329,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hello @pubpxp! https://t.co/CEqq8s6WVU"
+	},
+	{
 		"created_at": "Thu Nov 21 06:23:06 +0000 2019",
 		"id": 1197400020561154000,
 		"id_str": "1197400020561154049",
