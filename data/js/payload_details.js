503ac2e74a0a9b55747b79ac7ec0565377d66d47
-drik S <drikkes@gmail.com>
2019-12-01 02:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65677,
-	"created_at": "Sat Nov 30 23:11:19 +0000 2019",
+	"tweets": 65678,
+	"created_at": "Sun Dec 01 01:11:17 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,653 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 00:48:12 +0000 2019",
+		"id": 1200939620680323000,
+		"id_str": "1200939620680323074",
+		"full_text": "Die DS über Schönheitschirurg*innen im dt. @ELLEmagazine Dez. „eignet sich gut für Einsteiger“, „kürzere Ausfallzeiten“ etc. https://t.co/dsCYz0LY1t",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ELLEmagazine",
+					"name": "ELLE Magazine (US)",
+					"id": 20177423,
+					"id_str": "20177423",
+					"indices": [
+						43,
+						56
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1200939614929870800,
+					"id_str": "1200939614929870848",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EKqYN4iW4AAL_m0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKqYN4iW4AAL_m0.jpg",
+					"url": "https://t.co/dsCYz0LY1t",
+					"display_url": "pic.twitter.com/dsCYz0LY1t",
+					"expanded_url": "https://twitter.com/drikkes/status/1200939620680323074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 669,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 444,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 669,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 89,
+									"y": 41,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 286,
+									"y": 54,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 623,
+									"y": 59,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 477,
+									"y": 68,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 790,
+									"y": 79,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 184,
+									"y": 99,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 380,
+									"y": 109,
+									"h": 37,
+									"w": 37
+								},
+								{
+									"x": 716,
+									"y": 111,
+									"h": 38,
+									"w": 38
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 59,
+									"y": 27,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 189,
+									"y": 35,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 413,
+									"y": 39,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 316,
+									"y": 45,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 524,
+									"y": 52,
+									"h": 23,
+									"w": 23
+								},
+								{
+									"x": 122,
+									"y": 65,
+									"h": 27,
+									"w": 27
+								},
+								{
+									"x": 252,
+									"y": 72,
+									"h": 24,
+									"w": 24
+								},
+								{
+									"x": 475,
+									"y": 73,
+									"h": 25,
+									"w": 25
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 89,
+									"y": 41,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 286,
+									"y": 54,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 623,
+									"y": 59,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 477,
+									"y": 68,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 790,
+									"y": 79,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 184,
+									"y": 99,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 380,
+									"y": 109,
+									"h": 37,
+									"w": 37
+								},
+								{
+									"x": 716,
+									"y": 111,
+									"h": 38,
+									"w": 38
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 89,
+									"y": 41,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 286,
+									"y": 54,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 623,
+									"y": 59,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 477,
+									"y": 68,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 790,
+									"y": 79,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 184,
+									"y": 99,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 380,
+									"y": 109,
+									"h": 37,
+									"w": 37
+								},
+								{
+									"x": 716,
+									"y": 111,
+									"h": 38,
+									"w": 38
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1200939614929870800,
+					"id_str": "1200939614929870848",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EKqYN4iW4AAL_m0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKqYN4iW4AAL_m0.jpg",
+					"url": "https://t.co/dsCYz0LY1t",
+					"display_url": "pic.twitter.com/dsCYz0LY1t",
+					"expanded_url": "https://twitter.com/drikkes/status/1200939620680323074/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 669,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 444,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 669,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 89,
+									"y": 41,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 286,
+									"y": 54,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 623,
+									"y": 59,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 477,
+									"y": 68,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 790,
+									"y": 79,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 184,
+									"y": 99,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 380,
+									"y": 109,
+									"h": 37,
+									"w": 37
+								},
+								{
+									"x": 716,
+									"y": 111,
+									"h": 38,
+									"w": 38
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 59,
+									"y": 27,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 189,
+									"y": 35,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 413,
+									"y": 39,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 316,
+									"y": 45,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 524,
+									"y": 52,
+									"h": 23,
+									"w": 23
+								},
+								{
+									"x": 122,
+									"y": 65,
+									"h": 27,
+									"w": 27
+								},
+								{
+									"x": 252,
+									"y": 72,
+									"h": 24,
+									"w": 24
+								},
+								{
+									"x": 475,
+									"y": 73,
+									"h": 25,
+									"w": 25
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 89,
+									"y": 41,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 286,
+									"y": 54,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 623,
+									"y": 59,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 477,
+									"y": 68,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 790,
+									"y": 79,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 184,
+									"y": 99,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 380,
+									"y": 109,
+									"h": 37,
+									"w": 37
+								},
+								{
+									"x": 716,
+									"y": 111,
+									"h": 38,
+									"w": 38
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 89,
+									"y": 41,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 286,
+									"y": 54,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 623,
+									"y": 59,
+									"h": 39,
+									"w": 39
+								},
+								{
+									"x": 477,
+									"y": 68,
+									"h": 38,
+									"w": 38
+								},
+								{
+									"x": 790,
+									"y": 79,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 184,
+									"y": 99,
+									"h": 41,
+									"w": 41
+								},
+								{
+									"x": 380,
+									"y": 109,
+									"h": 37,
+									"w": 37
+								},
+								{
+									"x": 716,
+									"y": 111,
+									"h": 38,
+									"w": 38
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 161,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4908,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19359,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "255115bcb65f5da6",
+			"url": "https://api.twitter.com/1.1/geo/id/255115bcb65f5da6.json",
+			"place_type": "neighborhood",
+			"name": "Bayenthal",
+			"full_name": "Bayenthal, Köln",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.960374,
+							50.903919
+						],
+						[
+							6.979611,
+							50.903919
+						],
+						[
+							6.979611,
+							50.917496
+						],
+						[
+							6.960374,
+							50.917496
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Die DS über Schönheitschirurg*innen im dt. @ELLEmagazine Dez. „eignet sich gut für Einsteiger“, „kürzere Ausfallzeiten“ etc. https://t.co/dsCYz0LY1t"
+	}
+]
