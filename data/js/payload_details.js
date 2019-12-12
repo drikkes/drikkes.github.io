70b7f55d4f4b896f9e9fbbd9464747fdba6c7da3
-drik S <drikkes@gmail.com>
2019-12-12 11:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65710,
-	"created_at": "Wed Dec 11 14:11:18 +0000 2019",
+	"tweets": 65711,
+	"created_at": "Thu Dec 12 10:11:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 12 09:46:12 +0000 2019",
+		"id": 1205061277690843100,
+		"id_str": "1205061277690843136",
+		"full_text": "Sehr ambitionierter Spin, Verspätungen als Feature verkaufen zu wollen, liebe Bahn. https://t.co/EKnOdFQAkc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1205061275765682200,
+					"id_str": "1205061275765682176",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/ELk82HqXsAAQm4p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELk82HqXsAAQm4p.jpg",
+					"url": "https://t.co/EKnOdFQAkc",
+					"display_url": "pic.twitter.com/EKnOdFQAkc",
+					"expanded_url": "https://twitter.com/drikkes/status/1205061277690843136/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1205061275765682200,
+					"id_str": "1205061275765682176",
+					"indices": [
+						84,
+						107
+					],
+					"media_url": "http://pbs.twimg.com/media/ELk82HqXsAAQm4p.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELk82HqXsAAQm4p.jpg",
+					"url": "https://t.co/EKnOdFQAkc",
+					"display_url": "pic.twitter.com/EKnOdFQAkc",
+					"expanded_url": "https://twitter.com/drikkes/status/1205061277690843136/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 332,
+									"y": 132,
+									"h": 36,
+									"w": 36
+								},
+								{
+									"x": 423,
+									"y": 264,
+									"h": 44,
+									"w": 44
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://drikk.es/\" rel=\"nofollow\">meinGehirn</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 851,
+			"friends_count": 161,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4915,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19395,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Sehr ambitionierter Spin, Verspätungen als Feature verkaufen zu wollen, liebe Bahn. https://t.co/EKnOdFQAkc"
+	},
+	{
 		"created_at": "Wed Dec 11 13:55:25 +0000 2019",
 		"id": 1204761609664635000,
 		"id_str": "1204761609664634880",
