25b1b51fd6778b949ca943dd43a46bd1bae7591e
-drik S <drikkes@gmail.com>
2019-12-23 15:11:19 +0100

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,382 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 23 13:47:54 +0000 2019",
+		"id": 1209108371602886700,
+		"id_str": "1209108371602886658",
+		"full_text": "Das Geld anderer Leute https://t.co/fJEj38FT02 https://t.co/hl2Ni1FO7W",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fJEj38FT02",
+					"expanded_url": "http://drikkes.com/?p=14075",
+					"display_url": "drikkes.com/?p=14075",
+					"indices": [
+						23,
+						46
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1209108366284509200,
+					"id_str": "1209108366284509184",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/EMedpytXUAAnR5C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMedpytXUAAnR5C.jpg",
+					"url": "https://t.co/hl2Ni1FO7W",
+					"display_url": "pic.twitter.com/hl2Ni1FO7W",
+					"expanded_url": "https://twitter.com/drikkes/status/1209108371602886658/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 510,
+									"y": 361,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 208,
+									"y": 443,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 401,
+									"y": 47,
+									"h": 76,
+									"w": 76
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1538,
+									"y": 1088,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 628,
+									"y": 1336,
+									"h": 142,
+									"w": 142
+								},
+								{
+									"x": 1208,
+									"y": 144,
+									"h": 230,
+									"w": 230
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 901,
+									"y": 637,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 367,
+									"y": 782,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 707,
+									"y": 84,
+									"h": 134,
+									"w": 134
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1538,
+									"y": 1088,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 628,
+									"y": 1336,
+									"h": 142,
+									"w": 142
+								},
+								{
+									"x": 1208,
+									"y": 144,
+									"h": 230,
+									"w": 230
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1209108366284509200,
+					"id_str": "1209108366284509184",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/media/EMedpytXUAAnR5C.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMedpytXUAAnR5C.jpg",
+					"url": "https://t.co/hl2Ni1FO7W",
+					"display_url": "pic.twitter.com/hl2Ni1FO7W",
+					"expanded_url": "https://twitter.com/drikkes/status/1209108371602886658/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 510,
+									"y": 361,
+									"h": 25,
+									"w": 25
+								},
+								{
+									"x": 208,
+									"y": 443,
+									"h": 47,
+									"w": 47
+								},
+								{
+									"x": 401,
+									"y": 47,
+									"h": 76,
+									"w": 76
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 1538,
+									"y": 1088,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 628,
+									"y": 1336,
+									"h": 142,
+									"w": 142
+								},
+								{
+									"x": 1208,
+									"y": 144,
+									"h": 230,
+									"w": 230
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 901,
+									"y": 637,
+									"h": 44,
+									"w": 44
+								},
+								{
+									"x": 367,
+									"y": 782,
+									"h": 83,
+									"w": 83
+								},
+								{
+									"x": 707,
+									"y": 84,
+									"h": 134,
+									"w": 134
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 1538,
+									"y": 1088,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 628,
+									"y": 1336,
+									"h": 142,
+									"w": 142
+								},
+								{
+									"x": 1208,
+									"y": 144,
+									"h": 230,
+									"w": 230
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 849,
+			"friends_count": 163,
+			"listed_count": 83,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4925,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19434,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Das Geld anderer Leute https://t.co/fJEj38FT02 https://t.co/hl2Ni1FO7W"
+	},
+	{
 		"created_at": "Sun Dec 22 18:41:55 +0000 2019",
 		"id": 1208819974590148600,
 		"id_str": "1208819974590148610",
