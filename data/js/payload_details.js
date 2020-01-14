f8d9546c0c484d8cc9969c8393f4bac83d29856a
-drik S <drikkes@gmail.com>
2020-01-14 15:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65816,
-	"created_at": "Tue Jan 14 11:11:18 +0000 2020",
+	"tweets": 65817,
+	"created_at": "Tue Jan 14 14:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Tue Jan 14 13:29:37 +0000 2020",
+		"id": 1217076301179375600,
+		"id_str": "1217076301179375616",
+		"full_text": "@jensscholz @heyhelge @BRLObeer @ihkberlin @scholzfriends „Für die Pitchpräse brauchen wir coole Layouts, freshe Köpfe und eine bold Message. Ob das nachher wirklich so umsetzbar ist, da kräht nach dem Etatgewinn kein Hahn mehr nach.“",
+		"truncated": false,
+		"display_text_range": [
+			58,
+			234
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jensscholz",
+					"name": "Jens Scholz",
+					"id": 6408332,
+					"id_str": "6408332",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "heyhelge",
+					"name": "Helge Peters",
+					"id": 1140884441730093000,
+					"id_str": "1140884441730093056",
+					"indices": [
+						12,
+						21
+					]
+				},
+				{
+					"screen_name": "BRLObeer",
+					"name": "BRLO",
+					"id": 2396345120,
+					"id_str": "2396345120",
+					"indices": [
+						22,
+						31
+					]
+				},
+				{
+					"screen_name": "ihkberlin",
+					"name": "IHK Berlin",
+					"id": 90597236,
+					"id_str": "90597236",
+					"indices": [
+						32,
+						42
+					]
+				},
+				{
+					"screen_name": "scholzfriends",
+					"name": "Scholz & Friends",
+					"id": 17522935,
+					"id_str": "17522935",
+					"indices": [
+						43,
+						57
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1217055026721566700,
+		"in_reply_to_status_id_str": "1217055026721566720",
+		"in_reply_to_user_id": 6408332,
+		"in_reply_to_user_id_str": "6408332",
+		"in_reply_to_screen_name": "jensscholz",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/7ynPrqkpaM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7ynPrqkpaM",
+							"expanded_url": "http://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 851,
+			"friends_count": 162,
+			"listed_count": 82,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4955,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19508,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1454326384",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0ea0e665161844a4",
+			"url": "https://api.twitter.com/1.1/geo/id/0ea0e665161844a4.json",
+			"place_type": "neighborhood",
+			"name": "Pempelfort",
+			"full_name": "Pempelfort, Düsseldorf",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.765948,
+							51.228052
+						],
+						[
+							6.798002,
+							51.228052
+						],
+						[
+							6.798002,
+							51.243446
+						],
+						[
+							6.765948,
+							51.243446
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@jensscholz @heyhelge @BRLObeer @ihkberlin @scholzfriends „Für die Pitchpräse brauchen wir coole Layouts, freshe Köpfe und eine bold Message. Ob das nachher wirklich so umsetzbar ist, da kräht nach dem Etatgewinn kein Hahn mehr nach.“"
+	},
+	{
 		"created_at": "Tue Jan 14 10:58:25 +0000 2020",
 		"id": 1217038252999356400,
 		"id_str": "1217038252999356417",
