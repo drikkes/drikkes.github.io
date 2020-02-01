7922a978bb6f9a2e51d39bec0b4bfabd2bcf3357
-drik S <drikkes@gmail.com>
2020-02-01 09:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65878,
-	"created_at": "Fri Jan 31 20:11:19 +0000 2020",
+	"tweets": 65879,
+	"created_at": "Sat Feb 01 08:11:18 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_02.js
@@ -0,0 +1,453 @@
+Grailbird.data.tweets_2020_02 = 
+[
+	{
+		"created_at": "Sat Feb 01 08:09:13 +0000 2020",
+		"id": 1223360386998702000,
+		"id_str": "1223518654937157632",
+		"full_text": "gucci belt has me thrown https://t.co/SFtLQNP6NE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SFtLQNP6NE",
+					"expanded_url": "https://twitter.com/GettyImagesNews/status/1223344677568634880",
+					"display_url": "twitter.com/GettyImagesNew…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 117516709,
+			"id_str": "117516709",
+			"name": "NTP",
+			"screen_name": "nathanpemberton",
+			"location": "custerdome",
+			"description": "pr intern at Fraud Guarantee",
+			"url": "https://t.co/uhgtvp0xto",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uhgtvp0xto",
+							"expanded_url": "http://ntp.fyi/",
+							"display_url": "ntp.fyi",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 902,
+			"friends_count": 623,
+			"listed_count": 26,
+			"created_at": "Thu Feb 25 20:18:59 +0000 2010",
+			"favourites_count": 5040,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4129,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/690223781701255169/NZBN3W8O_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/690223781701255169/NZBN3W8O_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/117516709/1511140263",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1223344677568635000,
+		"quoted_status_id_str": "1223344677568634880",
+		"quoted_status_permalink": {
+			"url": "https://t.co/SFtLQNP6NE",
+			"expanded": "https://twitter.com/GettyImagesNews/status/1223344677568634880",
+			"display": "twitter.com/GettyImagesNew…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jan 31 20:37:54 +0000 2020",
+			"id": 1223344677568635000,
+			"id_str": "1223344677568634880",
+			"full_text": "Gun rights activists carrying semi-automatic firearms walk through the Capitol Building on January 31, 2020 in Frankfort, Kentucky. 📷: @woolstonphoto https://t.co/YCGHNtTJNS",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				149
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "woolstonphoto",
+						"name": "Bryan Woolston, Photojournalist",
+						"id": 26643165,
+						"id_str": "26643165",
+						"indices": [
+							135,
+							149
+						]
+					}
+				],
+				"urls": [],
+				"media": [
+					{
+						"id": 1223344366711951400,
+						"id_str": "1223344366711951363",
+						"indices": [
+							150,
+							173
+						],
+						"media_url": "http://pbs.twimg.com/media/EPoxOE_XUAM7pYF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EPoxOE_XUAM7pYF.jpg",
+						"url": "https://t.co/YCGHNtTJNS",
+						"display_url": "pic.twitter.com/YCGHNtTJNS",
+						"expanded_url": "https://twitter.com/GettyImagesNews/status/1223344677568634880/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2000,
+								"h": 1333,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": [
+									{
+										"x": 378,
+										"y": 167,
+										"h": 171,
+										"w": 171
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 214,
+										"y": 94,
+										"h": 96,
+										"w": 96
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 630,
+										"y": 279,
+										"h": 285,
+										"w": 285
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 630,
+										"y": 279,
+										"h": 285,
+										"w": 285
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1223344366711951400,
+						"id_str": "1223344366711951363",
+						"indices": [
+							150,
+							173
+						],
+						"media_url": "http://pbs.twimg.com/media/EPoxOE_XUAM7pYF.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EPoxOE_XUAM7pYF.jpg",
+						"url": "https://t.co/YCGHNtTJNS",
+						"display_url": "pic.twitter.com/YCGHNtTJNS",
+						"expanded_url": "https://twitter.com/GettyImagesNews/status/1223344677568634880/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2000,
+								"h": 1333,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"medium": {
+								"faces": [
+									{
+										"x": 378,
+										"y": 167,
+										"h": 171,
+										"w": 171
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 214,
+										"y": 94,
+										"h": 96,
+										"w": 96
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 630,
+										"y": 279,
+										"h": 285,
+										"w": 285
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 630,
+										"y": 279,
+										"h": 285,
+										"w": 285
+									}
+								]
+							}
+						}
+					},
+					{
+						"id": 1223344366703513600,
+						"id_str": "1223344366703513605",
+						"indices": [
+							150,
+							173
+						],
+						"media_url": "http://pbs.twimg.com/media/EPoxOE9WkAUc9wH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EPoxOE9WkAUc9wH.jpg",
+						"url": "https://t.co/YCGHNtTJNS",
+						"display_url": "pic.twitter.com/YCGHNtTJNS",
+						"expanded_url": "https://twitter.com/GettyImagesNews/status/1223344677568634880/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2000,
+								"h": 1333,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"large": {
+								"faces": [
+									{
+										"x": 787,
+										"y": 603,
+										"h": 177,
+										"w": 177
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 267,
+										"y": 205,
+										"h": 60,
+										"w": 60
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 472,
+										"y": 361,
+										"h": 106,
+										"w": 106
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 787,
+										"y": 603,
+										"h": 177,
+										"w": 177
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 22127274,
+				"id_str": "22127274",
+				"name": "Getty Images News",
+				"screen_name": "GettyImagesNews",
+				"location": "Global",
+				"description": "The best photojournalists in the world, bringing you the biggest news stories from across the globe",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 100450,
+				"friends_count": 8693,
+				"listed_count": 2410,
+				"created_at": "Fri Feb 27 10:28:23 +0000 2009",
+				"favourites_count": 449,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 21312,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/930847173562028039/AQwDde3X_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/930847173562028039/AQwDde3X_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/22127274/1481149725",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "9FA484",
+				"profile_text_color": "8A8436",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1943,
+			"favorite_count": 2530,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 567,
+		"favorite_count": 2984,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "gucci belt has me thrown https://t.co/SFtLQNP6NE"
+	}
+]
