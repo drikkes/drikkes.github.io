b9c98c750ff1c895d7f2f99aa08dabeb810bfc59
-drik S <drikkes@gmail.com>
2020-02-03 15:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65886,
-	"created_at": "Mon Feb 03 13:11:18 +0000 2020",
+	"tweets": 65887,
+	"created_at": "Mon Feb 03 14:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,184 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 03 14:00:54 +0000 2020",
+		"id": 1224331931862667300,
+		"id_str": "1224331931862667264",
+		"full_text": "@mspro @ennopark @baranek @paddya91 @plaetzchen @343max @marcelweiss Frage mich sowieso, warum gMaps 1 Fußgänger mit 99 Smartphones für 1 Autostau hält. Nur, weil die Navi-Apps auf „Auto“ stehen? Nur, weil er mitten auf der Straße läuft?",
+		"truncated": false,
+		"display_text_range": [
+			69,
+			237
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mspro",
+					"name": "Michael Seemann",
+					"id": 5751892,
+					"id_str": "5751892",
+					"indices": [
+						0,
+						6
+					]
+				},
+				{
+					"screen_name": "ennopark",
+					"name": "Enno Park",
+					"id": 16034275,
+					"id_str": "16034275",
+					"indices": [
+						7,
+						16
+					]
+				},
+				{
+					"screen_name": "baranek",
+					"name": "Dirk Baranek",
+					"id": 928881,
+					"id_str": "928881",
+					"indices": [
+						17,
+						25
+					]
+				},
+				{
+					"screen_name": "paddya91",
+					"name": "Patrick Bisenius",
+					"id": 51402413,
+					"id_str": "51402413",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"screen_name": "plaetzchen",
+					"name": "Philip Brechler",
+					"id": 14305613,
+					"id_str": "14305613",
+					"indices": [
+						36,
+						47
+					]
+				},
+				{
+					"screen_name": "343max",
+					"name": "Max von Webel",
+					"id": 2284151,
+					"id_str": "2284151",
+					"indices": [
+						48,
+						55
+					]
+				},
+				{
+					"screen_name": "marcelweiss",
+					"name": "Marcel Weiss",
+					"id": 1007251,
+					"id_str": "1007251",
+					"indices": [
+						56,
+						68
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1224330551475294200,
+		"in_reply_to_status_id_str": "1224330551475294210",
+		"in_reply_to_user_id": 5751892,
+		"in_reply_to_user_id_str": "5751892",
+		"in_reply_to_screen_name": "mspro",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 853,
+			"friends_count": 163,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 4975,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19586,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@mspro @ennopark @baranek @paddya91 @plaetzchen @343max @marcelweiss Frage mich sowieso, warum gMaps 1 Fußgänger mit 99 Smartphones für 1 Autostau hält. Nur, weil die Navi-Apps auf „Auto“ stehen? Nur, weil er mitten auf der Straße läuft?"
+	},
+	{
 		"created_at": "Mon Feb 03 13:06:18 +0000 2020",
 		"id": 1224318193335971800,
 		"id_str": "1224318193335971841",
