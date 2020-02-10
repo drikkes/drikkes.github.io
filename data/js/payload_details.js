f1ef68bb338e88cc6bd765dd499ca689244e285c
-drik S <drikkes@gmail.com>
2020-02-10 09:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65907,
-	"created_at": "Sun Feb 09 16:11:18 +0000 2020",
+	"tweets": 65908,
+	"created_at": "Mon Feb 10 08:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,118 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 10 07:25:14 +0000 2020",
+		"id": 1226611792597733400,
+		"id_str": "1226769074648121344",
+		"full_text": "Accessibility is a relative concept https://t.co/x5k0BaWPHZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/x5k0BaWPHZ",
+					"expanded_url": "https://twitter.com/thefashionlaw/status/1226582285291528193",
+					"display_url": "twitter.com/thefashionlaw/…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 28175824,
+			"id_str": "28175824",
+			"name": "Vanessa Friedman",
+			"screen_name": "VVFriedman",
+			"location": "",
+			"description": "New York Times and INYT fashion director/critic, FT alumna, mother (of 3), flying trapezer, slow-but-happy bike rider, views are all my own",
+			"url": "https://t.co/UmqCI5RSM5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/UmqCI5RSM5",
+							"expanded_url": "http://www.nytimes.com",
+							"display_url": "nytimes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 153970,
+			"friends_count": 324,
+			"listed_count": 1095,
+			"created_at": "Wed Apr 01 19:36:57 +0000 2009",
+			"favourites_count": 5928,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12666,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000049147877/f8f3b0e8ea12e91072ca533416334fcf_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000049147877/f8f3b0e8ea12e91072ca533416334fcf_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1226582285291528200,
+		"quoted_status_id_str": "1226582285291528193",
+		"quoted_status_permalink": {
+			"url": "https://t.co/x5k0BaWPHZ",
+			"expanded": "https://twitter.com/thefashionlaw/status/1226582285291528193",
+			"display": "twitter.com/thefashionlaw/…"
+		},
+		"retweet_count": 38,
+		"favorite_count": 208,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Accessibility is a relative concept https://t.co/x5k0BaWPHZ"
+	},
+	{
 		"created_at": "Sun Feb 09 15:27:37 +0000 2020",
 		"id": 1226528081424396300,
 		"id_str": "1226528081424396288",
