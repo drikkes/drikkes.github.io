cb8c675dd331ec0e91db171cf88b157ae0d01cb6
-drik S <drikkes@gmail.com>
2020-02-28 13:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65975,
-	"created_at": "Fri Feb 28 08:11:19 +0000 2020",
+	"tweets": 65976,
+	"created_at": "Fri Feb 28 12:11:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,202 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Fri Feb 28 11:20:29 +0000 2020",
+		"id": 1233314049124962300,
+		"id_str": "1233351258360229888",
+		"full_text": "https://t.co/1INXb6r5X6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1233314044683210800,
+					"id_str": "1233314044683210753",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/ER2cltnX0AELiAK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER2cltnX0AELiAK.jpg",
+					"url": "https://t.co/1INXb6r5X6",
+					"display_url": "pic.twitter.com/1INXb6r5X6",
+					"expanded_url": "https://twitter.com/BafiKush/status/1233314049124962304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 652,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 591,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1233314044683210800,
+					"id_str": "1233314044683210753",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/ER2cltnX0AELiAK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER2cltnX0AELiAK.jpg",
+					"url": "https://t.co/1INXb6r5X6",
+					"display_url": "pic.twitter.com/1INXb6r5X6",
+					"expanded_url": "https://twitter.com/BafiKush/status/1233314049124962304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 652,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 591,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1233314044691480600,
+					"id_str": "1233314044691480577",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/ER2cltpWAAEypsq.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ER2cltpWAAEypsq.jpg",
+					"url": "https://t.co/1INXb6r5X6",
+					"display_url": "pic.twitter.com/1INXb6r5X6",
+					"expanded_url": "https://twitter.com/BafiKush/status/1233314049124962304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 608,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 608,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1555526863,
+			"id_str": "1555526863",
+			"name": "Bafi im Bann der Dämonen",
+			"screen_name": "BafiKush",
+			"location": "In der Bahn",
+			"description": "Gestern Gallus Heute Charts",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3303,
+			"friends_count": 391,
+			"listed_count": 13,
+			"created_at": "Sat Jun 29 11:52:01 +0000 2013",
+			"favourites_count": 58439,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16308,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1203689639732744194/6_re23ij_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1203689639732744194/6_re23ij_normal.jpg",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 497,
+		"favorite_count": 2246,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/1INXb6r5X6"
+	},
+	{
 		"created_at": "Fri Feb 28 07:55:38 +0000 2020",
 		"id": 1233299707914604500,
 		"id_str": "1233299707914604548",
