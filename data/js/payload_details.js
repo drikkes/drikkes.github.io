01b96831d816422a23380e449bc8100a18da5027
-drik S <drikkes@gmail.com>
2020-03-01 23:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65981,
-	"created_at": "Sun Mar 01 19:11:19 +0000 2020",
+	"tweets": 65982,
+	"created_at": "Sun Mar 01 22:11:17 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 01 21:20:44 +0000 2020",
+		"id": 1233042201288093700,
+		"id_str": "1234227092054777856",
+		"full_text": "When I tell you this sent me FLAT on the floor https://t.co/lGzLpd75fS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1233041487031611400,
+					"id_str": "1233041487031611394",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1233041487031611394/pu/img/TXDC65JWVDRmD5pl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1233041487031611394/pu/img/TXDC65JWVDRmD5pl.jpg",
+					"url": "https://t.co/lGzLpd75fS",
+					"display_url": "pic.twitter.com/lGzLpd75fS",
+					"expanded_url": "https://twitter.com/mrvndn/status/1233042201288093696/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1233041487031611400,
+					"id_str": "1233041487031611394",
+					"indices": [
+						47,
+						70
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1233041487031611394/pu/img/TXDC65JWVDRmD5pl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1233041487031611394/pu/img/TXDC65JWVDRmD5pl.jpg",
+					"url": "https://t.co/lGzLpd75fS",
+					"display_url": "pic.twitter.com/lGzLpd75fS",
+					"expanded_url": "https://twitter.com/mrvndn/status/1233042201288093696/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 93118,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1233041487031611394/pu/vid/1280x720/4yNSCpKDp_tbhfEI.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1233041487031611394/pu/vid/640x360/MGm_6bZLckNTVWNT.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1233041487031611394/pu/pl/kbltacLFA-a9XPgR.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1233041487031611394/pu/vid/480x270/8nmpnMfLw8ctftHs.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 22454663,
+			"id_str": "22454663",
+			"name": "marv 🗽",
+			"screen_name": "mrvndn",
+			"location": "",
+			"description": "Warren gay 🗳 Elect progressive women everywhere. He/Him/Él",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 17875,
+			"friends_count": 8882,
+			"listed_count": 263,
+			"created_at": "Mon Mar 02 05:19:31 +0000 2009",
+			"favourites_count": 200815,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53039,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "423939",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1231983363256455168/3wT1wilE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1231983363256455168/3wT1wilE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/22454663/1582585106",
+			"profile_link_color": "868C96",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "EB7C7C",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 51972,
+		"favorite_count": 240771,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "When I tell you this sent me FLAT on the floor https://t.co/lGzLpd75fS"
+	},
+	{
 		"created_at": "Sun Mar 01 18:25:58 +0000 2020",
 		"id": 1234183110234955800,
 		"id_str": "1234183110234955776",
