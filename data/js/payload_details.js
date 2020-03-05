65bc479f6cee3131666566c116a4513331bc484f
-drik S <drikkes@gmail.com>
2020-03-05 13:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 65998,
-	"created_at": "Thu Mar 05 11:11:18 +0000 2020",
+	"tweets": 65999,
+	"created_at": "Thu Mar 05 12:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Thu Mar 05 11:46:25 +0000 2020",
+		"id": 1235527275614843000,
+		"id_str": "1235532111391731717",
+		"full_text": "Bald dann die ganze Timeline voll mit Screenshots aus Twitter Storys j@@@@@@@@!!!!!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1235526748600754200,
+		"in_reply_to_status_id_str": "1235526748600754177",
+		"in_reply_to_user_id": 2970160174,
+		"in_reply_to_user_id_str": "2970160174",
+		"in_reply_to_screen_name": "KurtProedel",
+		"user": {
+			"id": 2970160174,
+			"id_str": "2970160174",
+			"name": "Kurt Prödel",
+			"screen_name": "KurtProedel",
+			"location": "",
+			"description": "Coach & Speaker @diescreenshots",
+			"url": "https://t.co/3mUS5s7tWY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3mUS5s7tWY",
+							"expanded_url": "https://www.instagram.com/diescreenshots",
+							"display_url": "instagram.com/diescreenshots",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 25250,
+			"friends_count": 571,
+			"listed_count": 87,
+			"created_at": "Fri Jan 09 17:56:54 +0000 2015",
+			"favourites_count": 55898,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10418,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099289697342369792/B7QzXSv8_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099289697342369792/B7QzXSv8_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2970160174/1533390446",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 35,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "de",
+		"text": "Bald dann die ganze Timeline voll mit Screenshots aus Twitter Storys j@@@@@@@@!!!!!"
+	},
+	{
 		"created_at": "Thu Mar 05 10:12:18 +0000 2020",
 		"id": 1235407520916738000,
 		"id_str": "1235508428950237184",
