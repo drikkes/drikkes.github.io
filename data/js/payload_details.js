884ba98cedc5108f1e60ade4484e1070f428c24a
-drik S <drikkes@gmail.com>
2020-03-20 16:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66044,
-	"created_at": "Fri Mar 20 13:11:18 +0000 2020",
+	"tweets": 66045,
+	"created_at": "Fri Mar 20 15:11:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Fri Mar 20 14:36:20 +0000 2020",
+		"id": 1241010693765304300,
+		"id_str": "1241010693765304320",
+		"full_text": "@koeppenjan @janboehm Find ich gut, daß das ok ist. (Hey, immerhin könnte ich jetzt behaupten, schon krank zu sein, nur um Dich auflaufen zu lassen.) Alles gut.",
+		"truncated": false,
+		"display_text_range": [
+			22,
+			160
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "koeppenjan",
+					"name": "J A N   K Ö P P E N",
+					"id": 237017783,
+					"id_str": "237017783",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "janboehm",
+					"name": "🦠 Jan ZUHAUSE BLEIBEN! 🦠 HÄNDE WASCHEN! 🦠🤨",
+					"id": 19072286,
+					"id_str": "19072286",
+					"indices": [
+						12,
+						21
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1241008402807042000,
+		"in_reply_to_status_id_str": "1241008402807042049",
+		"in_reply_to_user_id": 237017783,
+		"in_reply_to_user_id_str": "237017783",
+		"in_reply_to_screen_name": "koeppenjan",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 850,
+			"friends_count": 162,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5034,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19758,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/556473927602806784/J9H4QXjx_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@koeppenjan @janboehm Find ich gut, daß das ok ist. (Hey, immerhin könnte ich jetzt behaupten, schon krank zu sein, nur um Dich auflaufen zu lassen.) Alles gut."
+	},
+	{
 		"created_at": "Fri Mar 20 12:56:43 +0000 2020",
 		"id": 1240985622854189000,
 		"id_str": "1240985622854189056",
