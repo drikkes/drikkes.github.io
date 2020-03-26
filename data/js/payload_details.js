e07a4fdc6db182a17c4e64d81ef2c2ca506cb122
-drik S <drikkes@gmail.com>
2020-03-26 12:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66070,
-	"created_at": "Thu Mar 26 10:11:19 +0000 2020",
+	"tweets": 66071,
+	"created_at": "Thu Mar 26 11:11:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Thu Mar 26 10:54:47 +0000 2020",
+		"id": 1243129263915782100,
+		"id_str": "1243129263915782144",
+		"full_text": "@innere_simone @freelancepolice In meinem westdeutschen Nobelträgerviertel ist alles durchgekoppelt: Elsa-Brändström-Straße, Paul-Ehrlich-Straße, Von-Suttner-Straße, Otto-Hahn-Straße, Thomas-Mann-Straße, … Allerdings dazwischen random vornamenlos. Gibt auch Mommsenstraße, Dunantstraße, …",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			288
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "innere_simone",
+					"name": "oligarchennichte",
+					"id": 16902386,
+					"id_str": "16902386",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "freelancepolice",
+					"name": "Michael Brake",
+					"id": 14311831,
+					"id_str": "14311831",
+					"indices": [
+						15,
+						31
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1243124204066783200,
+		"in_reply_to_status_id_str": "1243124204066783235",
+		"in_reply_to_user_id": 16902386,
+		"in_reply_to_user_id_str": "16902386",
+		"in_reply_to_screen_name": "innere_simone",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 850,
+			"friends_count": 162,
+			"listed_count": 81,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5048,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 19787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@innere_simone @freelancepolice In meinem westdeutschen Nobelträgerviertel ist alles durchgekoppelt: Elsa-Brändström-Straße, Paul-Ehrlich-Straße, Von-Suttner-Straße, Otto-Hahn-Straße, Thomas-Mann-Straße, … Allerdings dazwischen random vornamenlos. Gibt auch Mommsenstraße, Dunantstraße, …"
+	},
+	{
 		"created_at": "Thu Mar 26 09:36:38 +0000 2020",
 		"id": 1243109595435937800,
 		"id_str": "1243109595435937792",
