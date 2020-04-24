642d78408cacdb77a700bc2b87026e988b6d9f3d
-drik S <drikkes@gmail.com>
2020-04-24 12:11:21 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66208,
-	"created_at": "Fri Apr 24 09:11:19 +0000 2020",
+	"tweets": 66209,
+	"created_at": "Fri Apr 24 10:11:20 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Fri Apr 24 09:30:15 +0000 2020",
+		"id": 1251219882563088400,
+		"id_str": "1253617239313592320",
+		"full_text": "Once lockdown is lifted and we’re no longer on Zoom, I’m walking around with a tiny mirror above my face so you can continue to stare at your own face while we’re talking. https://t.co/TXmwvyRx9w",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			171
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1251219878280745000,
+					"id_str": "1251219878280744960",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EV052ZzU0AAexec.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EV052ZzU0AAexec.jpg",
+					"url": "https://t.co/TXmwvyRx9w",
+					"display_url": "pic.twitter.com/TXmwvyRx9w",
+					"expanded_url": "https://twitter.com/djbaskin/status/1251219882563088384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 441,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1328,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 778,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1251219878280745000,
+					"id_str": "1251219878280744960",
+					"indices": [
+						172,
+						195
+					],
+					"media_url": "http://pbs.twimg.com/media/EV052ZzU0AAexec.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EV052ZzU0AAexec.jpg",
+					"url": "https://t.co/TXmwvyRx9w",
+					"display_url": "pic.twitter.com/TXmwvyRx9w",
+					"expanded_url": "https://twitter.com/djbaskin/status/1251219882563088384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 441,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1328,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 778,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 55662470,
+			"id_str": "55662470",
+			"name": "Danielle Baskin",
+			"screen_name": "djbaskin",
+			"location": "San Francisco, CA",
+			"description": "Dialup ceo. Painter. Phone operator. Tarot reader. URL undertaker. Urbexplorer. Puts logos on fruit. Companies as art form. 💾➡️https://t.co/PhVOrkzZ98 \n@dialupdotcom",
+			"url": "https://t.co/Gq1wzsEtwd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Gq1wzsEtwd",
+							"expanded_url": "https://www.daniellebaskin.com",
+							"display_url": "daniellebaskin.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PhVOrkzZ98",
+							"expanded_url": "http://dialup.com",
+							"display_url": "dialup.com",
+							"indices": [
+								127,
+								150
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 7974,
+			"friends_count": 523,
+			"listed_count": 92,
+			"created_at": "Fri Jul 10 21:00:48 +0000 2009",
+			"favourites_count": 3966,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 2057,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "292929",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme12/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1219233434322628608/ZD1qvkE-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1219233434322628608/ZD1qvkE-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/55662470/1473923216",
+			"profile_link_color": "DEB100",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "00C414",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 733,
+		"favorite_count": 4655,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Once lockdown is lifted and we’re no longer on Zoom, I’m walking around with a tiny mirror above my face so you can continue to stare at your own face while we’re talking. https://t.co/TXmwvyRx9w"
+	},
+	{
 		"created_at": "Fri Apr 24 08:56:48 +0000 2020",
 		"id": 1253608821966110700,
 		"id_str": "1253608821966110721",
