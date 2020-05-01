bf9c9f78246d4c7310edee5d58188e5807212d8c
-drik S <drikkes@gmail.com>
2020-05-01 13:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66239,
-	"created_at": "Fri May 01 07:11:18 +0000 2020",
+	"tweets": 66240,
+	"created_at": "Fri May 01 11:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 01 10:13:38 +0000 2020",
+		"id": 1255892939986874400,
+		"id_str": "1256164870892007424",
+		"full_text": "#NSU-Urteil - \"Wir waren Ihnen kein Wort wert\" Opferfamilien klagen über Urteil im #NSU Prozess\nvia @sz https://t.co/gOndBM748t",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NSU",
+					"indices": [
+						0,
+						4
+					]
+				},
+				{
+					"text": "NSU",
+					"indices": [
+						83,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "SZ",
+					"name": "Süddeutsche Zeitung",
+					"id": 114508061,
+					"id_str": "114508061",
+					"indices": [
+						100,
+						103
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gOndBM748t",
+					"expanded_url": "https://sz.de/1.4893697",
+					"display_url": "sz.de/1.4893697",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 987038425,
+			"id_str": "987038425",
+			"name": "Annette Ramelsberger",
+			"screen_name": "ARamelsberger",
+			"location": "",
+			"description": "Gerichtsreporterin bei der Süddeutschen Zeitung.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6229,
+			"friends_count": 959,
+			"listed_count": 139,
+			"created_at": "Mon Dec 03 17:07:32 +0000 2012",
+			"favourites_count": 697,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1726,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/412216779813822464/8bm7DgLJ_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/412216779813822464/8bm7DgLJ_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 108,
+		"favorite_count": 173,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "#NSU-Urteil - \"Wir waren Ihnen kein Wort wert\" Opferfamilien klagen über Urteil im #NSU Prozess\nvia @sz https://t.co/gOndBM748t"
+	},
+	{
 		"created_at": "Fri May 01 06:16:03 +0000 2020",
 		"id": 1256105083273650200,
 		"id_str": "1256105083273650176",
