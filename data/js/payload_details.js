8c978c2360a478b6b1f8e7c9e08c92d6b7756aea
-drik S <drikkes@gmail.com>
2020-05-10 08:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66277,
-	"created_at": "Sat May 09 14:11:18 +0000 2020",
+	"tweets": 66278,
+	"created_at": "Sun May 10 06:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 10 05:57:43 +0000 2020",
+		"id": 1259360736255459300,
+		"id_str": "1259361957301432320",
+		"full_text": "Singapore uses a terrifying @BostonDynamics robot to remind people in public parks to follow social distancing rules. \nhttps://t.co/vHJINVjflr",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BostonDynamics",
+					"name": "Boston Dynamics",
+					"id": 517473207,
+					"id_str": "517473207",
+					"indices": [
+						28,
+						43
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/vHJINVjflr",
+					"expanded_url": "https://youtu.be/viuR7N6E2LA",
+					"display_url": "youtu.be/viuR7N6E2LA",
+					"indices": [
+						119,
+						142
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 11003812,
+			"id_str": "11003812",
+			"name": "Mathias Peer",
+			"screen_name": "mpeer",
+			"location": "Bangkok",
+			"description": "Correspondent for Southeast Asia and India @handelsblatt - peer@weltreporter.net - Tweets in English and German",
+			"url": "https://t.co/hhhsFJJLdq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hhhsFJJLdq",
+							"expanded_url": "https://www.handelsblatt.com/autoren/mathias-peer/8740902.html",
+							"display_url": "handelsblatt.com/autoren/mathia…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2628,
+			"friends_count": 2190,
+			"listed_count": 99,
+			"created_at": "Sun Dec 09 22:46:10 +0000 2007",
+			"favourites_count": 9295,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4650,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1049675268271398912/sgRCDbED_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1049675268271398912/sgRCDbED_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/11003812/1416543787",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Singapore uses a terrifying @BostonDynamics robot to remind people in public parks to follow social distancing rules. \nhttps://t.co/vHJINVjflr"
+	},
+	{
 		"created_at": "Sat May 09 13:22:26 +0000 2020",
 		"id": 1259111487412584400,
 		"id_str": "1259111487412584448",
