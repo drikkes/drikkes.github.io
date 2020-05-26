96fc0305fa9d737fb3f98517ed1d49042adff36c
-drik S <drikkes@gmail.com>
2020-05-26 09:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66339,
-	"created_at": "Tue May 26 00:11:18 +0000 2020",
+	"tweets": 66340,
+	"created_at": "Tue May 26 07:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Tue May 26 06:14:10 +0000 2020",
+		"id": 1265132406438350800,
+		"id_str": "1265164303965065219",
+		"full_text": "The final show takes the form of a three-minute video, and like a prestigious IRL fashion event, it’s soundtracked by Michel Gaubert https://t.co/XCAEC7Zfm3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XCAEC7Zfm3",
+					"expanded_url": "https://www.vogue.com/article/animal-crossing-fashion-show-reference-berlin",
+					"display_url": "vogue.com/article/animal…",
+					"indices": [
+						133,
+						156
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14287222,
+			"id_str": "14287222",
+			"name": "Marcus Bösch",
+			"screen_name": "m_boesch",
+			"location": "Berlin, Deutschland",
+			"description": "Digital & spatial storytelling. Projects & workshops. Co-founder @vragments @thegoodevilcom",
+			"url": "https://t.co/1iUAbVNIM4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1iUAbVNIM4",
+							"expanded_url": "https://www.marcus-boesch.de",
+							"display_url": "marcus-boesch.de",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4293,
+			"friends_count": 1998,
+			"listed_count": 348,
+			"created_at": "Wed Apr 02 20:31:01 +0000 2008",
+			"favourites_count": 9686,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9129,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1021892786050744320/oZaDCcud_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021892786050744320/oZaDCcud_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14287222/1559138821",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "0E120A",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "504141",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The final show takes the form of a three-minute video, and like a prestigious IRL fashion event, it’s soundtracked by Michel Gaubert https://t.co/XCAEC7Zfm3"
+	},
+	{
 		"created_at": "Mon May 25 23:17:46 +0000 2020",
 		"id": 1265059514857984000,
 		"id_str": "1265059514857984001",
