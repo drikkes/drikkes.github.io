cb527836c5a58be4438eb57747b999e26f791b4d
-drik S <drikkes@gmail.com>
2020-06-05 18:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66374,
-	"created_at": "Fri Jun 05 13:11:18 +0000 2020",
+	"tweets": 66375,
+	"created_at": "Fri Jun 05 16:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Fri Jun 05 15:56:01 +0000 2020",
+		"id": 1268916126190272500,
+		"id_str": "1268934609972146177",
+		"full_text": "Stop treating the protests like Coachella pt 17 https://t.co/zOzf4ZvvWf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			47
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1268916043587649500,
+					"id_str": "1268916043587649538",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1268916043587649538/pu/img/U8wzyzf1AOHkRYVa.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1268916043587649538/pu/img/U8wzyzf1AOHkRYVa.jpg",
+					"url": "https://t.co/zOzf4ZvvWf",
+					"display_url": "pic.twitter.com/zOzf4ZvvWf",
+					"expanded_url": "https://twitter.com/influencersitw/status/1268916126190272514/video/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1268916043587649500,
+					"id_str": "1268916043587649538",
+					"indices": [
+						48,
+						71
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1268916043587649538/pu/img/U8wzyzf1AOHkRYVa.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1268916043587649538/pu/img/U8wzyzf1AOHkRYVa.jpg",
+					"url": "https://t.co/zOzf4ZvvWf",
+					"display_url": "pic.twitter.com/zOzf4ZvvWf",
+					"expanded_url": "https://twitter.com/influencersitw/status/1268916126190272514/video/1",
+					"type": "video",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 6800,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1268916043587649538/pu/pl/ChWJ15p8lEQyE3T6.m3u8?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1268916043587649538/pu/vid/320x400/cMdpo23sFxJ-djiL.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1268916043587649538/pu/vid/360x450/Pq8hcrvFV-2KqzjG.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1268916043587649538/pu/vid/720x900/yQ7Lb8p3NgVj1fBZ.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1219740080354529300,
+			"id_str": "1219740080354529285",
+			"name": "influencersinthewild",
+			"screen_name": "influencersitw",
+			"location": "",
+			"description": "Celebrating content culture",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 100675,
+			"friends_count": 2,
+			"listed_count": 110,
+			"created_at": "Tue Jan 21 21:54:42 +0000 2020",
+			"favourites_count": 90,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 152,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1219740230279933953/emGKRiTy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1219740230279933953/emGKRiTy_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4860,
+		"favorite_count": 20153,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Stop treating the protests like Coachella pt 17 https://t.co/zOzf4ZvvWf"
+	},
+	{
 		"created_at": "Fri Jun 05 12:57:47 +0000 2020",
 		"id": 1268889755321761800,
 		"id_str": "1268889755321761793",
