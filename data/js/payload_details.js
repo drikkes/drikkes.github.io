a8c538d887635e9a21bf207ae6b9b7003ddb877b
-drik S <drikkes@gmail.com>
2020-06-07 14:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66382,
-	"created_at": "Sun Jun 07 10:11:18 +0000 2020",
+	"tweets": 66383,
+	"created_at": "Sun Jun 07 12:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 07 11:38:10 +0000 2020",
+		"id": 1268622077180825600,
+		"id_str": "1269594497127788556",
+		"full_text": "“the do nothing democrats will do anything...” these text messages are a parody of themselves https://t.co/IK9HxRsZLd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			93
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1268622073934622700,
+					"id_str": "1268622073934622722",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EZsNDgcXsAISTwr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZsNDgcXsAISTwr.jpg",
+					"url": "https://t.co/IK9HxRsZLd",
+					"display_url": "pic.twitter.com/IK9HxRsZLd",
+					"expanded_url": "https://twitter.com/sarahcpr/status/1268622077180825600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 886,
+							"h": 498,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 886,
+							"h": 498,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1268622073934622700,
+					"id_str": "1268622073934622722",
+					"indices": [
+						94,
+						117
+					],
+					"media_url": "http://pbs.twimg.com/media/EZsNDgcXsAISTwr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZsNDgcXsAISTwr.jpg",
+					"url": "https://t.co/IK9HxRsZLd",
+					"display_url": "pic.twitter.com/IK9HxRsZLd",
+					"expanded_url": "https://twitter.com/sarahcpr/status/1268622077180825600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 886,
+							"h": 498,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 886,
+							"h": 498,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14642495,
+			"id_str": "14642495",
+			"name": "Sarah Cooper",
+			"screen_name": "sarahcpr",
+			"location": "Brooklyn, NY",
+			"description": "writer / comedian / #blockedbytrump / wrote 100 Tricks to Appear Smart in Meetings & How to Be Successful Without Hurting Men's Feelings",
+			"url": "https://t.co/SSzNovmSrb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SSzNovmSrb",
+							"expanded_url": "http://sarahcpr.com",
+							"display_url": "sarahcpr.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1195035,
+			"friends_count": 2764,
+			"listed_count": 3161,
+			"created_at": "Sat May 03 23:30:29 +0000 2008",
+			"favourites_count": 136309,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 37087,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1256381928246296578/yPPL34Um_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1256381928246296578/yPPL34Um_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14642495/1581527591",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5949,
+		"favorite_count": 48884,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“the do nothing democrats will do anything...” these text messages are a parody of themselves https://t.co/IK9HxRsZLd"
+	},
+	{
 		"created_at": "Sun Jun 07 09:34:37 +0000 2020",
 		"id": 1269495810980556800,
 		"id_str": "1269563401963024384",
