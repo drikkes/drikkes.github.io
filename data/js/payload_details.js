f2d8283117c2c8b63f5d61b00e3cfcab7970843e
-drik S <drikkes@gmail.com>
2020-07-03 11:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66473,
-	"created_at": "Thu Jul 02 15:11:23 +0000 2020",
+	"tweets": 66474,
+	"created_at": "Fri Jul 03 09:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,178 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 03 09:06:09 +0000 2020",
+		"id": 1278978323310313500,
+		"id_str": "1278978323310313472",
+		"full_text": "@hermsfarm Da ist der Slipknot-Frontmann auch schon drauf gekommen. https://t.co/MjkJKcgYCM",
+		"truncated": false,
+		"display_text_range": [
+			11,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hermsfarm",
+					"name": "Herm",
+					"id": 14298281,
+					"id_str": "14298281",
+					"indices": [
+						0,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MjkJKcgYCM",
+					"expanded_url": "https://variety.com/2019/music/news/imagine-dragons-nickelback-worst-band-slipknot-corey-taylor-1203145926/",
+					"display_url": "variety.com/2019/music/new…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1278962044658688000,
+		"in_reply_to_status_id_str": "1278962044658688001",
+		"in_reply_to_user_id": 14298281,
+		"in_reply_to_user_id_str": "14298281",
+		"in_reply_to_screen_name": "hermsfarm",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 159,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5182,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20235,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				51.49902344,
+				3.491825
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				3.491825,
+				51.49902344
+			]
+		},
+		"place": {
+			"id": "b548fab00c4e7b8a",
+			"url": "https://api.twitter.com/1.1/geo/id/b548fab00c4e7b8a.json",
+			"place_type": "city",
+			"name": "Veere",
+			"full_name": "Veere, The Netherlands",
+			"country_code": "NL",
+			"country": "The Netherlands",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							3.4313757,
+							51.4623453
+						],
+						[
+							3.7488978,
+							51.4623453
+						],
+						[
+							3.7488978,
+							51.6502839
+						],
+						[
+							3.4313757,
+							51.6502839
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "@hermsfarm Da ist der Slipknot-Frontmann auch schon drauf gekommen. https://t.co/MjkJKcgYCM"
+	},
+	{
 		"created_at": "Thu Jul 02 14:25:24 +0000 2020",
 		"id": 1278696279686643700,
 		"id_str": "1278696279686643712",
