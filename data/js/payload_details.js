dec566a5436104fd0bbc3dd12a7df0fdeeb566ae
-drik S <drikkes@gmail.com>
2020-07-10 13:12:04 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66496,
-	"created_at": "Fri Jul 10 10:11:19 +0000 2020",
+	"tweets": 66497,
+	"created_at": "Fri Jul 10 11:12:03 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,331 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Fri Jul 10 10:31:50 +0000 2020",
+		"id": 1281536602876125200,
+		"id_str": "1281536602876125184",
+		"full_text": "Hier der Artikel online. https://t.co/XojdNZMJib https://t.co/zEKk6hmEhY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			48
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XojdNZMJib",
+					"expanded_url": "https://www.sueddeutsche.de/politik/corona-bekaempfung-hinter-dem-stoff-1.4962563",
+					"display_url": "sueddeutsche.de/politik/corona…",
+					"indices": [
+						25,
+						48
+					]
+				},
+				{
+					"url": "https://t.co/zEKk6hmEhY",
+					"expanded_url": "https://twitter.com/FJ_Murau/status/1281520956503199744",
+					"display_url": "twitter.com/FJ_Murau/statu…",
+					"indices": [
+						49,
+						72
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 853,
+			"friends_count": 160,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5204,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20261,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "255115bcb65f5da6",
+			"url": "https://api.twitter.com/1.1/geo/id/255115bcb65f5da6.json",
+			"place_type": "neighborhood",
+			"name": "Bayenthal",
+			"full_name": "Bayenthal, Köln",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.960374,
+							50.903919
+						],
+						[
+							6.979611,
+							50.903919
+						],
+						[
+							6.979611,
+							50.917496
+						],
+						[
+							6.960374,
+							50.917496
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1281520956503199700,
+		"quoted_status_id_str": "1281520956503199744",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zEKk6hmEhY",
+			"expanded": "https://twitter.com/FJ_Murau/status/1281520956503199744",
+			"display": "twitter.com/FJ_Murau/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jul 10 09:29:40 +0000 2020",
+			"id": 1281520956503199700,
+			"id_str": "1281520956503199744",
+			"full_text": "Ok (aus: SZ) https://t.co/9WmjPgVThu",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				12
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1281520950299820000,
+						"id_str": "1281520950299820034",
+						"indices": [
+							13,
+							36
+						],
+						"media_url": "http://pbs.twimg.com/media/Ecjgg7bXgAIiy-K.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ecjgg7bXgAIiy-K.jpg",
+						"url": "https://t.co/9WmjPgVThu",
+						"display_url": "pic.twitter.com/9WmjPgVThu",
+						"expanded_url": "https://twitter.com/FJ_Murau/status/1281520956503199744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 501,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1508,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 884,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1281520950299820000,
+						"id_str": "1281520950299820034",
+						"indices": [
+							13,
+							36
+						],
+						"media_url": "http://pbs.twimg.com/media/Ecjgg7bXgAIiy-K.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ecjgg7bXgAIiy-K.jpg",
+						"url": "https://t.co/9WmjPgVThu",
+						"display_url": "pic.twitter.com/9WmjPgVThu",
+						"expanded_url": "https://twitter.com/FJ_Murau/status/1281520956503199744/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 501,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1508,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 884,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1662224046,
+				"id_str": "1662224046",
+				"name": "Nils Markwardt",
+				"screen_name": "FJ_Murau",
+				"location": "Berlin",
+				"description": "(Leitender Redakteur beim Philosophie Magazin; Autor für Zeit Online, Deutschlandfunk Kultur, Republik u.a.)",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16899,
+				"friends_count": 688,
+				"listed_count": 208,
+				"created_at": "Sun Aug 11 10:46:55 +0000 2013",
+				"favourites_count": 63892,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 23953,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/998335945584689153/4Su00DTw_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/998335945584689153/4Su00DTw_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1662224046/1405693197",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 48,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "hu"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Hier der Artikel online. https://t.co/XojdNZMJib https://t.co/zEKk6hmEhY"
+	},
+	{
 		"created_at": "Fri Jul 10 09:51:02 +0000 2020",
 		"id": 1281526336968892400,
 		"id_str": "1281526336968892417",
