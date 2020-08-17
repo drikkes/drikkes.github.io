1da673eed25720a2eb8a7a73c9ce127da600e7c3
-drik S <drikkes@gmail.com>
2020-08-17 11:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66642,
-	"created_at": "Sun Aug 16 20:11:19 +0000 2020",
+	"tweets": 66643,
+	"created_at": "Mon Aug 17 09:11:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Mon Aug 17 08:54:04 +0000 2020",
+		"id": 1295264451659214800,
+		"id_str": "1295282736098414594",
+		"full_text": "Maskenschussgerät: https://t.co/gYq75wRwI4 https://t.co/0C508x4GzB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gYq75wRwI4",
+					"expanded_url": "https://www.kraftfuttermischwerk.de/blogg/maskenschussgeraet/",
+					"display_url": "kraftfuttermischwerk.de/blogg/maskensc…",
+					"indices": [
+						19,
+						42
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1295264427499978800,
+					"id_str": "1295264427499978757",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Efm0IltWAAUXOe4.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Efm0IltWAAUXOe4.jpg",
+					"url": "https://t.co/0C508x4GzB",
+					"display_url": "pic.twitter.com/0C508x4GzB",
+					"expanded_url": "https://twitter.com/das_kfmw/status/1295264451659214849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1295264427499978800,
+					"id_str": "1295264427499978757",
+					"indices": [
+						43,
+						66
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/Efm0IltWAAUXOe4.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/Efm0IltWAAUXOe4.jpg",
+					"url": "https://t.co/0C508x4GzB",
+					"display_url": "pic.twitter.com/0C508x4GzB",
+					"expanded_url": "https://twitter.com/das_kfmw/status/1295264451659214849/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 338,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							300,
+							169
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/Efm0IltWAAUXOe4.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20154852,
+			"id_str": "20154852",
+			"name": "Ronny Kraak",
+			"screen_name": "das_kfmw",
+			"location": "Potsdam, ehemalige SBZ",
+			"description": "Ein Drittel Heizöl, zwei Drittel Benzin.\n\nMacht was mit Liebe. Freiheit. Alles! Und Musik.",
+			"url": "https://t.co/3Bik1kLJwH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/3Bik1kLJwH",
+							"expanded_url": "http://www.kraftfuttermischwerk.de/blogg",
+							"display_url": "kraftfuttermischwerk.de/blogg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9471,
+			"friends_count": 511,
+			"listed_count": 263,
+			"created_at": "Thu Feb 05 15:08:01 +0000 2009",
+			"favourites_count": 35994,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 96150,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1285696747159859206/ETl98SN2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1285696747159859206/ETl98SN2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20154852/1395359133",
+			"profile_link_color": "19CF86",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FCFAF8",
+			"profile_text_color": "268C36",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 10,
+		"favorite_count": 28,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Maskenschussgerät: https://t.co/gYq75wRwI4 https://t.co/0C508x4GzB"
+	},
+	{
 		"created_at": "Sun Aug 16 20:01:00 +0000 2020",
 		"id": 1295088187484835800,
 		"id_str": "1295088187484835841",
