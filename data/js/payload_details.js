b7047c5ddfa2da649cf28ce7ef4d0bcff7029215
-drik S <drikkes@gmail.com>
2020-08-18 11:11:20 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66647,
-	"created_at": "Tue Aug 18 08:11:19 +0000 2020",
+	"tweets": 66648,
+	"created_at": "Tue Aug 18 09:11:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Tue Aug 18 08:39:55 +0000 2020",
+		"id": 1295641564677574700,
+		"id_str": "1295641564677574660",
+		"full_text": "@Tumblrsupport When I log-in on a new laptop, @Tumblr wants to redirect me to a privacy consent, but there’s no content at all, nothing clickable. (I use @firefox.) https://t.co/ML0aRQ7CRS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			164
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Tumblrsupport",
+					"name": "Tumblr Support",
+					"id": 1027216171421065200,
+					"id_str": "1027216171421065216",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "tumblr",
+					"name": "Tumblr",
+					"id": 52484614,
+					"id_str": "52484614",
+					"indices": [
+						46,
+						53
+					]
+				},
+				{
+					"screen_name": "firefox",
+					"name": "Firefox 🔥",
+					"id": 2142731,
+					"id_str": "2142731",
+					"indices": [
+						154,
+						162
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1295641558977589200,
+					"id_str": "1295641558977589249",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/EfsLIh8XsAEfk5f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfsLIh8XsAEfk5f.jpg",
+					"url": "https://t.co/ML0aRQ7CRS",
+					"display_url": "pic.twitter.com/ML0aRQ7CRS",
+					"expanded_url": "https://twitter.com/drikkes/status/1295641564677574660/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 502,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 949,
+							"h": 700,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 949,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1295641558977589200,
+					"id_str": "1295641558977589249",
+					"indices": [
+						165,
+						188
+					],
+					"media_url": "http://pbs.twimg.com/media/EfsLIh8XsAEfk5f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EfsLIh8XsAEfk5f.jpg",
+					"url": "https://t.co/ML0aRQ7CRS",
+					"display_url": "pic.twitter.com/ML0aRQ7CRS",
+					"expanded_url": "https://twitter.com/drikkes/status/1295641564677574660/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 502,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 949,
+							"h": 700,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 949,
+							"h": 700,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 1027216171421065200,
+		"in_reply_to_user_id_str": "1027216171421065216",
+		"in_reply_to_screen_name": "Tumblrsupport",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 160,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5262,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20436,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@Tumblrsupport When I log-in on a new laptop, @Tumblr wants to redirect me to a privacy consent, but there’s no content at all, nothing clickable. (I use @firefox.) https://t.co/ML0aRQ7CRS"
+	},
+	{
 		"created_at": "Tue Aug 18 08:04:59 +0000 2020",
 		"id": 1295632772330074000,
 		"id_str": "1295632772330074112",
