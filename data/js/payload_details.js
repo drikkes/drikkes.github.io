d504ec1673262289793d65954e84bc615fea1b1f
-drik S <drikkes@gmail.com>
2020-08-28 19:11:21 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66685,
-	"created_at": "Fri Aug 28 16:11:19 +0000 2020",
+	"tweets": 66686,
+	"created_at": "Fri Aug 28 17:11:20 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,132 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Fri Aug 28 16:19:42 +0000 2020",
+		"id": 1299381152277561300,
+		"id_str": "1299381152277561345",
+		"full_text": "@nilsphey @MEEDIA Auf der eierlosen Seite tun Sie es schon. „Die meiste Werbung ist so langweilig wie der Nachmittagstee auf der Demenzstation.“ Oder warum geht es da nicht um gefühlte Qualität? (Ändert nicht daran, daß ich Ihrer generellen Sichtweise zustimme.)",
+		"truncated": false,
+		"display_text_range": [
+			18,
+			262
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nilsphey",
+					"name": "Nils-Peter Hey",
+					"id": 99927246,
+					"id_str": "99927246",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "MEEDIA",
+					"name": "MEEDIA",
+					"id": 14497517,
+					"id_str": "14497517",
+					"indices": [
+						10,
+						17
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1299374574065389600,
+		"in_reply_to_status_id_str": "1299374574065389568",
+		"in_reply_to_user_id": 99927246,
+		"in_reply_to_user_id_str": "99927246",
+		"in_reply_to_screen_name": "nilsphey",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 852,
+			"friends_count": 161,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5278,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20477,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@nilsphey @MEEDIA Auf der eierlosen Seite tun Sie es schon. „Die meiste Werbung ist so langweilig wie der Nachmittagstee auf der Demenzstation.“ Oder warum geht es da nicht um gefühlte Qualität? (Ändert nicht daran, daß ich Ihrer generellen Sichtweise zustimme.)"
+	},
+	{
 		"created_at": "Fri Aug 28 15:42:51 +0000 2020",
 		"id": 1299371879766151200,
 		"id_str": "1299371879766151168",
