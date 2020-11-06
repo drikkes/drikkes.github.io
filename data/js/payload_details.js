c7ae68da3c92c63ccc646d89554848cb71b3cfc9
-drik S <drikkes@gmail.com>
2020-11-06 18:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66878,
-	"created_at": "Fri Nov 06 16:11:18 +0000 2020",
+	"tweets": 66879,
+	"created_at": "Fri Nov 06 17:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Fri Nov 06 16:55:06 +0000 2020",
+		"id": 1324757212443648000,
+		"id_str": "1324757212443648002",
+		"full_text": "@marco79 @DerZyklop So ist’s cool, danke. Dann wollte ich noch neunmalschlau den hier machen: https://t.co/Y7OQXkwyxi Aber dann wird die Infektionszahl bei mir doppelt angezeigt – einmal mit, einmal ohne Punkt. (Obwohl ein Komma Strenggenommen besser wäre.)",
+		"truncated": false,
+		"display_text_range": [
+			20,
+			257
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marco79",
+					"name": "Marco Dengel",
+					"id": 13598112,
+					"id_str": "13598112",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "DerZyklop",
+					"name": "DΞRZYKLΘP",
+					"id": 25325637,
+					"id_str": "25325637",
+					"indices": [
+						9,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Y7OQXkwyxi",
+					"expanded_url": "https://gist.github.com/oliverandrich/0f34c8d4e6de4b6ff32937c584009a65#gistcomment-3505807",
+					"display_url": "gist.github.com/oliverandrich/…",
+					"indices": [
+						94,
+						117
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1324755852989993000,
+		"in_reply_to_status_id_str": "1324755852989992967",
+		"in_reply_to_user_id": 13598112,
+		"in_reply_to_user_id_str": "13598112",
+		"in_reply_to_screen_name": "marco79",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 845,
+			"friends_count": 157,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20683,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91698947,
+				6.96589202
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96589202,
+				50.91698947
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "@marco79 @DerZyklop So ist’s cool, danke. Dann wollte ich noch neunmalschlau den hier machen: https://t.co/Y7OQXkwyxi Aber dann wird die Infektionszahl bei mir doppelt angezeigt – einmal mit, einmal ohne Punkt. (Obwohl ein Komma Strenggenommen besser wäre.)"
+	},
+	{
 		"created_at": "Fri Nov 06 15:51:57 +0000 2020",
 		"id": 1324741320104157200,
 		"id_str": "1324741320104157184",
