7576e995deb234ec0555f797495ba91b4516b040
-drik S <drikkes@gmail.com>
2020-11-12 15:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66894,
-	"created_at": "Thu Nov 12 13:11:19 +0000 2020",
+	"tweets": 66895,
+	"created_at": "Thu Nov 12 14:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Thu Nov 12 14:03:27 +0000 2020",
+		"id": 1326888339534991400,
+		"id_str": "1326888339534991360",
+		"full_text": "@adidas Kann mich zur Ruhe setzen, habe alles erreicht. (D. i. in der Bestellbestätigungsmail auf einen Link geklickt.) https://t.co/pftc59WWNc",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			119
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "adidas",
+					"name": "adidas",
+					"id": 300114634,
+					"id_str": "300114634",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1326888336292765700,
+					"id_str": "1326888336292765698",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/EmoN6WVW4AI234b.png",
+					"media_url_https": "https://pbs.twimg.com/media/EmoN6WVW4AI234b.png",
+					"url": "https://t.co/pftc59WWNc",
+					"display_url": "pic.twitter.com/pftc59WWNc",
+					"expanded_url": "https://twitter.com/drikkes/status/1326888339534991360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 476,
+							"h": 195,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 476,
+							"h": 195,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 476,
+							"h": 195,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1326888336292765700,
+					"id_str": "1326888336292765698",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/EmoN6WVW4AI234b.png",
+					"media_url_https": "https://pbs.twimg.com/media/EmoN6WVW4AI234b.png",
+					"url": "https://t.co/pftc59WWNc",
+					"display_url": "pic.twitter.com/pftc59WWNc",
+					"expanded_url": "https://twitter.com/drikkes/status/1326888339534991360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 476,
+							"h": 195,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 476,
+							"h": 195,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 476,
+							"h": 195,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1326871058058797000,
+		"in_reply_to_status_id_str": "1326871058058797066",
+		"in_reply_to_user_id": 15378060,
+		"in_reply_to_user_id_str": "15378060",
+		"in_reply_to_screen_name": "drikkes",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 846,
+			"friends_count": 157,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5259,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20700,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91703056,
+				6.96585937
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96585937,
+				50.91703056
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "@adidas Kann mich zur Ruhe setzen, habe alles erreicht. (D. i. in der Bestellbestätigungsmail auf einen Link geklickt.) https://t.co/pftc59WWNc"
+	},
+	{
 		"created_at": "Thu Nov 12 12:54:46 +0000 2020",
 		"id": 1326871058058797000,
 		"id_str": "1326871058058797066",
