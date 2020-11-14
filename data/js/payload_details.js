cfef85732fd4a18fa016517f779e8518c0e812f0
-drik S <drikkes@gmail.com>
2020-11-14 12:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66903,
-	"created_at": "Sat Nov 14 10:11:18 +0000 2020",
+	"tweets": 66904,
+	"created_at": "Sat Nov 14 11:11:18 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,303 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Sat Nov 14 10:24:45 +0000 2020",
+		"id": 1327558077202231300,
+		"id_str": "1327558077202231298",
+		"full_text": "Bisher nur vier Rechtschreibkorrekturen in den Replies, da geht noch was. https://t.co/91BPfTa1c6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/91BPfTa1c6",
+					"expanded_url": "https://twitter.com/Antje_Kapek/status/1327516540267327494",
+					"display_url": "twitter.com/Antje_Kapek/st…",
+					"indices": [
+						74,
+						97
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 846,
+			"friends_count": 157,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5263,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20709,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "255115bcb65f5da6",
+			"url": "https://api.twitter.com/1.1/geo/id/255115bcb65f5da6.json",
+			"place_type": "neighborhood",
+			"name": "Bayenthal",
+			"full_name": "Bayenthal, Köln",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.960374,
+							50.903919
+						],
+						[
+							6.979611,
+							50.903919
+						],
+						[
+							6.979611,
+							50.917496
+						],
+						[
+							6.960374,
+							50.917496
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1327516540267327500,
+		"quoted_status_id_str": "1327516540267327494",
+		"quoted_status_permalink": {
+			"url": "https://t.co/91BPfTa1c6",
+			"expanded": "https://twitter.com/Antje_Kapek/status/1327516540267327494",
+			"display": "twitter.com/Antje_Kapek/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Nov 14 07:39:41 +0000 2020",
+			"id": 1327516540267327500,
+			"id_str": "1327516540267327494",
+			"full_text": "Die #Niederlande verbieten das #Sylvester Feuerwerk.\n\nDas ist ein wichtiger Schritt, um Krankenhäuser zu entlasten, Sanitäter zu schützen und Neuinfektionen zu verhindern.\n\nIch möchte, dass Berlin und Deutschland folgen. Sylvester in der #Coronakrise muss #stayathome heißen! https://t.co/eewS8Ym2Hr",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				275
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Niederlande",
+						"indices": [
+							4,
+							16
+						]
+					},
+					{
+						"text": "Sylvester",
+						"indices": [
+							31,
+							41
+						]
+					},
+					{
+						"text": "Coronakrise",
+						"indices": [
+							238,
+							250
+						]
+					},
+					{
+						"text": "stayathome",
+						"indices": [
+							256,
+							267
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/eewS8Ym2Hr",
+						"expanded_url": "https://twitter.com/tagesschau/status/1327269073156272134",
+						"display_url": "twitter.com/tagesschau/sta…",
+						"indices": [
+							276,
+							299
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 410237349,
+				"id_str": "410237349",
+				"name": "Antje Kapek",
+				"screen_name": "Antje_Kapek",
+				"location": "Berlin",
+				"description": "#Grün. #Links. #Berlin. #Europäerin #Demokratin #Antifaschistin #Feministin #Fraktionsvorsitzende der @GrueneFraktionB im #Abgeordnetenhaus von Berlin #R2G",
+				"url": "https://t.co/sJsBgJr7eM",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/sJsBgJr7eM",
+								"expanded_url": "http://www.antje-kapek.de",
+								"display_url": "antje-kapek.de",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6725,
+				"friends_count": 1239,
+				"listed_count": 180,
+				"created_at": "Fri Nov 11 20:46:17 +0000 2011",
+				"favourites_count": 15037,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 11338,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1314155233493372928/2bGZnB9v_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1314155233493372928/2bGZnB9v_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/410237349/1590734124",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1327269073156272000,
+			"quoted_status_id_str": "1327269073156272134",
+			"quoted_status_permalink": {
+				"url": "https://t.co/eewS8Ym2Hr",
+				"expanded": "https://twitter.com/tagesschau/status/1327269073156272134",
+				"display": "twitter.com/tagesschau/sta…"
+			},
+			"retweet_count": 39,
+			"favorite_count": 187,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Bisher nur vier Rechtschreibkorrekturen in den Replies, da geht noch was. https://t.co/91BPfTa1c6"
+	},
+	{
 		"created_at": "Sat Nov 14 10:09:59 +0000 2020",
 		"id": 1327554363120873500,
 		"id_str": "1327554363120873472",
