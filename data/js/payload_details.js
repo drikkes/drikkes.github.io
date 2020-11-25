6a9d33b7d8fe1660e8da89030d5cf7003d3f816c
-drik S <drikkes@gmail.com>
2020-11-25 12:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 66946,
-	"created_at": "Wed Nov 25 10:11:20 +0000 2020",
+	"tweets": 66947,
+	"created_at": "Wed Nov 25 11:11:19 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_11.js
+++ data/js/tweets/2020_11.js
@@ -1,6 +1,246 @@
 Grailbird.data.tweets_2020_11 = 
 [
 	{
+		"created_at": "Wed Nov 25 10:23:01 +0000 2020",
+		"id": 1331543907138756600,
+		"id_str": "1331543907138756608",
+		"full_text": "@KurtProedel Wenn man sich damals das hier näher angeschaut hätte, kann einen diese Entwicklung allerdings kaum überraschen. https://t.co/c7n9ZqbgwR",
+		"truncated": false,
+		"display_text_range": [
+			13,
+			124
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KurtProedel",
+					"name": "Kurt „TOP 50“ Prödel",
+					"id": 2970160174,
+					"id_str": "2970160174",
+					"indices": [
+						0,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1331543903389016000,
+					"id_str": "1331543903389016066",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EnqYIBRW4AIroi5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnqYIBRW4AIroi5.jpg",
+					"url": "https://t.co/c7n9ZqbgwR",
+					"display_url": "pic.twitter.com/c7n9ZqbgwR",
+					"expanded_url": "https://twitter.com/drikkes/status/1331543907138756608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 268,
+							"h": 335,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 268,
+							"h": 335,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 268,
+							"h": 335,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1331543903389016000,
+					"id_str": "1331543903389016066",
+					"indices": [
+						125,
+						148
+					],
+					"media_url": "http://pbs.twimg.com/media/EnqYIBRW4AIroi5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EnqYIBRW4AIroi5.jpg",
+					"url": "https://t.co/c7n9ZqbgwR",
+					"display_url": "pic.twitter.com/c7n9ZqbgwR",
+					"expanded_url": "https://twitter.com/drikkes/status/1331543907138756608/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 268,
+							"h": 335,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 268,
+							"h": 335,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 268,
+							"h": 335,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1331509106813890600,
+		"in_reply_to_status_id_str": "1331509106813890560",
+		"in_reply_to_user_id": 2970160174,
+		"in_reply_to_user_id_str": "2970160174",
+		"in_reply_to_screen_name": "KurtProedel",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. https://t.co/pAb0SRWGFm",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/pAb0SRWGFm",
+							"expanded_url": "http://drikk.es",
+							"display_url": "drikk.es",
+							"indices": [
+								46,
+								69
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 844,
+			"friends_count": 157,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5274,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20757,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91701749,
+				6.9659048
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.9659048,
+				50.91701749
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "@KurtProedel Wenn man sich damals das hier näher angeschaut hätte, kann einen diese Entwicklung allerdings kaum überraschen. https://t.co/c7n9ZqbgwR"
+	},
+	{
 		"created_at": "Wed Nov 25 09:59:19 +0000 2020",
 		"id": 1331537944625209300,
 		"id_str": "1331537944625209344",
