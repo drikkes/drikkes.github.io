6d13645e36a65008c6a9dcbee8526974641be578
-drik S <drikkes@gmail.com>
2021-01-10 14:11:20 +0100

    Added 1 tweets

--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 10 12:50:11 +0000 2021",
+		"id": 1348214497199140900,
+		"id_str": "1348250788620087296",
+		"full_text": "Hello Today. Ingeborg Bachmann is not on TikTok https://t.co/CZfRRC9SEX https://t.co/QAPRWMHfDB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CZfRRC9SEX",
+					"expanded_url": "https://anchor.fm/hellotoday/episodes/Hello-Today-010-eooois",
+					"display_url": "anchor.fm/hellotoday/epi…",
+					"indices": [
+						48,
+						71
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1348214491989815300,
+					"id_str": "1348214491989815298",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/ErXR7_QWMAIdDzP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErXR7_QWMAIdDzP.jpg",
+					"url": "https://t.co/QAPRWMHfDB",
+					"display_url": "pic.twitter.com/QAPRWMHfDB",
+					"expanded_url": "https://twitter.com/m_boesch/status/1348214497199140865/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 973,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1900,
+							"h": 1541,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 552,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1348214491989815300,
+					"id_str": "1348214491989815298",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/ErXR7_QWMAIdDzP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ErXR7_QWMAIdDzP.jpg",
+					"url": "https://t.co/QAPRWMHfDB",
+					"display_url": "pic.twitter.com/QAPRWMHfDB",
+					"expanded_url": "https://twitter.com/m_boesch/status/1348214497199140865/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 973,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1900,
+							"h": 1541,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 552,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14287222,
+			"id_str": "14287222",
+			"name": "Marcus Bösch",
+			"screen_name": "m_boesch",
+			"location": "Berlin, Deutschland",
+			"description": "Workshops & Facilitation. Daily Podcast: Hello Today https://t.co/sbLWBn6mxT Weekly Newsletter: Understanding TikTok https://t.co/JcTY6wQAvw",
+			"url": "https://t.co/1iUAbVNIM4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1iUAbVNIM4",
+							"expanded_url": "https://www.marcus-boesch.de",
+							"display_url": "marcus-boesch.de",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/sbLWBn6mxT",
+							"expanded_url": "http://anchor.fm/hellotoday",
+							"display_url": "anchor.fm/hellotoday",
+							"indices": [
+								53,
+								76
+							]
+						},
+						{
+							"url": "https://t.co/JcTY6wQAvw",
+							"expanded_url": "http://tiktoktiktoktiktok.substack.com",
+							"display_url": "tiktoktiktoktiktok.substack.com",
+							"indices": [
+								117,
+								140
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 4351,
+			"friends_count": 1594,
+			"listed_count": 349,
+			"created_at": "Wed Apr 02 20:31:01 +0000 2008",
+			"favourites_count": 11384,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 9547,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1021892786050744320/oZaDCcud_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1021892786050744320/oZaDCcud_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14287222/1559138821",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "0E120A",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "504141",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hello Today. Ingeborg Bachmann is not on TikTok https://t.co/CZfRRC9SEX https://t.co/QAPRWMHfDB"
+	},
+	{
 		"created_at": "Sun Jan 10 11:20:48 +0000 2021",
 		"id": 1348228294630629400,
 		"id_str": "1348228294630629377",
