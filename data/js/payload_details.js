87f8b356d54406f22b0938f13375cdd92c870096
-drik S <drikkes@gmail.com>
2021-01-21 08:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67163,
-	"created_at": "Wed Jan 20 07:11:19 +0000 2021",
+	"tweets": 67164,
+	"created_at": "Thu Jan 21 07:11:19 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,226 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Thu Jan 21 07:01:30 +0000 2021",
+		"id": 1352070155141079000,
+		"id_str": "1352149306212569089",
+		"full_text": "I made a website where you can put bernie in places using google maps street view. Enjoy!\n\nhttps://t.co/UfY5g9xU2k https://t.co/8rstiEXOHf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			114
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/UfY5g9xU2k",
+					"expanded_url": "https://bernie-sits.herokuapp.com/",
+					"display_url": "bernie-sits.herokuapp.com",
+					"indices": [
+						91,
+						114
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1352069950060560400,
+					"id_str": "1352069950060560387",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/EsOEdLzXAAM2cNB.png",
+					"media_url_https": "https://pbs.twimg.com/media/EsOEdLzXAAM2cNB.png",
+					"url": "https://t.co/8rstiEXOHf",
+					"display_url": "pic.twitter.com/8rstiEXOHf",
+					"expanded_url": "https://twitter.com/nick_sawhney/status/1352070155141079043/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1352069950060560400,
+					"id_str": "1352069950060560387",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/EsOEdLzXAAM2cNB.png",
+					"media_url_https": "https://pbs.twimg.com/media/EsOEdLzXAAM2cNB.png",
+					"url": "https://t.co/8rstiEXOHf",
+					"display_url": "pic.twitter.com/8rstiEXOHf",
+					"expanded_url": "https://twitter.com/nick_sawhney/status/1352070155141079043/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1352070132353429500,
+					"id_str": "1352070132353429506",
+					"indices": [
+						115,
+						138
+					],
+					"media_url": "http://pbs.twimg.com/media/EsOEny5XcAIUEDR.png",
+					"media_url_https": "https://pbs.twimg.com/media/EsOEny5XcAIUEDR.png",
+					"url": "https://t.co/8rstiEXOHf",
+					"display_url": "pic.twitter.com/8rstiEXOHf",
+					"expanded_url": "https://twitter.com/nick_sawhney/status/1352070155141079043/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 747249833778389000,
+			"id_str": "747249833778388992",
+			"name": "nick",
+			"screen_name": "nick_sawhney",
+			"location": "Lower East Side, Manhattan",
+			"description": "human shaped cup of cold brew // getting my masters @ NYU // occasional host @wnyu // i like to study transit policing, urban science, and software engineering",
+			"url": "https://t.co/xEeKGcYqoF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xEeKGcYqoF",
+							"expanded_url": "http://nicksawhney.github.io/me/",
+							"display_url": "nicksawhney.github.io/me/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 595,
+			"friends_count": 432,
+			"listed_count": 6,
+			"created_at": "Mon Jun 27 02:06:58 +0000 2016",
+			"favourites_count": 5808,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 951,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1308194513513578499/0xQ77kIu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1308194513513578499/0xQ77kIu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/747249833778388992/1583015500",
+			"profile_link_color": "000080",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8229,
+		"favorite_count": 39534,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I made a website where you can put bernie in places using google maps street view. Enjoy!\n\nhttps://t.co/UfY5g9xU2k https://t.co/8rstiEXOHf"
+	},
+	{
 		"created_at": "Wed Jan 20 06:43:19 +0000 2021",
 		"id": 1325275427355553800,
 		"id_str": "1351782339131682829",
