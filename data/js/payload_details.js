75b0ebb94370562325cf3d64c568db393a3e198e
-drik S <drikkes@gmail.com>
2021-01-31 10:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67200,
-	"created_at": "Sat Jan 30 14:11:19 +0000 2021",
+	"tweets": 67201,
+	"created_at": "Sun Jan 31 09:11:18 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 31 08:18:26 +0000 2021",
+		"id": 1355792543141781500,
+		"id_str": "1355792543141781505",
+		"full_text": "#clubmateflascheumtretundüberdenbodenkullersoundeffekt https://t.co/pS6ZTKCaqK",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			54
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "clubmateflascheumtretundüberdenbodenkullersoundeffekt",
+					"indices": [
+						0,
+						54
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pS6ZTKCaqK",
+					"expanded_url": "https://twitter.com/schnellenbachj/status/1355630603450900484",
+					"display_url": "twitter.com/schnellenbachj…",
+					"indices": [
+						55,
+						78
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 843,
+			"friends_count": 159,
+			"listed_count": 77,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5359,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21038,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "255115bcb65f5da6",
+			"url": "https://api.twitter.com/1.1/geo/id/255115bcb65f5da6.json",
+			"place_type": "neighborhood",
+			"name": "Bayenthal",
+			"full_name": "Bayenthal, Köln",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.960374,
+							50.903919
+						],
+						[
+							6.979611,
+							50.903919
+						],
+						[
+							6.979611,
+							50.917496
+						],
+						[
+							6.960374,
+							50.917496
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1355630603450900500,
+		"quoted_status_id_str": "1355630603450900484",
+		"quoted_status_permalink": {
+			"url": "https://t.co/pS6ZTKCaqK",
+			"expanded": "https://twitter.com/schnellenbachj/status/1355630603450900484",
+			"display": "twitter.com/schnellenbachj…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Jan 30 21:34:56 +0000 2021",
+			"id": 1355630603450900500,
+			"id_str": "1355630603450900484",
+			"full_text": "Wieso gibt es bei Clubhouse nur die Option \"Leave quietly\", aber nicht die Option \"Leave, throwing over a table, slamming the door, and shouting obscenities?\"",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				158
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 829677473042485200,
+				"id_str": "829677473042485249",
+				"name": "Jan Schnellenbach",
+				"screen_name": "schnellenbachj",
+				"location": "Solingen & Cottbus",
+				"description": "Economics Prof @ BTU CS. \n\nAll opinions expressed here are strictly my own.\n\nPolitical Econ, Public Finance, Behavioral Econ, Federalism.",
+				"url": "https://t.co/Em90V9HAGZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Em90V9HAGZ",
+								"expanded_url": "https://www.b-tu.de/fg-vwl-mikro/team/prof-dr-jan-schnellenbach",
+								"display_url": "b-tu.de/fg-vwl-mikro/t…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9202,
+				"friends_count": 2097,
+				"listed_count": 77,
+				"created_at": "Thu Feb 09 13:04:59 +0000 2017",
+				"favourites_count": 45377,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 19961,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1250547807020290049/vetYa3_i_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1250547807020290049/vetYa3_i_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/829677473042485249/1601230658",
+				"profile_link_color": "000000",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 12,
+			"favorite_count": 215,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "#clubmateflascheumtretundüberdenbodenkullersoundeffekt https://t.co/pS6ZTKCaqK"
+	},
+	{
 		"created_at": "Sat Jan 30 13:41:06 +0000 2021",
 		"id": 1355511356611584000,
 		"id_str": "1355511356611584001",
