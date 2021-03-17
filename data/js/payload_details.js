caf23764a92c93795f0a04338314ce281f138036
-drik S <drikkes@gmail.com>
2021-03-17 13:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67325,
-	"created_at": "Wed Mar 17 00:11:17 +0000 2021",
+	"tweets": 67326,
+	"created_at": "Wed Mar 17 12:11:17 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,131 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Wed Mar 17 11:30:47 +0000 2021",
+		"id": 1370644197754146800,
+		"id_str": "1372148402671194114",
+		"full_text": "Wie man mit einer fein geschliffenen Axt ein Hausboot versenkt. Maren Haffke im #Kompressor @dlfkultur. https://t.co/kH5BaMTbNu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			127
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Kompressor",
+					"indices": [
+						80,
+						91
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dlfkultur",
+					"name": "Deutschlandfunk Kultur",
+					"id": 320412967,
+					"id_str": "320412967",
+					"indices": [
+						92,
+						102
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kH5BaMTbNu",
+					"expanded_url": "https://www.deutschlandfunkkultur.de/hausboot-mit-olli-schulz-und-fynn-kliemann-bromance-auf-dem.2156.de.html?dram:article_id=493921",
+					"display_url": "deutschlandfunkkultur.de/hausboot-mit-o…",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 244010115,
+			"id_str": "244010115",
+			"name": "Radiokritik",
+			"screen_name": "Radiokritik",
+			"location": "Berlin",
+			"description": "Sachdienliche Hinweise zur #Hörspiel-Kritik  ... irgendjemand muss es ja machen. Drahtlose Belehrung und Unterhaltung.",
+			"url": "http://t.co/UrtEeUPtxC",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/UrtEeUPtxC",
+							"expanded_url": "http://www.hoerspielkritik.de",
+							"display_url": "hoerspielkritik.de",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 518,
+			"friends_count": 495,
+			"listed_count": 19,
+			"created_at": "Fri Jan 28 09:05:49 +0000 2011",
+			"favourites_count": 1524,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2186,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2381618876/fd9wrlt8o4oqg2gtrwmk_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2381618876/fd9wrlt8o4oqg2gtrwmk_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/244010115/1563626297",
+			"profile_link_color": "2FC2EF",
+			"profile_sidebar_border_color": "181A1E",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Wie man mit einer fein geschliffenen Axt ein Hausboot versenkt. Maren Haffke im #Kompressor @dlfkultur. https://t.co/kH5BaMTbNu"
+	},
+	{
 		"created_at": "Tue Mar 16 23:58:16 +0000 2021",
 		"id": 1371974126198530000,
 		"id_str": "1371974126198530052",
