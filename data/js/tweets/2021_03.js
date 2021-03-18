0e49ddf20a560e96ba1eb082ef658e9b6d795e6f
-drik S <drikkes@gmail.com>
2021-03-18 23:11:18 +0100

    Added 1 tweets

--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,109 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Thu Mar 18 21:28:33 +0000 2021",
+		"id": 1372537725275009000,
+		"id_str": "1372661223746068480",
+		"full_text": "\"When you do your first installation, your heart will be working harder than Donald Trump’s lawyers.\" \n\nFor the new followers interested in taking up subvertising, here's a how-to guide by the co-founder of Brandalism. \n\nhttps://t.co/C7l6Y0Txb1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			244
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/C7l6Y0Txb1",
+					"expanded_url": "https://www.actionstories.no/stories-actions/ad-shell-subvertising",
+					"display_url": "actionstories.no/stories-action…",
+					"indices": [
+						221,
+						244
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3188490303,
+			"id_str": "3188490303",
+			"name": "AdDistortion",
+			"screen_name": "DistortedAd",
+			"location": "",
+			"description": "All news related to the subverting, hijacking & hacking of advertising spaces. \n\nFor a history of #Subvertising: https://t.co/63U5phOYPO\n\nDMs open, send your subverts.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/63U5phOYPO",
+							"expanded_url": "http://bit.ly/2FKoPiv",
+							"display_url": "bit.ly/2FKoPiv",
+							"indices": [
+								113,
+								136
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 2459,
+			"friends_count": 65,
+			"listed_count": 24,
+			"created_at": "Mon Apr 20 22:09:42 +0000 2015",
+			"favourites_count": 256,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 918,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/590613492576739329/qPx_zez2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/590613492576739329/qPx_zez2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3188490303/1595004830",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"When you do your first installation, your heart will be working harder than Donald Trump’s lawyers.\" \n\nFor the new followers interested in taking up subvertising, here's a how-to guide by the co-founder of Brandalism. \n\nhttps://t.co/C7l6Y0Txb1"
+	},
+	{
 		"created_at": "Thu Mar 18 09:22:46 +0000 2021",
 		"id": 1372450706859393000,
 		"id_str": "1372478576621158412",
