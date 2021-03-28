2515f5b98965fddd8a48941c03114fb86f736a6c
-drik S <drikkes@gmail.com>
2021-03-28 16:11:19 +0200

    Added 1 tweets

--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,185 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Sun Mar 28 13:54:35 +0000 2021",
+		"id": 1376170859501662200,
+		"id_str": "1376170859501662212",
+		"full_text": "@henrikgreger @akk In #s01e05 tauchen zum ersten Mal die Ferengi auf. Werde berichten, falls ich dabei etwas über korrupte Politiker und die @fdp lerne.",
+		"truncated": false,
+		"display_text_range": [
+			19,
+			152
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "s01e05",
+					"indices": [
+						22,
+						29
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "henrikgreger",
+					"name": "henrik greger",
+					"id": 10768042,
+					"id_str": "10768042",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "akk",
+					"name": "A. Kramp-Karrenbauer",
+					"id": 47620601,
+					"id_str": "47620601",
+					"indices": [
+						14,
+						18
+					]
+				},
+				{
+					"screen_name": "fdp",
+					"name": "FDP",
+					"id": 39475170,
+					"id_str": "39475170",
+					"indices": [
+						141,
+						145
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1376167843868381200,
+		"in_reply_to_status_id_str": "1376167843868381188",
+		"in_reply_to_user_id": 10768042,
+		"in_reply_to_user_id_str": "10768042",
+		"in_reply_to_screen_name": "henrikgreger",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 840,
+			"friends_count": 158,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5398,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21215,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1242912774881189888/5hMWLi-q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91699219,
+				6.9658948
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.9658948,
+				50.91699219
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@henrikgreger @akk In #s01e05 tauchen zum ersten Mal die Ferengi auf. Werde berichten, falls ich dabei etwas über korrupte Politiker und die @fdp lerne."
+	},
+	{
 		"created_at": "Sun Mar 28 11:11:01 +0000 2021",
 		"id": 1376129696857997300,
 		"id_str": "1376129696857997312",
