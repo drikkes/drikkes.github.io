d7c092b1ecf25678a90bf76deb1f2c84d12e599c
-drik S <drikkes@gmail.com>
2021-04-13 22:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67420,
-	"created_at": "Tue Apr 13 15:11:17 +0000 2021",
+	"tweets": 67421,
+	"created_at": "Tue Apr 13 20:11:18 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Tue Apr 13 19:21:58 +0000 2021",
+		"id": 1345331673119019000,
+		"id_str": "1382051452558962691",
+		"full_text": "WE LIKE TO PARTY !\n(THE VENGABUS)\n@vengaboys https://t.co/apoimLxgRB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "vengaboys",
+					"name": "Vengaboys",
+					"id": 80600126,
+					"id_str": "80600126",
+					"indices": [
+						34,
+						44
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1345331479501541400,
+					"id_str": "1345331479501541381",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1345331479501541381/pu/img/LSJKXJjzEk9sa1ls.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1345331479501541381/pu/img/LSJKXJjzEk9sa1ls.jpg",
+					"url": "https://t.co/apoimLxgRB",
+					"display_url": "pic.twitter.com/apoimLxgRB",
+					"expanded_url": "https://twitter.com/damienrobi/status/1345331673119019009/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1345331479501541400,
+					"id_str": "1345331479501541381",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1345331479501541381/pu/img/LSJKXJjzEk9sa1ls.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1345331479501541381/pu/img/LSJKXJjzEk9sa1ls.jpg",
+					"url": "https://t.co/apoimLxgRB",
+					"display_url": "pic.twitter.com/apoimLxgRB",
+					"expanded_url": "https://twitter.com/damienrobi/status/1345331673119019009/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 383,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1280,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 675,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							9,
+							16
+						],
+						"duration_millis": 127305,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1345331479501541381/pu/pl/ruWl7fNxi3dE4aiy.m3u8?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1345331479501541381/pu/vid/320x568/NMIgZqX2Rww9T3eP.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1345331479501541381/pu/vid/360x640/3AV5t1BnIdbg8Jql.mp4?tag=10"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1345331479501541381/pu/vid/720x1280/BSWNL-_WLPV8HNB5.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 173809097,
+			"id_str": "173809097",
+			"name": "Damien Robitaille",
+			"screen_name": "damienrobi",
+			"location": "Montreal : Canada",
+			"description": "Trying to spread joy through music 😁🎹Business/ media inquiries :info@9evague.com Tips https://t.co/xIZEwVmMq8",
+			"url": "https://t.co/1cjzeTE8hi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1cjzeTE8hi",
+							"expanded_url": "https://youtube.com/channel/UCstkgUh55fWiwSWGRctbVbg",
+							"display_url": "youtube.com/channel/UCstkg…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/xIZEwVmMq8",
+							"expanded_url": "https://paypal.me/DamienRobitaille",
+							"display_url": "paypal.me/DamienRobitail…",
+							"indices": [
+								86,
+								109
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 72475,
+			"friends_count": 1453,
+			"listed_count": 205,
+			"created_at": "Mon Aug 02 12:13:00 +0000 2010",
+			"favourites_count": 9538,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 4310,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1246415456468119557/r-X2TEqV_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1246415456468119557/r-X2TEqV_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/173809097/1606924289",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 672,
+		"favorite_count": 4915,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "WE LIKE TO PARTY !\n(THE VENGABUS)\n@vengaboys https://t.co/apoimLxgRB"
+	},
+	{
 		"created_at": "Tue Apr 13 14:33:21 +0000 2021",
 		"id": 1381708881034940400,
 		"id_str": "1381978818953551880",
