88e0b53165073578e2626c2917eeb2cc50af454c
-drik S <drikkes@gmail.com>
2021-04-24 21:11:18 +0200

    Added 1 tweets

--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sat Apr 24 18:58:36 +0000 2021",
+		"id": 1386030441560375300,
+		"id_str": "1386031840285310976",
+		"full_text": "Fly fishers hit out at Supreme’s ‘laughable’ range of utility wear https://t.co/5zF3aNCV25",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5zF3aNCV25",
+					"expanded_url": "https://www.theguardian.com/fashion/2021/apr/24/fly-fishers-hit-out-at-supremes-range-of-utility-wear?utm_source=dlvr.it&utm_medium=twitter",
+					"display_url": "theguardian.com/fashion/2021/a…",
+					"indices": [
+						67,
+						90
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://dlvrit.com/\" rel=\"nofollow\">dlvr.it</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 308442339,
+			"id_str": "308442339",
+			"name": "Guardian Fashion",
+			"screen_name": "GuardianFashion",
+			"location": "",
+			"description": "The Twitter home of the @Guardian's Fashion team. Instagram: @guardianfashion",
+			"url": "http://t.co/RnMJXd5ISR",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/RnMJXd5ISR",
+							"expanded_url": "http://theguardian.com/fashion",
+							"display_url": "theguardian.com/fashion",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 41396,
+			"friends_count": 873,
+			"listed_count": 538,
+			"created_at": "Tue May 31 12:29:35 +0000 2011",
+			"favourites_count": 37,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 12458,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1061928551711195136/5OSAuMU0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1061928551711195136/5OSAuMU0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/308442339/1542018479",
+			"profile_link_color": "005689",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fly fishers hit out at Supreme’s ‘laughable’ range of utility wear https://t.co/5zF3aNCV25"
+	},
+	{
 		"created_at": "Sat Apr 24 09:38:27 +0000 2021",
 		"id": 1385890871854391300,
 		"id_str": "1385890871854391296",
