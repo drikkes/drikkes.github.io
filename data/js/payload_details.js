d4fc0b00e41c0b0239f907aa6862cac20391f314
-drik S <drikkes@gmail.com>
2021-06-06 12:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67602,
-	"created_at": "Sun Jun 06 09:11:17 +0000 2021",
+	"tweets": 67603,
+	"created_at": "Sun Jun 06 10:11:18 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_06.js
+++ data/js/tweets/2021_06.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2021_06 = 
 [
 	{
+		"created_at": "Sun Jun 06 09:21:41 +0000 2021",
+		"id": 1401060602370576400,
+		"id_str": "1401469334136967169",
+		"full_text": "Heute vor 250 Jahren kommt Ernst August I. in London zur Welt. Mit Victorias Thronbesteigung fällt Hannover an den skandalumwitterten britischen Königssohn. https://t.co/vNRfOMFJ0E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vNRfOMFJ0E",
+					"expanded_url": "http://www.ndr.de/geschichte/koepfe/Vor-250-Jahren-wurde-Hannovers-Koenig-Ernst-August-I-geboren,ernstaugust174.html",
+					"display_url": "ndr.de/geschichte/koe…",
+					"indices": [
+						157,
+						180
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 132677711,
+			"id_str": "132677711",
+			"name": "NDR.de",
+			"screen_name": "ndr",
+			"location": "Norddeutschland",
+			"description": "Nachrichten aus Hamburg, Niedersachsen, Schleswig-Holstein und Mecklenburg-Vorpommern. Hier twittert die Online-Redaktion. | Impressum: http://t.co/uuRQxVnFeP",
+			"url": "http://t.co/eDvglF9DgJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/eDvglF9DgJ",
+							"expanded_url": "http://www.ndr.de",
+							"display_url": "ndr.de",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "http://t.co/uuRQxVnFeP",
+							"expanded_url": "http://ndr.de/impressum",
+							"display_url": "ndr.de/impressum",
+							"indices": [
+								136,
+								158
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 176311,
+			"friends_count": 68,
+			"listed_count": 1446,
+			"created_at": "Tue Apr 13 22:14:40 +0000 2010",
+			"favourites_count": 4323,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 63740,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "2671C6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1156477415947984896/-CuyGRgs_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1156477415947984896/-CuyGRgs_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/132677711/1585675478",
+			"profile_link_color": "3366B8",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "92B3E8",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Heute vor 250 Jahren kommt Ernst August I. in London zur Welt. Mit Victorias Thronbesteigung fällt Hannover an den skandalumwitterten britischen Königssohn. https://t.co/vNRfOMFJ0E"
+	},
+	{
 		"created_at": "Sun Jun 06 08:53:16 +0000 2021",
 		"id": 1401462179262603300,
 		"id_str": "1401462179262603265",
