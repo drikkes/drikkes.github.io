415520fc07158a3e37f450afffabbed0c8a0f169
-drik S <drikkes@gmail.com>
2021-08-07 11:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67786,
-	"created_at": "Fri Aug 06 22:11:17 +0000 2021",
+	"tweets": 67787,
+	"created_at": "Sat Aug 07 09:11:17 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_08.js
+++ data/js/tweets/2021_08.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2021_08 = 
 [
 	{
+		"created_at": "Sat Aug 07 08:35:32 +0000 2021",
+		"id": 1423925764672610300,
+		"id_str": "1423925764672610309",
+		"full_text": "@googlemaps @googleeurope @GoogleDE @killedbygoogle I can‘t understand, why you put down this super-useful widget feature. https://t.co/8SrfV4xQOn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			146
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "googlemaps",
+					"name": "Google Maps",
+					"id": 31311757,
+					"id_str": "31311757",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "googleeurope",
+					"name": "Google Europe",
+					"id": 743699428448469000,
+					"id_str": "743699428448468992",
+					"indices": [
+						12,
+						25
+					]
+				},
+				{
+					"screen_name": "GoogleDE",
+					"name": "Google Deutschland",
+					"id": 52407579,
+					"id_str": "52407579",
+					"indices": [
+						26,
+						35
+					]
+				},
+				{
+					"screen_name": "killedbygoogle",
+					"name": "Killed by Google 🔪",
+					"id": 1041175266133827600,
+					"id_str": "1041175266133827585",
+					"indices": [
+						36,
+						51
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8SrfV4xQOn",
+					"expanded_url": "https://sites.google.com/site/iosandsightloss/google-public-transport-widget-iphone",
+					"display_url": "sites.google.com/site/iosandsig…",
+					"indices": [
+						123,
+						146
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1423251247868620800,
+		"in_reply_to_status_id_str": "1423251247868620800",
+		"in_reply_to_user_id": 15378060,
+		"in_reply_to_user_id_str": "15378060",
+		"in_reply_to_screen_name": "drikkes",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 822,
+			"friends_count": 153,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5518,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21675,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91702271,
+				6.96572708
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96572708,
+				50.91702271
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@googlemaps @googleeurope @GoogleDE @killedbygoogle I can‘t understand, why you put down this super-useful widget feature. https://t.co/8SrfV4xQOn"
+	},
+	{
 		"created_at": "Fri Aug 06 21:32:11 +0000 2021",
 		"id": 1423758826801700900,
 		"id_str": "1423758826801700868",
