28e800c373da3be22c1e3257035ce8d6ab1c1668
-drik S <drikkes@gmail.com>
2021-10-15 11:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 67996,
-	"created_at": "Thu Oct 14 11:11:18 +0000 2021",
+	"tweets": 67997,
+	"created_at": "Fri Oct 15 09:11:18 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Fri Oct 15 08:40:26 +0000 2021",
+		"id": 1448728822166208500,
+		"id_str": "1448931760637628416",
+		"full_text": "Wenn dich BraTee, DirTea und HafTea nerven, wünsch ich dir viel Spaß wenn nach der Legalisierung Hanfbefehl, Kushido und Capital Gras kommen, es wird alles so scheisse",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 969750482,
+			"id_str": "969750482",
+			"name": "E L   H O T Z O",
+			"screen_name": "elhotzo",
+			"location": "BI",
+			"description": "Liebhaber von Hunden und dieser einen zu starken Düse im Freibad\n\nlebendiges satire meme\n\nhe/him\n\nwichtigegeschaefte.elhotzo@heavygermanshit.de",
+			"url": "https://t.co/ihhNeQmCtH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ihhNeQmCtH",
+							"expanded_url": "https://www.heavygermanshit.de/sebastianhotz",
+							"display_url": "heavygermanshit.de/sebastianhotz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 216013,
+			"friends_count": 560,
+			"listed_count": 263,
+			"created_at": "Sun Nov 25 09:49:59 +0000 2012",
+			"favourites_count": 72032,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 39804,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/954348751799373825/_rztgdVC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/954348751799373825/_rztgdVC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/969750482/1613584569",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 115,
+		"favorite_count": 4126,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "de",
+		"text": "Wenn dich BraTee, DirTea und HafTea nerven, wünsch ich dir viel Spaß wenn nach der Legalisierung Hanfbefehl, Kushido und Capital Gras kommen, es wird alles so scheisse"
+	},
+	{
 		"created_at": "Thu Oct 14 10:50:01 +0000 2021",
 		"id": 1448601983825371100,
 		"id_str": "1448601983825371136",
