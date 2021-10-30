215b6581c67a99f3a73f84647095b2db7ae2a66e
-drik S <drikkes@gmail.com>
2021-10-30 14:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68035,
-	"created_at": "Sat Oct 30 09:11:18 +0000 2021",
+	"tweets": 68036,
+	"created_at": "Sat Oct 30 12:11:18 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 30 11:53:42 +0000 2021",
+		"id": 1454205158028632000,
+		"id_str": "1454416214952972292",
+		"full_text": "Sports Illustrated January 16 1978 https://t.co/z700fmEoay",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1454205154853630000,
+					"id_str": "1454205154853629953",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/FC5f4jWXsAE_jBR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FC5f4jWXsAE_jBR.jpg",
+					"url": "https://t.co/z700fmEoay",
+					"display_url": "pic.twitter.com/z700fmEoay",
+					"expanded_url": "https://twitter.com/pastvertising/status/1454205158028632071/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 664,
+							"h": 943,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 664,
+							"h": 943,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1454205154853630000,
+					"id_str": "1454205154853629953",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/FC5f4jWXsAE_jBR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FC5f4jWXsAE_jBR.jpg",
+					"url": "https://t.co/z700fmEoay",
+					"display_url": "pic.twitter.com/z700fmEoay",
+					"expanded_url": "https://twitter.com/pastvertising/status/1454205158028632071/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 479,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 664,
+							"h": 943,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 664,
+							"h": 943,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://github.com/spinfo/autoChirp\" rel=\"nofollow\">autoChirper</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1274426825528283100,
+			"id_str": "1274426825528283136",
+			"name": "Ads from the Past",
+			"screen_name": "pastvertising",
+			"location": "",
+			"description": "Ads from the Past. By @moritz_hoffmann, @cjahnz and @mschfr. German: @die_Reklame. No brand affiliation whatsoever, technology by @auto_chirp",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 217,
+			"friends_count": 7,
+			"listed_count": 2,
+			"created_at": "Sat Jun 20 19:40:13 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 165,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1434837135320879112/ajRsf-0m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1434837135320879112/ajRsf-0m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1274426825528283136/1630926406",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 31,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sports Illustrated January 16 1978 https://t.co/z700fmEoay"
+	},
+	{
 		"created_at": "Sat Oct 30 08:44:37 +0000 2021",
 		"id": 1454368632344039400,
 		"id_str": "1454368632344039425",
