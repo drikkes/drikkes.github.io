969493713058559edecc8dd24898bfecf6b64e76
-drik S <drikkes@gmail.com>
2021-11-25 01:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68116,
-	"created_at": "Wed Nov 24 19:11:18 +0000 2021",
+	"tweets": 68117,
+	"created_at": "Thu Nov 25 00:11:17 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Wed Nov 24 23:58:50 +0000 2021",
+		"id": 1463437642809315300,
+		"id_str": "1463658400626135053",
+		"full_text": "I love being surprised by questions in a good way during an interview. 🙂🔛🏅\n👇\nhttps://t.co/RsepqXaBxN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/RsepqXaBxN",
+					"expanded_url": "https://032c.com/chess-grandmaster-judit-polgar",
+					"display_url": "032c.com/chess-grandmas…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 189068159,
+			"id_str": "189068159",
+			"name": "Judit Polgar",
+			"screen_name": "GMJuditPolgar",
+			"location": "Budapest",
+			"description": "Chess Grandmaster led the World Rankings for 26 years. Ambassador of Chess in Education, UN Planet 50-50 Champion, Honorary VP of FIDE, European Chess Legend",
+			"url": "https://t.co/E9XB7ipewn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/E9XB7ipewn",
+							"expanded_url": "http://www.juditpolgar.com",
+							"display_url": "juditpolgar.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 82086,
+			"friends_count": 204,
+			"listed_count": 1100,
+			"created_at": "Fri Sep 10 08:57:16 +0000 2010",
+			"favourites_count": 83,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1718,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "352726",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1073854970242482176/b9tzmCUY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1073854970242482176/b9tzmCUY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/189068159/1637584059",
+			"profile_link_color": "D02B55",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "99CC33",
+			"profile_text_color": "3E4415",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 47,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I love being surprised by questions in a good way during an interview. 🙂🔛🏅\n👇\nhttps://t.co/RsepqXaBxN"
+	},
+	{
 		"created_at": "Wed Nov 24 18:24:46 +0000 2021",
 		"id": 1463574329359229000,
 		"id_str": "1463574329359228934",
