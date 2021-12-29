a7a5cb4827d8864e25249f0414ca8ca4ef7342b9
-drik S <drikkes@gmail.com>
2021-12-29 23:11:19 +0100

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2021_12.js",
 		"year": 2021,
 		"var_name": "tweets_2021_12",
-		"tweet_count": 108,
+		"tweet_count": 109,
 		"month": 12
 	},
 	{
