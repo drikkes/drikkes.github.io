2463d4b0b75c3a9b70d13d3df91e6c7f0ac806cd
-drik S <drikkes@gmail.com>
2022-03-24 08:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68546,
-	"created_at": "Wed Mar 23 19:11:17 +0000 2022",
+	"tweets": 68547,
+	"created_at": "Thu Mar 24 07:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 24 06:59:53 +0000 2022",
+		"id": 1506761113198743600,
+		"id_str": "1506888515715481600",
+		"full_text": "RIP Stephen Wilhite, creator of the GIF. This was the first GIF ever created in 1987. https://t.co/gqtMBqEOdg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1506761104411660300,
+					"id_str": "1506761104411660291",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOkXPcXVEAMVy5S.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOkXPcXVEAMVy5S.jpg",
+					"url": "https://t.co/gqtMBqEOdg",
+					"display_url": "pic.twitter.com/gqtMBqEOdg",
+					"expanded_url": "https://twitter.com/Blade__McG/status/1506761113198743554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 148,
+							"h": 148,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 400,
+							"h": 148,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 148,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 148,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506761104411660300,
+					"id_str": "1506761104411660291",
+					"indices": [
+						86,
+						109
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FOkXPcXVEAMVy5S.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FOkXPcXVEAMVy5S.jpg",
+					"url": "https://t.co/gqtMBqEOdg",
+					"display_url": "pic.twitter.com/gqtMBqEOdg",
+					"expanded_url": "https://twitter.com/Blade__McG/status/1506761113198743554/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 148,
+							"h": 148,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 400,
+							"h": 148,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 148,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 400,
+							"h": 148,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							100,
+							37
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FOkXPcXVEAMVy5S.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1466839987769385000,
+			"id_str": "1466839987769384961",
+			"name": "Blade McG",
+			"screen_name": "Blade__McG",
+			"location": "",
+			"description": "I am a sentient anomaly created from within the algorithm.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 275,
+			"friends_count": 631,
+			"listed_count": 2,
+			"created_at": "Fri Dec 03 18:41:25 +0000 2021",
+			"favourites_count": 27918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6162,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1484391628022312961/xjfqjBaD_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1484391628022312961/xjfqjBaD_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1466839987769384961/1646353731",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1261,
+		"favorite_count": 4688,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "RIP Stephen Wilhite, creator of the GIF. This was the first GIF ever created in 1987. https://t.co/gqtMBqEOdg"
+	},
+	{
 		"created_at": "Wed Mar 23 18:47:21 +0000 2022",
 		"id": 1506704165904523300,
 		"id_str": "1506704165904523265",
