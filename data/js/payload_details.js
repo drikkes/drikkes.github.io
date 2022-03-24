ac0f743a2e32afe60d1d527275c1d40a0d5e9058
-drik S <drikkes@gmail.com>
2022-03-24 10:11:17 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68547,
-	"created_at": "Thu Mar 24 07:11:18 +0000 2022",
+	"tweets": 68548,
+	"created_at": "Thu Mar 24 09:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Thu Mar 24 09:03:59 +0000 2022",
+		"id": 1506919747937570800,
+		"id_str": "1506919747937570816",
+		"full_text": "Werde älter. Neustartangst. https://t.co/LKtWDDBX17",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			27
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1506919744158552000,
+					"id_str": "1506919744158552066",
+					"indices": [
+						28,
+						51
+					],
+					"media_url": "http://pbs.twimg.com/media/FOmnhfeX0AIjbeJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/FOmnhfeX0AIjbeJ.png",
+					"url": "https://t.co/LKtWDDBX17",
+					"display_url": "pic.twitter.com/LKtWDDBX17",
+					"expanded_url": "https://twitter.com/drikkes/status/1506919747937570816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 37,
+							"h": 37,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 626,
+							"h": 37,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 626,
+							"h": 37,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 626,
+							"h": 37,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1506919744158552000,
+					"id_str": "1506919744158552066",
+					"indices": [
+						28,
+						51
+					],
+					"media_url": "http://pbs.twimg.com/media/FOmnhfeX0AIjbeJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/FOmnhfeX0AIjbeJ.png",
+					"url": "https://t.co/LKtWDDBX17",
+					"display_url": "pic.twitter.com/LKtWDDBX17",
+					"expanded_url": "https://twitter.com/drikkes/status/1506919747937570816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 37,
+							"h": 37,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 626,
+							"h": 37,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 626,
+							"h": 37,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 626,
+							"h": 37,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 814,
+			"friends_count": 153,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5733,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22486,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Werde älter. Neustartangst. https://t.co/LKtWDDBX17"
+	},
+	{
 		"created_at": "Thu Mar 24 06:59:53 +0000 2022",
 		"id": 1506761113198743600,
 		"id_str": "1506888515715481600",
