6c9a4eb252d72f0a99e3e82400ab4337563f9fa4
-drik S <drikkes@gmail.com>
2022-03-30 16:11:22 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68564,
-	"created_at": "Wed Mar 30 12:11:18 +0000 2022",
+	"tweets": 68565,
+	"created_at": "Wed Mar 30 14:11:21 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_03.js
+++ data/js/tweets/2022_03.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_03 = 
 [
 	{
+		"created_at": "Wed Mar 30 14:09:20 +0000 2022",
+		"id": 1509170917792006100,
+		"id_str": "1509170917792006149",
+		"full_text": "@lueckerath Klar. Aber umso erstaunlicher, dass er nach Filmen wie Sonatine, Hana-bi, Kikujiros Sommer oder Zatoichi wieder in die Rolle als Castle-Host zurückkehrt. Wäre vielleicht eine Erwähnung wert. Verstehe aber auch, daß es den Rahmen der Meldung sprengen könnte.",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lueckerath",
+					"name": "Thomas Lückerath",
+					"id": 631012906,
+					"id_str": "631012906",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": 1509168673696764000,
+		"in_reply_to_status_id_str": "1509168673696763918",
+		"in_reply_to_user_id": 631012906,
+		"in_reply_to_user_id_str": "631012906",
+		"in_reply_to_screen_name": "lueckerath",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 814,
+			"friends_count": 154,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5735,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22505,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@lueckerath Klar. Aber umso erstaunlicher, dass er nach Filmen wie Sonatine, Hana-bi, Kikujiros Sommer oder Zatoichi wieder in die Rolle als Castle-Host zurückkehrt. Wäre vielleicht eine Erwähnung wert. Verstehe aber auch, daß es den Rahmen der Meldung sprengen könnte."
+	},
+	{
 		"created_at": "Wed Mar 30 11:27:35 +0000 2022",
 		"id": 1509130210372956200,
 		"id_str": "1509130210372956164",
