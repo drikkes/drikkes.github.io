55e156583cc57e464ee547badb3441ae4c573a27
-drik S <drikkes@gmail.com>
2022-04-01 08:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68569,
-	"created_at": "Thu Mar 31 16:11:17 +0000 2022",
+	"tweets": 68570,
+	"created_at": "Fri Apr 01 06:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2022_04.js",
+		"year": 2022,
+		"var_name": "tweets_2022_04",
+		"tweet_count": 1,
+		"month": 4
+	},
+	{
 		"file_name": "data/js/tweets/2022_03.js",
 		"year": 2022,
 		"var_name": "tweets_2022_03",
