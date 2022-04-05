2ac871b70dbdafc20f53821fb591575da0d264bf
-drik S <drikkes@gmail.com>
2022-04-05 13:11:17 +0200

    Added 1 tweets

--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,282 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 05 10:42:30 +0000 2022",
+		"id": 1511293194436235300,
+		"id_str": "1511293194436235264",
+		"full_text": "It’s been 14 years since I moved from Bochum to Cologne, and I can still relate to this. (May I add, you never leave behind the previous team completely.) Also: Go, @sixers! https://t.co/cK5mqGHQwx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			173
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sixers",
+					"name": "x - Philadelphia 76ers",
+					"id": 16201775,
+					"id_str": "16201775",
+					"indices": [
+						165,
+						172
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/cK5mqGHQwx",
+					"expanded_url": "https://twitter.com/mckinneykelsey/status/1510644963717095429",
+					"display_url": "twitter.com/mckinneykelsey…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 814,
+			"friends_count": 154,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5739,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22525,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91703979,
+				6.96590459
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96590459,
+				50.91703979
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1510644963717095400,
+		"quoted_status_id_str": "1510644963717095429",
+		"quoted_status_permalink": {
+			"url": "https://t.co/cK5mqGHQwx",
+			"expanded": "https://twitter.com/mckinneykelsey/status/1510644963717095429",
+			"display": "twitter.com/mckinneykelsey…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Apr 03 15:46:40 +0000 2022",
+			"id": 1510644963717095400,
+			"id_str": "1510644963717095429",
+			"full_text": "attention everyone i am now a Philadelphia resident and fan. I have chosen violence (rooting for the Phillies) \n\nhttps://t.co/Pfflt5lf07",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				136
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/Pfflt5lf07",
+						"expanded_url": "https://defector.com/rooting-for-the-home-team/",
+						"display_url": "defector.com/rooting-for-th…",
+						"indices": [
+							113,
+							136
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16795115,
+				"id_str": "16795115",
+				"name": "kelsey mckinney",
+				"screen_name": "mckinneykelsey",
+				"location": "kelsey@defector.com",
+				"description": "co-owner @defectormedia, author of GOD SPARE THE GIRLS. host of NORMAL GOSSIP.",
+				"url": "https://t.co/kBEaajBo9T",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/kBEaajBo9T",
+								"expanded_url": "http://kelseymckinney.com",
+								"display_url": "kelseymckinney.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 86799,
+				"friends_count": 2067,
+				"listed_count": 396,
+				"created_at": "Wed Oct 15 21:47:55 +0000 2008",
+				"favourites_count": 70823,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 805,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "808285",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1430884994910728194/5l03tfDH_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1430884994910728194/5l03tfDH_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16795115/1646605382",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EADEAA",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 131,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It’s been 14 years since I moved from Bochum to Cologne, and I can still relate to this. (May I add, you never leave behind the previous team completely.) Also: Go, @sixers! https://t.co/cK5mqGHQwx"
+	},
+	{
 		"created_at": "Tue Apr 05 08:46:45 +0000 2022",
 		"id": 1511264065359863800,
 		"id_str": "1511264065359863810",
