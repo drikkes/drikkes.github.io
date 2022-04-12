a62cce77dbcc82b65ece1b59dbb9bb3b096f11e4
-drik S <drikkes@gmail.com>
2022-04-12 23:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68615,
-	"created_at": "Tue Apr 12 17:11:23 +0000 2022",
+	"tweets": 68616,
+	"created_at": "Tue Apr 12 21:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 12 21:03:32 +0000 2022",
+		"id": 1513986197877629000,
+		"id_str": "1513986197877628930",
+		"full_text": "Und direkt nach dem letzten Tweet den hier liken. https://t.co/An3Vhwp7Iu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/An3Vhwp7Iu",
+					"expanded_url": "https://twitter.com/sebastianbaumer/status/1513916001892683783",
+					"display_url": "twitter.com/sebastianbaume…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 813,
+			"friends_count": 154,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5741,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22559,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1513916001892683800,
+		"quoted_status_id_str": "1513916001892683783",
+		"quoted_status_permalink": {
+			"url": "https://t.co/An3Vhwp7Iu",
+			"expanded": "https://twitter.com/sebastianbaumer/status/1513916001892683783",
+			"display": "twitter.com/sebastianbaume…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Apr 12 16:24:36 +0000 2022",
+			"id": 1513916001892683800,
+			"id_str": "1513916001892683783",
+			"full_text": "Nicht vergessen, jedes einzelne Ereignis about yourself zu machen und für Deine Agenda auszuschlachten, das sind absolute Twitter-Basics, ohne die brauchst Du hier gar nicht ankommen.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				183
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 34933443,
+				"id_str": "34933443",
+				"name": "Sebastian Fenris Baumer",
+				"screen_name": "sebastianbaumer",
+				"location": "Hamburg",
+				"description": "Ich komm ganz langsam wieder. Art Director, Writer & Photographer. International IAP Design Award Winner & written in A Phrygian Mode. Altsumerische Gottheit.",
+				"url": "https://t.co/dYCr65dssX",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/dYCr65dssX",
+								"expanded_url": "https://sebastianbaumer.myportfolio.com/",
+								"display_url": "sebastianbaumer.myportfolio.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 369,
+				"friends_count": 854,
+				"listed_count": 5,
+				"created_at": "Fri Apr 24 14:09:29 +0000 2009",
+				"favourites_count": 7805,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 5560,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1386762107098648580/H3m-9G5P_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1386762107098648580/H3m-9G5P_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/34933443/1627765617",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 7,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Und direkt nach dem letzten Tweet den hier liken. https://t.co/An3Vhwp7Iu"
+	},
+	{
 		"created_at": "Tue Apr 12 16:45:00 +0000 2022",
 		"id": 1513921133258711000,
 		"id_str": "1513921133258711044",
