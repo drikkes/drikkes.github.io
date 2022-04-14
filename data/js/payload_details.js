82527e69ea8adaa0949a49e451b97a38ca61e083
-drik S <drikkes@gmail.com>
2022-04-14 15:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68620,
-	"created_at": "Thu Apr 14 10:11:17 +0000 2022",
+	"tweets": 68621,
+	"created_at": "Thu Apr 14 13:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,236 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Thu Apr 14 13:04:59 +0000 2022",
+		"id": 1514590539873665000,
+		"id_str": "1514590539873665034",
+		"full_text": "Zum Glück ist Fritz erst 11. https://t.co/jUwBKj67oC https://t.co/9KXXgIr5jd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			52
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/jUwBKj67oC",
+					"expanded_url": "https://www.horizont.net/zahlen-des-tages",
+					"display_url": "horizont.net/zahlen-des-tag…",
+					"indices": [
+						29,
+						52
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1514590536467984400,
+					"id_str": "1514590536467984390",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/FQToEXpXsAYyXny.png",
+					"media_url_https": "https://pbs.twimg.com/media/FQToEXpXsAYyXny.png",
+					"url": "https://t.co/9KXXgIr5jd",
+					"display_url": "pic.twitter.com/9KXXgIr5jd",
+					"expanded_url": "https://twitter.com/drikkes/status/1514590539873665034/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 665,
+							"h": 253,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 665,
+							"h": 253,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 665,
+							"h": 253,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1514590536467984400,
+					"id_str": "1514590536467984390",
+					"indices": [
+						53,
+						76
+					],
+					"media_url": "http://pbs.twimg.com/media/FQToEXpXsAYyXny.png",
+					"media_url_https": "https://pbs.twimg.com/media/FQToEXpXsAYyXny.png",
+					"url": "https://t.co/9KXXgIr5jd",
+					"display_url": "pic.twitter.com/9KXXgIr5jd",
+					"expanded_url": "https://twitter.com/drikkes/status/1514590539873665034/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 665,
+							"h": 253,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 665,
+							"h": 253,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 665,
+							"h": 253,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 813,
+			"friends_count": 154,
+			"listed_count": 78,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5741,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22565,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91697795,
+				6.96592386
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96592386,
+				50.91697795
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Zum Glück ist Fritz erst 11. https://t.co/jUwBKj67oC https://t.co/9KXXgIr5jd"
+	},
+	{
 		"created_at": "Thu Apr 14 09:38:07 +0000 2022",
 		"id": 1514538480143147000,
 		"id_str": "1514538480143147010",
