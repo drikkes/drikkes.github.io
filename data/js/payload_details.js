b3b69e7b4d4f5b7627e24398d9c1b607d2397a2a
-drik S <drikkes@gmail.com>
2022-04-26 23:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 68653,
-	"created_at": "Tue Apr 26 14:11:17 +0000 2022",
+	"tweets": 68654,
+	"created_at": "Tue Apr 26 21:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_04.js
+++ data/js/tweets/2022_04.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2022_04 = 
 [
 	{
+		"created_at": "Tue Apr 26 21:02:52 +0000 2022",
+		"id": 1519002156199731200,
+		"id_str": "1519059457724096512",
+		"full_text": "\"Stevens cites age inappropriateness, social-emotional learning, mentions of bestiality and rape, and \"wokeness\" as reasons to ban the Bible.\"\nhttps://t.co/CI75FEQDeZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/CI75FEQDeZ",
+					"expanded_url": "https://www.miaminewtimes.com/news/broward-man-petitions-to-ban-christian-bible-from-eight-florida-school-districts-14335777",
+					"display_url": "miaminewtimes.com/news/broward-m…",
+					"indices": [
+						143,
+						166
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18763491,
+			"id_str": "18763491",
+			"name": "Below, a fictional Андрей Андреев",
+			"screen_name": "avbelow",
+			"location": "Cologne",
+			"description": "macOS / iOS Developer mit einer generellen Abneigung zu allen Technologien im Innovationsbereich 'Blockchain'",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1929,
+			"friends_count": 2538,
+			"listed_count": 158,
+			"created_at": "Thu Jan 08 13:41:56 +0000 2009",
+			"favourites_count": 149343,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 112136,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1379164056607719428/Nva4iqw6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1379164056607719428/Nva4iqw6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18763491/1422612286",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Stevens cites age inappropriateness, social-emotional learning, mentions of bestiality and rape, and \"wokeness\" as reasons to ban the Bible.\"\nhttps://t.co/CI75FEQDeZ"
+	},
+	{
 		"created_at": "Tue Apr 26 13:59:24 +0000 2022",
 		"id": 1518952890219610000,
 		"id_str": "1518952890219610116",
