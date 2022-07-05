3de1702cfe6d93e36fc0eef6f7ab750d53bf5deb
-drik S <drikkes@gmail.com>
2022-07-05 12:11:17 +0200

    Added 1 tweets

--- data/js/tweets/2022_07.js
+++ data/js/tweets/2022_07.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2022_07 = 
 [
 	{
+		"created_at": "Tue Jul 05 09:34:00 +0000 2022",
+		"id": 218439593240956930,
+		"id_str": "1544253250634932225",
+		"full_text": "Everything happens so much",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 174958347,
+			"id_str": "174958347",
+			"name": "Horse ebooks",
+			"screen_name": "Horse_ebooks",
+			"location": "",
+			"description": "Horse ebooks",
+			"url": "http://t.co/pVoIka58TM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/pVoIka58TM",
+							"expanded_url": "http://horse-ebooks.com/",
+							"display_url": "horse-ebooks.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 139336,
+			"friends_count": 13171,
+			"listed_count": 1363,
+			"created_at": "Thu Aug 05 07:18:32 +0000 2010",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18808,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1096005346/1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1096005346/1_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 90531,
+		"favorite_count": 101357,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Everything happens so much"
+	},
+	{
 		"created_at": "Tue Jul 05 08:16:41 +0000 2022",
 		"id": 1544233791438422000,
 		"id_str": "1544233791438422016",
