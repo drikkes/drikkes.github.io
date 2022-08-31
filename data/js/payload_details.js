e1898a54f8edbbf7fffb704389b7e77cbc4143cf
-drik S <drikkes@gmail.com>
2022-08-31 16:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69002,
-	"created_at": "Wed Aug 31 13:11:18 +0000 2022",
+	"tweets": 69003,
+	"created_at": "Wed Aug 31 14:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_08.js
+++ data/js/tweets/2022_08.js
@@ -1,6 +1,228 @@
 Grailbird.data.tweets_2022_08 = 
 [
 	{
+		"created_at": "Wed Aug 31 13:48:00 +0000 2022",
+		"id": 1564973280032968700,
+		"id_str": "1564973280032968705",
+		"full_text": "Bought tickets for the @EuroBasket match @DBB_Basketball vs @kzs_si to see @MaxiKleber, @dtheis10, @franzboogie play against @luka7doncic and @Goran_Dragic.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			156
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EuroBasket",
+					"name": "#EuroBasket 2022",
+					"id": 897116645902225400,
+					"id_str": "897116645902225408",
+					"indices": [
+						23,
+						34
+					]
+				},
+				{
+					"screen_name": "DBB_Basketball",
+					"name": "Dt. Basketball Bund",
+					"id": 274985570,
+					"id_str": "274985570",
+					"indices": [
+						41,
+						56
+					]
+				},
+				{
+					"screen_name": "kzs_si",
+					"name": "Košarkarska zveza SIovenije",
+					"id": 533161699,
+					"id_str": "533161699",
+					"indices": [
+						60,
+						67
+					]
+				},
+				{
+					"screen_name": "MaxiKleber",
+					"name": "Maxi Kleber",
+					"id": 888161623155896300,
+					"id_str": "888161623155896321",
+					"indices": [
+						75,
+						86
+					]
+				},
+				{
+					"screen_name": "dtheis10",
+					"name": "Daniel Theis",
+					"id": 4044682379,
+					"id_str": "4044682379",
+					"indices": [
+						88,
+						97
+					]
+				},
+				{
+					"screen_name": "franzboogie",
+					"name": "Franz Wagner",
+					"id": 1404190041648242700,
+					"id_str": "1404190041648242688",
+					"indices": [
+						99,
+						111
+					]
+				},
+				{
+					"screen_name": "luka7doncic",
+					"name": "Luka Doncic",
+					"id": 460116210,
+					"id_str": "460116210",
+					"indices": [
+						125,
+						137
+					]
+				},
+				{
+					"screen_name": "Goran_Dragic",
+					"name": "Goran Dragić",
+					"id": 356859571,
+					"id_str": "356859571",
+					"indices": [
+						142,
+						155
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 806,
+			"friends_count": 154,
+			"listed_count": 77,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5779,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22961,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": {
+			"type": "Point",
+			"coordinates": [
+				50.91706171,
+				6.96595133
+			]
+		},
+		"coordinates": {
+			"type": "Point",
+			"coordinates": [
+				6.96595133,
+				50.91706171
+			]
+		},
+		"place": {
+			"id": "8abc99434d4f5d28",
+			"url": "https://api.twitter.com/1.1/geo/id/8abc99434d4f5d28.json",
+			"place_type": "city",
+			"name": "Cologne",
+			"full_name": "Cologne, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							6.772567,
+							50.830287
+						],
+						[
+							7.162373,
+							50.830287
+						],
+						[
+							7.162373,
+							51.0849471
+						],
+						[
+							6.772567,
+							51.0849471
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "Bought tickets for the @EuroBasket match @DBB_Basketball vs @kzs_si to see @MaxiKleber, @dtheis10, @franzboogie play against @luka7doncic and @Goran_Dragic."
+	},
+	{
 		"created_at": "Wed Aug 31 12:46:39 +0000 2022",
 		"id": 1564957839243739100,
 		"id_str": "1564957839243739139",
