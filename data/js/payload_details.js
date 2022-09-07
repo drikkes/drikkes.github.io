dc5290ad5427745d303794d38d39e202f2f7a26a
-drik S <drikkes@gmail.com>
2022-09-08 00:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69021,
-	"created_at": "Wed Sep 07 17:11:17 +0000 2022",
+	"tweets": 69022,
+	"created_at": "Wed Sep 07 22:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 07 21:45:23 +0000 2022",
+		"id": 1567630131455295500,
+		"id_str": "1567630131455295492",
+		"full_text": "@der_rosenkranz @nouveaubeton Sie hat die Filmrechte schon an @NetflixDE verkauft.",
+		"truncated": false,
+		"display_text_range": [
+			30,
+			82
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "der_rosenkranz",
+					"name": "Boris Rosenkranz",
+					"id": 2450574870,
+					"id_str": "2450574870",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "nouveaubeton",
+					"name": "Winnetou Mariae Gloria von Béton",
+					"id": 1289180383108571100,
+					"id_str": "1289180383108571136",
+					"indices": [
+						16,
+						29
+					]
+				},
+				{
+					"screen_name": "NetflixDE",
+					"name": "netflixde",
+					"id": 2341884084,
+					"id_str": "2341884084",
+					"indices": [
+						62,
+						72
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1567568008800772000,
+		"in_reply_to_status_id_str": "1567568008800772096",
+		"in_reply_to_user_id": 2450574870,
+		"in_reply_to_user_id_str": "2450574870",
+		"in_reply_to_screen_name": "der_rosenkranz",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 806,
+			"friends_count": 154,
+			"listed_count": 77,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5783,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22980,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@der_rosenkranz @nouveaubeton Sie hat die Filmrechte schon an @NetflixDE verkauft."
+	},
+	{
 		"created_at": "Wed Sep 07 16:56:02 +0000 2022",
 		"id": 1567557314080538600,
 		"id_str": "1567557314080538625",
