b40ba3dec85c4cb5f6c7d5de990918f40be8692f
-drik S <drikkes@gmail.com>
2022-09-15 20:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69049,
-	"created_at": "Thu Sep 15 13:11:17 +0000 2022",
+	"tweets": 69050,
+	"created_at": "Thu Sep 15 18:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Thu Sep 15 17:47:33 +0000 2022",
+		"id": 1570469384514150400,
+		"id_str": "1570469384514150401",
+		"full_text": "@Senficon @sovtechfund @mspro @BMWK Count me in for the merch!",
+		"truncated": false,
+		"display_text_range": [
+			36,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Senficon",
+					"name": "Felix Reda",
+					"id": 14861745,
+					"id_str": "14861745",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "sovtechfund",
+					"name": "Sovereign Tech Fund",
+					"id": 1567101650619322400,
+					"id_str": "1567101650619322368",
+					"indices": [
+						10,
+						22
+					]
+				},
+				{
+					"screen_name": "mspro",
+					"name": "Michael Seemann",
+					"id": 5751892,
+					"id_str": "5751892",
+					"indices": [
+						23,
+						29
+					]
+				},
+				{
+					"screen_name": "BMWK",
+					"name": "Bundesministerium für Wirtschaft und Klimaschutz",
+					"id": 947587280,
+					"id_str": "947587280",
+					"indices": [
+						30,
+						35
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": 1570468061920071700,
+		"in_reply_to_status_id_str": "1570468061920071683",
+		"in_reply_to_user_id": 14861745,
+		"in_reply_to_user_id_str": "14861745",
+		"in_reply_to_screen_name": "Senficon",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 805,
+			"friends_count": 154,
+			"listed_count": 77,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5789,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23011,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Senficon @sovtechfund @mspro @BMWK Count me in for the merch!"
+	},
+	{
 		"created_at": "Thu Sep 15 13:06:22 +0000 2022",
 		"id": 1570398619404210200,
 		"id_str": "1570398619404210177",
