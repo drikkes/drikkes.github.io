89a5157394fcbf9bd203a78e4cd4615e2b25159f
-drik S <drikkes@gmail.com>
2022-09-29 01:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69087,
-	"created_at": "Wed Sep 28 16:11:19 +0000 2022",
+	"tweets": 69088,
+	"created_at": "Wed Sep 28 23:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_09.js
+++ data/js/tweets/2022_09.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_09 = 
 [
 	{
+		"created_at": "Wed Sep 28 23:02:09 +0000 2022",
+		"id": 1575229127514259500,
+		"id_str": "1575259595706187781",
+		"full_text": "https://t.co/3Rebb6ySy7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1575229032286888000,
+					"id_str": "1575229032286887936",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FdxWdX1XoAAgTcl.png",
+					"media_url_https": "https://pbs.twimg.com/media/FdxWdX1XoAAgTcl.png",
+					"url": "https://t.co/3Rebb6ySy7",
+					"display_url": "pic.twitter.com/3Rebb6ySy7",
+					"expanded_url": "https://twitter.com/palmreport/status/1575229127514259473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 754,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 676,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 754,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1575229032286888000,
+					"id_str": "1575229032286887936",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/FdxWdX1XoAAgTcl.png",
+					"media_url_https": "https://pbs.twimg.com/media/FdxWdX1XoAAgTcl.png",
+					"url": "https://t.co/3Rebb6ySy7",
+					"display_url": "pic.twitter.com/3Rebb6ySy7",
+					"expanded_url": "https://twitter.com/palmreport/status/1575229127514259473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 750,
+							"h": 754,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 676,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 754,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://palm.report\" rel=\"nofollow\">PalmBotV2</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1495655290125295600,
+			"id_str": "1495655290125295616",
+			"name": "PALM REPORT ☼ (Beta)",
+			"screen_name": "palmreport",
+			"location": "",
+			"description": "Newsletter & lifestyle research facility by @Poolsuite ☼ Subscribe below",
+			"url": "https://t.co/Qe157EnuLh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Qe157EnuLh",
+							"expanded_url": "https://palm.report",
+							"display_url": "palm.report",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3439,
+			"friends_count": 2,
+			"listed_count": 17,
+			"created_at": "Mon Feb 21 07:03:20 +0000 2022",
+			"favourites_count": 49,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 422,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1510705880433840128/2g5M_2g__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1510705880433840128/2g5M_2g__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1495655290125295616/1646118883",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 37,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "zxx",
+		"text": "https://t.co/3Rebb6ySy7"
+	},
+	{
 		"created_at": "Wed Sep 28 15:32:04 +0000 2022",
 		"id": 1575146329801638000,
 		"id_str": "1575146329801637889",
