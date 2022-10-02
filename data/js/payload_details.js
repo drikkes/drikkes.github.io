0c0865192b4a45331653697bc73146cba245c3b8
-drik S <drikkes@gmail.com>
2022-10-02 16:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69103,
-	"created_at": "Sun Oct 02 10:11:17 +0000 2022",
+	"tweets": 69104,
+	"created_at": "Sun Oct 02 14:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sun Oct 02 13:30:10 +0000 2022",
+		"id": 1576565205382926300,
+		"id_str": "1576565205382926339",
+		"full_text": "Sagt jemand, der für die „Oder sollen wir es lassen?“-Zeitung arbeitet. https://t.co/9S5ayVv3Io",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9S5ayVv3Io",
+					"expanded_url": "https://twitter.com/larsweisbrod/status/1576462759658913793",
+					"display_url": "twitter.com/larsweisbrod/s…",
+					"indices": [
+						72,
+						95
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 804,
+			"friends_count": 155,
+			"listed_count": 0,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5804,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23071,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1576462759658913800,
+		"quoted_status_id_str": "1576462759658913793",
+		"quoted_status_permalink": {
+			"url": "https://t.co/9S5ayVv3Io",
+			"expanded": "https://twitter.com/larsweisbrod/status/1576462759658913793",
+			"display": "twitter.com/larsweisbrod/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Oct 02 06:43:05 +0000 2022",
+			"id": 1576462759658913800,
+			"id_str": "1576462759658913793",
+			"full_text": "Twitter holt echt das schlimmste aus einem raus, ich fang jetzt sogar schon an Precht zu verteidigen",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				100
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16189050,
+				"id_str": "16189050",
+				"name": "Lars Weisbrod",
+				"screen_name": "larsweisbrod",
+				"location": "Hamburg",
+				"description": "carving nature at its joints @DIEZEIT Feuilleton",
+				"url": "https://t.co/ayX9hLu6Nk",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/ayX9hLu6Nk",
+								"expanded_url": "https://www.zeit.de/serie/die-sogenannte-gegenwart",
+								"display_url": "zeit.de/serie/die-soge…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 45523,
+				"friends_count": 1619,
+				"listed_count": 203,
+				"created_at": "Mon Sep 08 18:53:10 +0000 2008",
+				"favourites_count": 116201,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 28541,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1549248411907620864/rZSucANS_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1549248411907620864/rZSucANS_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16189050/1422928130",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 3,
+			"favorite_count": 233,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Sagt jemand, der für die „Oder sollen wir es lassen?“-Zeitung arbeitet. https://t.co/9S5ayVv3Io"
+	},
+	{
 		"created_at": "Sun Oct 02 09:54:48 +0000 2022",
 		"id": 1576511004946886700,
 		"id_str": "1576511004946886657",
