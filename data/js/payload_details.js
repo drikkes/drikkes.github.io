70ab22878cd56b2df0ba9ca042e6a50d7ff9e3b6
-drik S <drikkes@gmail.com>
2022-10-03 11:11:18 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69106,
-	"created_at": "Mon Oct 03 06:11:18 +0000 2022",
+	"tweets": 69107,
+	"created_at": "Mon Oct 03 09:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Mon Oct 03 08:58:17 +0000 2022",
+		"id": 1007595655517352000,
+		"id_str": "1576859172200644610",
+		"full_text": "Fakt: Die Deutsche Einheit ist 1 Gehtsnoch [Gn] und entspricht etwa 100 Hammersbald [Hb]",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14233899,
+			"id_str": "14233899",
+			"name": "Gabriel Yoran",
+			"screen_name": "GabrielBerlin",
+			"location": "Berlin, Deutschland",
+			"description": "Unternehmer @joinsteady, @steganos. Autor ”The Interfact” @openhumanities, „Warum heißt es“ @frohmannberlin, „Klassik verstehen“ @krautreporter, er/ihm 🏳️‍🌈",
+			"url": "https://t.co/F46eTUvEk0",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/F46eTUvEk0",
+							"expanded_url": "http://yoran.com",
+							"display_url": "yoran.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8620,
+			"friends_count": 1698,
+			"listed_count": 258,
+			"created_at": "Thu Mar 27 09:18:55 +0000 2008",
+			"favourites_count": 207752,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 146537,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "875022",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1576680963697942532/CUm_yzIf_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576680963697942532/CUm_yzIf_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14233899/1658401710",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "EAD4CD",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3078869807f9dd36",
+			"url": "https://api.twitter.com/1.1/geo/id/3078869807f9dd36.json",
+			"place_type": "city",
+			"name": "Berlin",
+			"full_name": "Berlin, Germany",
+			"country_code": "DE",
+			"country": "Germany",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							13.088304,
+							52.338079
+						],
+						[
+							13.760909,
+							52.338079
+						],
+						[
+							13.760909,
+							52.675323
+						],
+						[
+							13.088304,
+							52.675323
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 30,
+		"favorite_count": 129,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "de",
+		"text": "Fakt: Die Deutsche Einheit ist 1 Gehtsnoch [Gn] und entspricht etwa 100 Hammersbald [Hb]"
+	},
+	{
 		"created_at": "Mon Oct 03 05:36:57 +0000 2022",
 		"id": 1576785870375493600,
 		"id_str": "1576808503364259840",
