d8196bd35815efc864449957ab78380225675c3f
-drik S <drikkes@gmail.com>
2022-10-29 16:11:19 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69167,
-	"created_at": "Sat Oct 29 09:11:18 +0000 2022",
+	"tweets": 69168,
+	"created_at": "Sat Oct 29 14:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,194 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sat Oct 29 13:13:14 +0000 2022",
+		"id": 1586345414176940000,
+		"id_str": "1586345414176940033",
+		"full_text": "So Abschreibejournos werden die Plattform twitter dot com noch „Microblogging-Service“ nennen, wenn niemand (außer René) mehr weiß, was „Bloggen“ überhaupt gewesen ist. https://t.co/UiOva2rZOb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1586345387899719700,
+					"id_str": "1586345387899719680",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FgPUubjXkAAlPmF.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FgPUubjXkAAlPmF.jpg",
+					"url": "https://t.co/UiOva2rZOb",
+					"display_url": "pic.twitter.com/UiOva2rZOb",
+					"expanded_url": "https://twitter.com/drikkes/status/1586345414176940033/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 342,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 342,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 342,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1586345387899719700,
+					"id_str": "1586345387899719680",
+					"indices": [
+						169,
+						192
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/FgPUubjXkAAlPmF.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/FgPUubjXkAAlPmF.jpg",
+					"url": "https://t.co/UiOva2rZOb",
+					"display_url": "pic.twitter.com/UiOva2rZOb",
+					"expanded_url": "https://twitter.com/drikkes/status/1586345414176940033/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"medium": {
+							"w": 342,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 342,
+							"h": 480,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 342,
+							"h": 480,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							57,
+							80
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/FgPUubjXkAAlPmF.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 802,
+			"friends_count": 156,
+			"listed_count": 76,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5768,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23133,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "So Abschreibejournos werden die Plattform twitter dot com noch „Microblogging-Service“ nennen, wenn niemand (außer René) mehr weiß, was „Bloggen“ überhaupt gewesen ist. https://t.co/UiOva2rZOb"
+	},
+	{
 		"created_at": "Sat Oct 29 09:05:58 +0000 2022",
 		"id": 1586283186652418000,
 		"id_str": "1586283186652418050",
