f44edfd996a04875e83c2776791f12d3d025fd8a
-drik S <drikkes@gmail.com>
2022-11-01 11:11:19 +0100

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -1,5 +1,12 @@
 var tweet_index = [
 	{
+		"file_name": "data/js/tweets/2022_11.js",
+		"year": 2022,
+		"var_name": "tweets_2022_11",
+		"tweet_count": 1,
+		"month": 11
+	},
+	{
 		"file_name": "data/js/tweets/2022_10.js",
 		"year": 2022,
 		"var_name": "tweets_2022_10",
