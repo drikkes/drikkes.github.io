8a279fb50f4c4d65ca37d7ce4797a16c2493f1af
-drik S <drikkes@gmail.com>
2022-11-22 18:11:19 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69224,
-	"created_at": "Tue Nov 22 09:11:18 +0000 2022",
+	"tweets": 69225,
+	"created_at": "Tue Nov 22 17:11:18 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,327 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Tue Nov 22 16:35:40 +0000 2022",
+		"id": 1595093668125638700,
+		"id_str": "1595093668125638657",
+		"full_text": "Apropos: Ich hab die Spielerkärtchen immer benutzt, um die zum Überdecken der ekligen Raucherwarnungen hinter die Zellophanfolie der Kippenschachteln zu schieben. Was nimmt man da jetzt in Zeiten des WM/DFB-Boykotts? https://t.co/1phymVdvak",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1phymVdvak",
+					"expanded_url": "https://twitter.com/mathiasrichel/status/1595060861365788672",
+					"display_url": "twitter.com/mathiasrichel/…",
+					"indices": [
+						217,
+						240
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 794,
+			"friends_count": 158,
+			"listed_count": 74,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5784,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23200,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1595060861365788700,
+		"quoted_status_id_str": "1595060861365788672",
+		"quoted_status_permalink": {
+			"url": "https://t.co/1phymVdvak",
+			"expanded": "https://twitter.com/mathiasrichel/status/1595060861365788672",
+			"display": "twitter.com/mathiasrichel/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Nov 22 14:25:18 +0000 2022",
+			"id": 1595060861365788700,
+			"id_str": "1595060861365788672",
+			"full_text": "Rewe ist sehr konsequent im Bruch mit dem DFB. Jede Erklärung, die damit beginnt, dass der Vertrag eh ausgelaufen wäre, ist eine irrelevante Nebelkerze. https://t.co/67kqUNKgI9",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				152
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1595060854772334600,
+						"id_str": "1595060854772334592",
+						"indices": [
+							153,
+							176
+						],
+						"media_url": "http://pbs.twimg.com/media/FiLLZXvWAAATdUn.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FiLLZXvWAAATdUn.jpg",
+						"url": "https://t.co/67kqUNKgI9",
+						"display_url": "pic.twitter.com/67kqUNKgI9",
+						"expanded_url": "https://twitter.com/mathiasrichel/status/1595060861365788672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1170,
+								"h": 1751,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 802,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 454,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1595060854772334600,
+						"id_str": "1595060854772334592",
+						"indices": [
+							153,
+							176
+						],
+						"media_url": "http://pbs.twimg.com/media/FiLLZXvWAAATdUn.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FiLLZXvWAAATdUn.jpg",
+						"url": "https://t.co/67kqUNKgI9",
+						"display_url": "pic.twitter.com/67kqUNKgI9",
+						"expanded_url": "https://twitter.com/mathiasrichel/status/1595060861365788672/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1170,
+								"h": 1751,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 802,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 454,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1595043284149506000,
+			"in_reply_to_status_id_str": "1595043284149506050",
+			"in_reply_to_user_id": 5899392,
+			"in_reply_to_user_id_str": "5899392",
+			"in_reply_to_screen_name": "mathiasrichel",
+			"user": {
+				"id": 5899392,
+				"id_str": "5899392",
+				"name": "Mathias Richel",
+				"screen_name": "mathiasrichel",
+				"location": "Berlin-Pankow, Germany",
+				"description": "POLITIK :: RAP :: OSTEN //Radikal moderater Flügel //geboren in Frankfurt (Oder), leben in Berlin //Gründer Richel, Stauss GmbH",
+				"url": "https://t.co/iP0kQbRxOT",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/iP0kQbRxOT",
+								"expanded_url": "https://d-64.social/@mathiasrichel",
+								"display_url": "d-64.social/@mathiasrichel",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 15166,
+				"friends_count": 623,
+				"listed_count": 339,
+				"created_at": "Wed May 09 13:32:33 +0000 2007",
+				"favourites_count": 55363,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 69312,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1579541551339929610/hVGlUmW5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1579541551339929610/hVGlUmW5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/5899392/1622751516",
+				"profile_link_color": "9494FC",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "D2D2D2",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "09f6a7707f18e0b1",
+				"url": "https://api.twitter.com/1.1/geo/id/09f6a7707f18e0b1.json",
+				"place_type": "city",
+				"name": "Paris",
+				"full_name": "Paris, France",
+				"country_code": "FR",
+				"country": "France",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								2.2241006,
+								48.8155214
+							],
+							[
+								2.4699053,
+								48.8155214
+							],
+							[
+								2.4699053,
+								48.9021461
+							],
+							[
+								2.2241006,
+								48.9021461
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "de"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Apropos: Ich hab die Spielerkärtchen immer benutzt, um die zum Überdecken der ekligen Raucherwarnungen hinter die Zellophanfolie der Kippenschachteln zu schieben. Was nimmt man da jetzt in Zeiten des WM/DFB-Boykotts? https://t.co/1phymVdvak"
+	},
+	{
 		"created_at": "Tue Nov 22 08:53:12 +0000 2022",
 		"id": 1594977284598878200,
 		"id_str": "1594977284598878208",
