7e18035d3f3c3c350f80838e64690f6aa0f6db3a
-drik S <drikkes@gmail.com>
2022-11-23 11:11:20 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69227,
-	"created_at": "Wed Nov 23 06:11:18 +0000 2022",
+	"tweets": 69228,
+	"created_at": "Wed Nov 23 10:11:19 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 23 09:43:52 +0000 2022",
+		"id": 1595352421684973600,
+		"id_str": "1595352421684973570",
+		"full_text": "🏆 Die Entscheidung im Freitagstexter ist gefallen. \n🥉 @formschub \n🥈@_DABLJU_ \nUnd gewonnen hat:\n🏅 @zynaesthesie \nGratulation ans Treppchen, Dank an alle Teilnehmenden!\nhttps://t.co/3NIdUbFjTh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			191
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "formschub",
+					"name": "Lockdown der Herzen",
+					"id": 12911512,
+					"id_str": "12911512",
+					"indices": [
+						54,
+						64
+					]
+				},
+				{
+					"screen_name": "_DABLJU_",
+					"name": "/ˈdʌbljuː/",
+					"id": 222516028,
+					"id_str": "222516028",
+					"indices": [
+						67,
+						76
+					]
+				},
+				{
+					"screen_name": "zynaesthesie",
+					"name": "bee",
+					"id": 78050059,
+					"id_str": "78050059",
+					"indices": [
+						98,
+						111
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/3NIdUbFjTh",
+					"expanded_url": "https://drikkes.com/?p=16736",
+					"display_url": "drikkes.com/?p=16736",
+					"indices": [
+						168,
+						191
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 794,
+			"friends_count": 158,
+			"listed_count": 74,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5784,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23203,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "🏆 Die Entscheidung im Freitagstexter ist gefallen. \n🥉 @formschub \n🥈@_DABLJU_ \nUnd gewonnen hat:\n🏅 @zynaesthesie \nGratulation ans Treppchen, Dank an alle Teilnehmenden!\nhttps://t.co/3NIdUbFjTh"
+	},
+	{
 		"created_at": "Wed Nov 23 06:09:30 +0000 2022",
 		"id": 1595298476388130800,
 		"id_str": "1595298476388130822",
