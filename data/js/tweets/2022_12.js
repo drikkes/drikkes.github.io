5bb5590b54052db1e3d4ccd6bb8b3419a3f55074
-drik S <drikkes@gmail.com>
2022-12-02 18:11:18 +0100

    Added 1 tweets

--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,281 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Fri Dec 02 16:23:18 +0000 2022",
+		"id": 1598714435509670000,
+		"id_str": "1598714435509669888",
+		"full_text": "„Vanessa und Dirk Rouenhoff haben das neue Bierpaket mit ihrem Start-up entwickelt.“ https://t.co/8XAtCbITmz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			84
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8XAtCbITmz",
+					"expanded_url": "https://twitter.com/ralfheimann/status/1598633666225344512",
+					"display_url": "twitter.com/ralfheimann/st…",
+					"indices": [
+						85,
+						108
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 791,
+			"friends_count": 158,
+			"listed_count": 74,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5782,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23221,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1598633666225344500,
+		"quoted_status_id_str": "1598633666225344512",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8XAtCbITmz",
+			"expanded": "https://twitter.com/ralfheimann/status/1598633666225344512",
+			"display": "twitter.com/ralfheimann/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Dec 02 11:02:21 +0000 2022",
+			"id": 1598633666225344500,
+			"id_str": "1598633666225344512",
+			"full_text": "Journalismus am Limit https://t.co/KAAS4cwM9Q",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				21
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1598633634394669000,
+						"id_str": "1598633634394669057",
+						"indices": [
+							22,
+							45
+						],
+						"media_url": "http://pbs.twimg.com/media/Fi980hAWAAEmH1f.png",
+						"media_url_https": "https://pbs.twimg.com/media/Fi980hAWAAEmH1f.png",
+						"url": "https://t.co/KAAS4cwM9Q",
+						"display_url": "pic.twitter.com/KAAS4cwM9Q",
+						"expanded_url": "https://twitter.com/ralfheimann/status/1598633666225344512/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 755,
+								"h": 269,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 242,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 755,
+								"h": 269,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1598633634394669000,
+						"id_str": "1598633634394669057",
+						"indices": [
+							22,
+							45
+						],
+						"media_url": "http://pbs.twimg.com/media/Fi980hAWAAEmH1f.png",
+						"media_url_https": "https://pbs.twimg.com/media/Fi980hAWAAEmH1f.png",
+						"url": "https://t.co/KAAS4cwM9Q",
+						"display_url": "pic.twitter.com/KAAS4cwM9Q",
+						"expanded_url": "https://twitter.com/ralfheimann/status/1598633666225344512/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 755,
+								"h": 269,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 242,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 755,
+								"h": 269,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 23431577,
+				"id_str": "23431577",
+				"name": "Ralf Heimann",
+				"screen_name": "ralfheimann",
+				"location": "Münster",
+				"description": "Journalist\n\nMastodon: @ralfheimann@muenster.im",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 21799,
+				"friends_count": 4729,
+				"listed_count": 370,
+				"created_at": "Mon Mar 09 12:59:06 +0000 2009",
+				"favourites_count": 24910,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 22278,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1496567129805631488/KGWY1P5T_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1496567129805631488/KGWY1P5T_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/23431577/1461913210",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 19,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "„Vanessa und Dirk Rouenhoff haben das neue Bierpaket mit ihrem Start-up entwickelt.“ https://t.co/8XAtCbITmz"
+	},
+	{
 		"created_at": "Fri Dec 02 15:55:11 +0000 2022",
 		"id": 1598707356619214800,
 		"id_str": "1598707356619214851",
