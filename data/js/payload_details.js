ef5e2af300b84cf79c3abc85c07a078d704c3e22
-drik S <drikkes@gmail.com>
2022-12-06 20:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69252,
-	"created_at": "Tue Dec 06 14:11:18 +0000 2022",
+	"tweets": 69253,
+	"created_at": "Tue Dec 06 19:11:17 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_12.js
+++ data/js/tweets/2022_12.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2022_12 = 
 [
 	{
+		"created_at": "Tue Dec 06 18:55:51 +0000 2022",
+		"id": 1600116979091660800,
+		"id_str": "1600202378099163141",
+		"full_text": "Umfrage unter Kritikern: Der beste aller Filme https://t.co/7dvGrlMTpI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			70
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/7dvGrlMTpI",
+					"expanded_url": "https://www.faz.net/aktuell/feuilleton/kino/bester-film-der-geschichte-umfrage-unter-kritikern-18511828.html?GEPC=s3",
+					"display_url": "faz.net/aktuell/feuill…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 392295746,
+			"id_str": "392295746",
+			"name": "Claudius Seidl",
+			"screen_name": "Claudiusseidl",
+			"location": "",
+			"description": "\"Eine Meinung kann jeder haben. Ein guter Mann kann zwei bis drei Meinungen haben. Einer ist keiner, und über weniger als 200 brauchen wir nicht zu sprechen\"",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11802,
+			"friends_count": 2082,
+			"listed_count": 120,
+			"created_at": "Sun Oct 16 20:30:38 +0000 2011",
+			"favourites_count": 33806,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5856,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3363323025/fd70a817b393af86d95ededd11557d57_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3363323025/fd70a817b393af86d95ededd11557d57_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/392295746/1398344548",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Umfrage unter Kritikern: Der beste aller Filme https://t.co/7dvGrlMTpI"
+	},
+	{
 		"created_at": "Tue Dec 06 13:52:17 +0000 2022",
 		"id": 1600125979783037000,
 		"id_str": "1600125979783036930",
