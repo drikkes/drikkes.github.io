503c0f95b0b70447f857200cfd4e2798d285e096
-drik S <drikkes@gmail.com>
2023-01-01 13:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69341,
-	"created_at": "Sat Dec 31 14:11:18 +0000 2022",
+	"tweets": 69342,
+	"created_at": "Sun Jan 01 12:11:17 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Sun Jan 01 11:27:55 +0000 2023",
+		"id": 1609480799581085700,
+		"id_str": "1609511736058470405",
+		"full_text": "\"Germany's Next Topmodel\" startet in diesem Jahr an Weiberfastnacht https://t.co/4AnDR6vdX8 https://t.co/J3F2ltGov5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			91
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4AnDR6vdX8",
+					"expanded_url": "https://www.dwdl.de/nachrichten/91184/germanys_next_topmodel_startet_an_weiberfastnacht/",
+					"display_url": "dwdl.de/nachrichten/91…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1609480784548696000,
+					"id_str": "1609480784548696065",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FlYGPwPXkAEr8xO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FlYGPwPXkAEr8xO.jpg",
+					"url": "https://t.co/J3F2ltGov5",
+					"display_url": "pic.twitter.com/J3F2ltGov5",
+					"expanded_url": "https://twitter.com/DWDL/status/1609480799581085698/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 289,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1609480784548696000,
+					"id_str": "1609480784548696065",
+					"indices": [
+						92,
+						115
+					],
+					"media_url": "http://pbs.twimg.com/media/FlYGPwPXkAEr8xO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FlYGPwPXkAEr8xO.jpg",
+					"url": "https://t.co/J3F2ltGov5",
+					"display_url": "pic.twitter.com/J3F2ltGov5",
+					"expanded_url": "https://twitter.com/DWDL/status/1609480799581085698/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 289,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14332395,
+			"id_str": "14332395",
+			"name": "Medienmagazin DWDL",
+			"screen_name": "DWDL",
+			"location": "Cologne",
+			"description": "Hier twittert das Team von DWDL.de über den Redaktionsalltag, die Medien und besondere Events",
+			"url": "https://t.co/2GjpHo72Ge",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2GjpHo72Ge",
+							"expanded_url": "http://www.dwdl.de",
+							"display_url": "dwdl.de",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 100390,
+			"friends_count": 140,
+			"listed_count": 1775,
+			"created_at": "Tue Apr 08 13:58:28 +0000 2008",
+			"favourites_count": 2728,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 79916,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "339933",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1408294275121659904/eBoZd8VK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1408294275121659904/eBoZd8VK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14332395/1597212527",
+			"profile_link_color": "267F26",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "C2D7C0",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "\"Germany's Next Topmodel\" startet in diesem Jahr an Weiberfastnacht https://t.co/4AnDR6vdX8 https://t.co/J3F2ltGov5"
+	}
+]
