7ddf7720223ef98d88531f74000554b67618cee4
-drik S <drikkes@gmail.com>
2023-01-02 16:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69344,
-	"created_at": "Mon Jan 02 10:11:17 +0000 2023",
+	"tweets": 69345,
+	"created_at": "Mon Jan 02 15:11:18 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,134 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 02 15:05:43 +0000 2023",
+		"id": 1609924486299623400,
+		"id_str": "1609928934153457667",
+		"full_text": "Beste Neujahrsansprache. @sleafordmods \n\nhttps://t.co/r9wMvACm4M",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "sleafordmods",
+					"name": "Sleaford Mods",
+					"id": 23674486,
+					"id_str": "23674486",
+					"indices": [
+						25,
+						38
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/r9wMvACm4M",
+					"expanded_url": "https://www.instagram.com/reel/CmhQ2xvIIS0/?igshid=MDJmNzVkMjY=",
+					"display_url": "instagram.com/reel/CmhQ2xvII…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3782931,
+			"id_str": "3782931",
+			"name": "Johnny Haeusler",
+			"screen_name": "spreeblick",
+			"location": "Berlin",
+			"description": "Spreeblick @republica @tincon Plan B. Radio (manchmal Montags): https://t.co/LyEYcaZhNk - Auch möglich: @spreeblick@mastodon.social",
+			"url": "https://t.co/1eNDLRtU6Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/1eNDLRtU6Q",
+							"expanded_url": "http://spreeblick.com",
+							"display_url": "spreeblick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/LyEYcaZhNk",
+							"expanded_url": "http://mixcloud.com/spreeblick",
+							"display_url": "mixcloud.com/spreeblick",
+							"indices": [
+								64,
+								87
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 109709,
+			"friends_count": 9019,
+			"listed_count": 1628,
+			"created_at": "Sun Apr 08 12:02:35 +0000 2007",
+			"favourites_count": 35226,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 43628,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/876751941208088576/N7cb3Liu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/876751941208088576/N7cb3Liu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3782931/1666280134",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Beste Neujahrsansprache. @sleafordmods \n\nhttps://t.co/r9wMvACm4M"
+	},
+	{
 		"created_at": "Mon Jan 02 09:32:36 +0000 2023",
 		"id": 1609845100963561500,
 		"id_str": "1609845100963561473",
