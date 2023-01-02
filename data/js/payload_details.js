11615f90a80f9a7b9689bb56d0e79cabf2d620d4
-drik S <drikkes@gmail.com>
2023-01-02 11:11:18 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 69343,
-	"created_at": "Sun Jan 01 14:11:17 +0000 2023",
+	"tweets": 69344,
+	"created_at": "Mon Jan 02 10:11:17 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 02 09:32:36 +0000 2023",
+		"id": 1609845100963561500,
+		"id_str": "1609845100963561473",
+		"full_text": "Durchhalteparole von Handwerkern bis zur Mittagspause: \"Makita till you Fajita.\" https://t.co/qVojzc1Ftn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1609844734213619700,
+					"id_str": "1609844734213619712",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/FldRQaZWAAAu-Jv.png",
+					"media_url_https": "https://pbs.twimg.com/media/FldRQaZWAAAu-Jv.png",
+					"url": "https://t.co/qVojzc1Ftn",
+					"display_url": "pic.twitter.com/qVojzc1Ftn",
+					"expanded_url": "https://twitter.com/drikkes/status/1609845100963561473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1609844734213619700,
+					"id_str": "1609844734213619712",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/FldRQaZWAAAu-Jv.png",
+					"media_url_https": "https://pbs.twimg.com/media/FldRQaZWAAAu-Jv.png",
+					"url": "https://t.co/qVojzc1Ftn",
+					"display_url": "pic.twitter.com/qVojzc1Ftn",
+					"expanded_url": "https://twitter.com/drikkes/status/1609845100963561473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1609844774684528600,
+					"id_str": "1609844774684528643",
+					"indices": [
+						81,
+						104
+					],
+					"media_url": "http://pbs.twimg.com/media/FldRSxKXEAMgZ9L.png",
+					"media_url_https": "https://pbs.twimg.com/media/FldRSxKXEAMgZ9L.png",
+					"url": "https://t.co/qVojzc1Ftn",
+					"display_url": "pic.twitter.com/qVojzc1Ftn",
+					"expanded_url": "https://twitter.com/drikkes/status/1609845100963561473/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg.",
+			"url": "https://t.co/s9sYhNoDsA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/s9sYhNoDsA",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 789,
+			"friends_count": 153,
+			"listed_count": 74,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5773,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "de",
+		"text": "Durchhalteparole von Handwerkern bis zur Mittagspause: \"Makita till you Fajita.\" https://t.co/qVojzc1Ftn"
+	},
+	{
 		"created_at": "Sun Jan 01 13:27:37 +0000 2023",
 		"id": 1609541856743903200,
 		"id_str": "1609541856743903233",
