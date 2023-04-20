24b3f232c28eafbdfc125ffb9be92fba5bf45d34
-drik S <drikkes@gmail.com>
2023-04-20 10:11:18 +0200

    Added 1 tweets

--- data/js/tweets/2023_04.js
+++ data/js/tweets/2023_04.js
@@ -1,6 +1,183 @@
 Grailbird.data.tweets_2023_04 = 
 [
 	{
+		"created_at": "Thu Apr 20 07:52:57 +0000 2023",
+		"id": 1648957913111621600,
+		"id_str": "1648957913111621632",
+		"full_text": "@Horizont @bundeswehrInfo @scholzfriends @Microsoft @Twitter @elonmusk @Snapchat @netflix Nach dem Relaunch ist Euer Newsletter zwar schicker, aber mobil ist die Schriftgröße eher eine Schriftkleine und als Gmail-Webview wird er nicht vollständig angezeigt. Suboptimal, finde ich.",
+		"truncated": false,
+		"display_text_range": [
+			90,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Horizont",
+					"name": "HORIZONT",
+					"id": 17620455,
+					"id_str": "17620455",
+					"indices": [
+						0,
+						9
+					]
+				},
+				{
+					"screen_name": "bundeswehrInfo",
+					"name": "Bundeswehr",
+					"id": 121415484,
+					"id_str": "121415484",
+					"indices": [
+						10,
+						25
+					]
+				},
+				{
+					"screen_name": "scholzfriends",
+					"name": "Scholz & Friends",
+					"id": 17522935,
+					"id_str": "17522935",
+					"indices": [
+						26,
+						40
+					]
+				},
+				{
+					"screen_name": "Microsoft",
+					"name": "Microsoft",
+					"id": 74286565,
+					"id_str": "74286565",
+					"indices": [
+						41,
+						51
+					]
+				},
+				{
+					"screen_name": "Twitter",
+					"name": "Twitter",
+					"id": 783214,
+					"id_str": "783214",
+					"indices": [
+						52,
+						60
+					]
+				},
+				{
+					"screen_name": "elonmusk",
+					"name": "Elon Musk",
+					"id": 44196397,
+					"id_str": "44196397",
+					"indices": [
+						61,
+						70
+					]
+				},
+				{
+					"screen_name": "Snapchat",
+					"name": "Snapchat",
+					"id": 376502929,
+					"id_str": "376502929",
+					"indices": [
+						71,
+						80
+					]
+				},
+				{
+					"screen_name": "netflix",
+					"name": "Netflix",
+					"id": 16573941,
+					"id_str": "16573941",
+					"indices": [
+						81,
+						89
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1648935820714188800,
+		"in_reply_to_status_id_str": "1648935820714188800",
+		"in_reply_to_user_id": 17620455,
+		"in_reply_to_user_id_str": "17620455",
+		"in_reply_to_screen_name": "Horizont",
+		"user": {
+			"id": 15378060,
+			"id_str": "15378060",
+			"name": "𝙷𝚎𝚗𝚍𝚛𝚒𝚔 𝚂𝚙𝚛𝚎𝚎",
+			"screen_name": "drikkes",
+			"location": "Cologne, Germany",
+			"description": "Seit ich laufen konnte, stand ich nur im Weg. \n@drikkes@mastodon.social",
+			"url": "https://t.co/u8Hh81YeCw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/u8Hh81YeCw",
+							"expanded_url": "https://drikkes.com",
+							"display_url": "drikkes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 789,
+			"friends_count": 146,
+			"listed_count": 73,
+			"created_at": "Thu Jul 10 14:34:41 +0000 2008",
+			"favourites_count": 5664,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 23492,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "17DB42",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1398588348588081158/ASX5ThKx_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15378060/1579772068",
+			"profile_link_color": "17B1DB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "de",
+		"text": "@Horizont @bundeswehrInfo @scholzfriends @Microsoft @Twitter @elonmusk @Snapchat @netflix Nach dem Relaunch ist Euer Newsletter zwar schicker, aber mobil ist die Schriftgröße eher eine Schriftkleine und als Gmail-Webview wird er nicht vollständig angezeigt. Suboptimal, finde ich."
+	},
+	{
 		"created_at": "Wed Apr 19 12:01:24 +0000 2023",
 		"id": 1648658049656029200,
 		"id_str": "1648658049656029186",
